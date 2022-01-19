cbc98a625f478c2baa99c19848c89084fd9b24d1
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-19 17:24:58 +0200

    remove complex

--- app/main.py
+++ app/main.py
@@ -11,39 +11,39 @@ class Distance:
     def __add__(self, other):
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (int, float, complex)):
+        elif isinstance(other, (int, float)):
             return Distance(self.km + other)
 
     def __iadd__(self, other):
         if isinstance(other, Distance):
             self.km += other.km
             return self
-        elif isinstance(other, (int, float, complex)):
+        elif isinstance(other, (int, float)):
             self.km += other
             return self
 
     def __mul__(self, other):
         if isinstance(other, Distance):
             return Distance(self.km * other.km)
-        elif isinstance(other, (int, float, complex)):
+        elif isinstance(other, (int, float)):
             return Distance(self.km * other)
 
     def __truediv__(self, other):
         if isinstance(other, Distance):
             return Distance(round(self.km / other.km, 2))
-        elif isinstance(other, (int, float, complex)):
+        elif isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
 
     def __eq__(self, other):
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, (int, float, complex)):
+        elif isinstance(other, (int, float)):
             return self.km == other
 
     def __gt__(self, other):
         if isinstance(other, Distance):
             return self.km > other.km
-        elif isinstance(other, (int, float, complex)):
+        elif isinstance(other, (int, float)):
             return self.km > other
 
     def __lt__(self, other):
