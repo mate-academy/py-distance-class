c5da6ca567e06794a9a57d332bc8379823659d85
polinatytarenko <p.tytarenko.20@gmail.com>
2022-01-19 17:44:54 +0200

    Added checks for float

--- app/main.py
+++ app/main.py
@@ -12,13 +12,13 @@ class Distance:
     def __add__(self, other):
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        if isinstance(other, int):
+        if isinstance(other, (int, float)):
             return Distance(self.km + other)
 
     def __iadd__(self, other):
         if isinstance(other, Distance):
             self.km += other.km
-        elif isinstance(other, int):
+        elif isinstance(other, (int, float)):
             self.km += other
         return self
 
@@ -31,19 +31,19 @@ class Distance:
     def __eq__(self, other):
         if isinstance(other, Distance):
             return self.km == other.km
-        if isinstance(other, int):
+        if isinstance(other, (int, float)):
             return self.km == other
 
     def __lt__(self, other):
         if isinstance(other, Distance):
             return self.km < other.km
-        if isinstance(other, int):
+        if isinstance(other, (int, float)):
             return self.km < other
 
     def __gt__(self, other):
         if isinstance(other, Distance):
             return self.km > other.km
-        if isinstance(other, int):
+        if isinstance(other, (int, float)):
             return self.km > other
 
     def __le__(self, other):
