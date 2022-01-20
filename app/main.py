c35f9b9ffe5052d7dec61d076c1dcd0f84081734
Dmitry Ivanov <karward94@gmail.com>
2022-01-20 19:56:31 +0200

    Hot fix

--- app/main.py
+++ app/main.py
@@ -27,17 +27,13 @@ class Distance:
             raise TypeError("The right operand should be int or float.")
 
     def __mul__(self, other):
-        if isinstance(other, Distance):
-            return Distance(self.km * other.km)
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return Distance(self.km * other)
         else:
             raise TypeError("The right operand should be int or float.")
 
     def __truediv__(self, other):
-        if isinstance(other, Distance):
-            return Distance(round(self.km / other.km, 2))
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
         else:
             raise TypeError("The right operand should be int or float.")
