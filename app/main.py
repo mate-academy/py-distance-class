c42a334a1461113c8402e4a4517eb3b6a5087c97
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-03-25 10:22:36 +0200

    Create magic mathods : - add, str, repr, len, and func increment add

--- app/main.py
+++ app/main.py
@@ -1,3 +1,37 @@
+from  typing import  Union
+
 class Distance:
     # Write your code here
-    pass
+    def __init__(self, km: Union[int, float]) -> None:
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        try:
+            return Distance(self.km + other.km)
+        except AttributeError:
+            return Distance(self.km + other)
+
+    def __len__(self):
+        try:
+            return len(self.km)
+        except TypeError:
+            return self.km
+
+
+
+distance1 = Distance(20)
+distance2 = Distance(30)
+distance3 = distance1 + distance2
+
+print(distance3)
+distance3 = distance1 + 30
+print(distance3)
+distance3 += 20
+print(distance3)
+print(len(distance3))
