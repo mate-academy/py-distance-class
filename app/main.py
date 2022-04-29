d96c7ae729dc78a1092d15a6605e9aeee520c491
Denis <sairustv228@gmail.com>
2022-04-29 17:31:02 +0300

    distance v1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,40 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km):
+        self.km = km
+
+    def __add__(self, other):
+        return Distance(other + self.km)
+
+    def __iadd__(self, other):
+        self.km = other + self.km
+        return self
+
+    def __len__(self):
+        return self.km
+
+    def __repr__(self):
+        return 'Distance(km={})'.format(self.km)
+
+    def __mul__(self, other):
+        return Distance(self.km * other)
+
+    def __str__(self):
+        return 'Distance: {} kilometers'.format(self.km)
+
+    def __truediv__(self, other):
+        return Distance(round(self.km / other, 2))
+
+    def __eq__(self, other):
+        return self.km == other
+
+    def __lt__(self, other):
+        return self.km < other
+
+    def __gt__(self, other):
+        return self.km > other
+
+    def __le__(self, other):
+        return self.km <= other
+
+    def __ge__(self, other):
+        return self.km >= other
