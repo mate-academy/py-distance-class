8af27171c6b025cde880dfe5a16d2107a3fc4f22
Oleksii Isaiev <isaev.ieskela@gmail.com>
2022-05-17 14:37:37 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,59 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        else:
+            self.km += other
+            return self
+
+    def __mul__(self, other):
+        return Distance(self.km * other)
+
+    def __truediv__(self, other):
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other):
+        if isinstance(other, Distance):
+            return True if self.km < other.km else False
+        else:
+            return True if self.km < other else False
+
+    def __gt__(self, other):
+        if isinstance(other, Distance):
+            return True if self.km > other.km else False
+        else:
+            return True if self.km > other else False
+
+    def __eq__(self, other):
+        if isinstance(other, Distance):
+            return True if self.km == other.km else False
+        else:
+            return True if self.km == other else False
+
+    def __le__(self, other):
+        return True if self.__lt__(other) or self.__eq__(other) \
+            else False
+
+    def __ge__(self, other):
+        return True if self.__gt__(other) or self.__eq__(other) \
+            else False
+
+    def __len__(self):
+        return self.km
