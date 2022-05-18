cb2d8a547dfa4b9a232b31ea6d548bf90303eef4
Flashmobber <ufinaid@gmail.com>
2022-05-18 18:01:45 +0300

    Some optimization

--- app/main.py
+++ app/main.py
@@ -12,9 +12,9 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other):
-        if isinstance(other, Distance):
-            return Distance(kilometer=self.km + other.km)
-        return Distance(kilometer=self.km + other)
+        return Distance(kilometer=self.km + other.km) \
+            if isinstance(other, Distance) \
+            else Distance(kilometer=self.km + other)
 
     def __iadd__(self, other):
         if isinstance(other, Distance):
@@ -25,39 +25,30 @@ class Distance:
         return self
 
     def __mul__(self, other):
-        if isinstance(other, Distance):
-            return Distance(self.km * other.km)
         return Distance(self.km * other)
 
     def __truediv__(self, other):
-        if isinstance(other, Distance):
-            return Distance(round(self.km / other.km, 2))
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other):
-        if isinstance(other, Distance):
-            return self.km < other.km
-        return self.km < other
+        return self.km < other.km \
+            if isinstance(other, Distance) else self.km < other
 
     def __gt__(self, other):
-        if isinstance(other, Distance):
-            return self.km > other.km
-        return self.km > other
+        return self.km > other.km \
+            if isinstance(other, Distance) else self.km > other
 
     def __eq__(self, other):
-        if isinstance(other, Distance):
-            return self.km == other.km
-        return self.km == other
+        return self.km == other.km \
+            if isinstance(other, Distance) else self.km == other
 
     def __le__(self, other):
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        return self.km <= other
+        return self.km <= other.km \
+            if isinstance(other, Distance) else self.km <= other
 
     def __ge__(self, other):
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        return self.km >= other
+        return self.km >= other.km \
+            if isinstance(other, Distance) else self.km >= other
 
     def __len__(self):
         return self.km
