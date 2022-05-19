9c95f3a659b81fd533e382d85e193e9aec6a8b7e
Vasyl Smutok <23.smutok@gmail.com>
2022-05-19 13:21:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,50 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        return Distance(
+            self.km + other if isinstance(other, int) else self.km + other.km
+        )
+
+    def __iadd__(self, other):
+        if type(other) == int:
+            self.km += other
+        else:
+            self.km += other.km
+        return self
+
+    def __mul__(self, other):
+        return Distance(
+            self.km * other if isinstance(other, int) else self.km * other.km
+        )
+
+    def __truediv__(self, other):
+        if isinstance(other, int):
+            return Distance(round((self.km / other), 2))
+        else:
+            return Distance(round((self.km / other.km), 2))
+
+    def __lt__(self, other):
+        return self.km < other if type(other) == int else self.km < other.km
+
+    def __gt__(self, other):
+        return self.km > other if type(other) == int else self.km > other.km
+
+    def __eq__(self, other):
+        return self.km == other if type(other) == int else self.km == other.km
+
+    def __le__(self, other):
+        return self.km <= other if type(other) == int else self.km <= other.km
+
+    def __ge__(self, other):
+        return self.km >= other if type(other) == int else self.km >= other.km
+
+    def __len__(self):
+        return self.km
