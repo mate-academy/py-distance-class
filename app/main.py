bcb154a69fa6c824a3e464699320db79429658de
Yurii Kindrat <yurkinkek@gmail.com>
2022-05-19 15:53:10 +0300

    fixed all edits

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ class Distance:
         self.km = km
 
     def __str__(self):
-        return f'Distance: {self.km} kilometers.'
+        return f"Distance: {self.km} kilometers."
 
     def __repr__(self):
-        return f'Distance(km={self.km})'
+        return f"Distance(km={self.km})"
 
     def __add__(self, other):
         if isinstance(other, Distance):
@@ -17,10 +17,9 @@ class Distance:
     def __iadd__(self, other):
         if isinstance(other, Distance):
             self.km += other.km
-            return self
         else:
             self.km += other
-            return self
+        return self
 
     def __mul__(self, other):
         return Distance(self.km * other)
@@ -47,10 +46,10 @@ class Distance:
             return self.km == other
 
     def __le__(self, other):
-        return any([self.__lt__(other), self.__eq__(other)])
+        return not self.__gt__(other)
 
     def __ge__(self, other):
-        return any([self.__gt__(other), self.__eq__(other)])
+        return not self.__lt__(other)
 
     def __len__(self):
         return self.km
