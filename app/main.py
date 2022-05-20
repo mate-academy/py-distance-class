186b02b6f7b02148ea272bbe923fe876b7fc4055
Magus6784 <100879423+Magus6784@users.noreply.github.com>
2022-05-20 22:48:00 +0300

    add float

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ class Distance:
         self.km = km
 
     def __add__(self, other):
-        if type(other) is Distance:
+        if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif type(other) is int:
+        elif isinstance(other, (int, float)):
             return Distance(self.km + other)
 
     def __iadd__(self, other):
-        if type(other) is Distance:
+        if isinstance(other, Distance):
             self.km += other.km
-        elif type(other) is int:
+        elif isinstance(other, (int, float)):
             self.km += other
         return self
 
@@ -23,21 +23,21 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other):
-        if type(other) is Distance:
+        if isinstance(other, Distance):
             return self.km < other.km
-        else:
+        if isinstance(other, (int, float)):
             return self.km < other
 
     def __gt__(self, other):
-        if type(other) is Distance:
+        if isinstance(other, Distance):
             return self.km > other.km
-        else:
+        if isinstance(other, (int, float)):
             return self.km > other
 
     def __eq__(self, other):
-        if type(other) is Distance:
+        if isinstance(other, Distance):
             return self.km == other.km
-        else:
+        if isinstance(other, (int, float)):
             return self.km == other
 
     def __le__(self, other):
