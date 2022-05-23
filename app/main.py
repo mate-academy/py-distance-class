8fb103599df253caa788877b3e10bb2d706eff4e
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2022-05-23 13:50:05 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -28,25 +28,19 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other):
-        if isinstance(other, Distance):
-            return self.km < other.km
-        return self.km < other
+        return self.km < other.km if isinstance(other, Distance) else self.km < other
 
     def __gt__(self, other):
-        if isinstance(other, Distance):
-            return self.km > other.km
-        return self.km > other
+        return self.km > other.km if isinstance(other, Distance) else self.km > other
 
     def __eq__(self, other):
-        if isinstance(other, Distance):
-            return self.km == other.km
-        return self.km == other
+        return self.km == other.km if isinstance(other, Distance) else self.km == other
 
     def __le__(self, other):
-        return self.km <= other
+        return self.km <= other.km if isinstance(other, Distance) else self.km <= other
 
     def __ge__(self, other):
-        return self.km >= other
+        return self.km >= other.km if isinstance(other, Distance) else self.km >= other
 
     def __len__(self):
         return self.km
