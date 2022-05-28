6ecaa0e1395bc452d5527daead4ebca5f7955ac8
Oleg Fadeev <drivemax26@gmail.com>
2022-05-28 21:27:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -15,16 +15,17 @@ class Distance:
             return Distance(self.km + other)
 
     def __iadd__(self, other):
-        if isinstance(other, Distance):
-            return self.km += other.km
+        if isinstance(other, (int, float)):
+            self.km += other
         else:
-            return self.km += other
+            self.km += other.km
+        return self
 
     def __mul__(self, other):
         return Distance(self.km * other)
 
     def __truediv__(self, other):
-        return Distance(round(self.km / other,2))
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other):
         if isinstance(other, Distance):
@@ -52,12 +53,9 @@ class Distance:
 
     def __ge__(self, other):
         if isinstance(other, Distance):
-            return self.km > other.km
+            return self.km >= other.km
         else:
-            return self.km > other
+            return self.km >= other
 
     def __len__(self):
         return self.km
-
-
-
