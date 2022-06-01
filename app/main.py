fd3d02c33330c5b8e2ac02830e39274cda66aeb8
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-01 23:08:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,68 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if type(other) is Distance:
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __iadd__(self, other):
+        if type(other) is Distance:
+            self.km = self.km + other.km
+            return self
+        else:
+            self.km = self.km + other
+            return self
+
+    def __mul__(self, other):
+        if type(other) is Distance:
+            return Distance(self.km * other.km)
+        else:
+            return Distance(self.km * other)
+
+    def __truediv__(self, other):
+        if type(other) is Distance:
+            return Distance(round(self.km / other.km, 2))
+        else:
+            return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other):
+        if type(other) is Distance:
+            return self.km < other.km
+        else:
+            return self.km < other
+
+    def __gt__(self, other):
+        if type(other) is Distance:
+            return self.km > other.km
+        else:
+            return self.km > other
+
+    def __eq__(self, other):
+        if type(other) is Distance:
+            return self.km == other.km
+        else:
+            return self.km == other
+
+    def __le__(self, other):
+        if type(other) is Distance:
+            return self.km <= other.km
+        else:
+            return self.km <= other
+
+    def __ge__(self, other):
+        if type(other) is Distance:
+            return self.km >= other.km
+        else:
+            return self.km >= other
+
+    def __len__(self):
+        return self.km
