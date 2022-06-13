05d15db00b04a57e6dcdd75daa5dc506e5f4b450
Dmytro Melnyk <61544970+melnikd111@users.noreply.github.com>
2022-06-13 13:31:56 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,8 +12,7 @@ class Distance:
     def __add__(self, other):
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        else:
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
     def __iadd__(self, other):
         if isinstance(other, Distance):
@@ -33,32 +32,27 @@ class Distance:
     def __lt__(self, other):
         if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other):
         if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            return self.km > other
+        return self.km > other
 
     def __eq__(self, other):
         if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            return self.km == other
+        return self.km == other
 
     def __le__(self, other):
         if isinstance(other, Distance):
             return self.km <= other.km
-        else:
-            return self.km <= other
+        return self.km <= other
 
     def __ge__(self, other):
         if isinstance(other, Distance):
             return self.km >= other.km
-        else:
-            return self.km >= other
+        return self.km >= other
 
     def __len__(self):
         return self.km
