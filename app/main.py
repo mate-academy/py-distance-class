c4d2fcabe5948dc6b12f729fa504502ad12dba4a
Tetiana Omelchenko <t.omel4enko@gmail.com>
2022-06-18 13:56:32 +0800

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,64 @@
 class Distance:
-    # Write your code here
+    def __init__(self, km):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers"
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, num):
+        return Distance(self.km * num)
+
+    def __truediv__(self, num):
+        new_distance = round(self.km / num, 2)
+        return Distance(new_distance)
+
+    def __lt__(self, other):
+        if isinstance(other, Distance):
+            return self.km < other.km
+        else:
+            return self.km < other
+
+    def __gt__(self, other):
+        if isinstance(other, Distance):
+            return self.km > other.km
+        else:
+            return self.km > other
+
+    def __eq__(self, other):
+        if isinstance(other, Distance):
+            return self.km == other.km
+        else:
+            return self.km == other
+
+    def __le__(self, other):
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        else:
+            return self.km <= other
+
+    def __ge__(self, other):
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        else:
+            return self.km >= other
+
+    def __len__(self):
+        return self.km
+
     pass
