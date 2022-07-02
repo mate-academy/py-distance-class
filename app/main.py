5e0d12af55b88378e6776068eaf9d962456e3f88
Andriy Demeshko <ande777complaint@gmail.com>
2022-07-02 12:19:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,81 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km):
+        self.km = km
+
+    def __str__(self):
+        print(f"Distance: {self.km} kilometers.")
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other):
+        return Distance(self.km * other)
+
+    def __truediv__(self, other):
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other):
+        if isinstance(other, Distance):
+            if self.km < other.km:
+                return True
+            return False
+        else:
+            if self.km < other:
+                return True
+            return False
+
+    def __le__(self, other):
+        if isinstance(other, Distance):
+            if self.km <= other.km:
+                return True
+            return False
+        else:
+            if self.km <= other:
+                return True
+            return False
+
+    def __gt__(self, other):
+        if isinstance(other, Distance):
+            if self.km > other.km:
+                return True
+            return False
+        else:
+            if self.km > other:
+                return True
+            return False
+
+    def __ge__(self, other):
+        if isinstance(other, Distance):
+            if self.km >= other.km:
+                return True
+            return False
+        else:
+            if self.km >= other:
+                return True
+            return False
+
+    def __eq__(self, other):
+        if isinstance(other, Distance):
+            if self.km == other.km:
+                return True
+            return False
+        else:
+            if self.km == other:
+                return True
+            return False
+
+    def __len__(self):
+        return self.km
