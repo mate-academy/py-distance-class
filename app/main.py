108784057b3d6283b972538625337418e738a6ed
Roman <coderc770@gmail.com>
2022-07-07 10:25:44 +0300

    addComparisonToClass

--- app/main.py
+++ app/main.py
@@ -17,10 +17,10 @@ class Distance:
     def __iadd__(self, other):
         if isinstance(other, Distance):
             self.km += other.km
-            return self
         else:
             self.km += other
-            return self
+
+        return self
 
     def __mul__(self, other):
         return Distance(self.km * other)
@@ -29,18 +29,28 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other):
+        if isinstance(other, Distance):
+            return self.km < other.km
         return self.km < other
 
     def __gt__(self, other):
+        if isinstance(other, Distance):
+            return self.km > other.km
         return self.km > other
 
     def __eq__(self, other):
+        if isinstance(other, Distance):
+            return self.km == other.km
         return self.km == other
 
     def __le__(self, other):
+        if isinstance(other, Distance):
+            return self.km <= other.km
         return self.km <= other
 
     def __ge__(self, other):
+        if isinstance(other, Distance):
+            return self.km >= other.km
         return self.km >= other
 
     def __len__(self):
