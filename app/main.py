12771de6c96e4a5cae3de5e3ca1e38e1ef686567
Timur <timur.hike@gmail.com>
2022-07-19 16:02:30 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,63 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if type(other) is int:
+            return Distance(self.km + other)
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other):
+        if type(other) is int:
+            self.km += other
+            return self
+        self.km += other.km
+        return self
+
+    def __mul__(self, other):
+        if type(other) is int:
+            return Distance(self.km * other)
+        return Distance(self.km * other.km)
+
+    def __truediv__(self, other):
+        if type(other) is int:
+            return Distance(round(self.km / other, 2))
+        return Distance(round(self.km / other.km, 2))
+
+    def __len__(self):
+        return self.km
+
+    def __lt__(self, other):
+        if type(other) is int:
+            return self.km < other
+        return self.km < other.km
+
+    def __gt__(self, other):
+        if type(other) is int:
+            return self.km > other
+        return self.km > other.km
+
+    def __eq__(self, other):
+        if type(other) is int:
+            return self.km == other
+        return self.km == other.km
+
+    def __le__(self, other):
+        if type(other) is int:
+            return self.km <= other
+        return self.km <= other.km
+
+    def __ge__(self, other):
+        if type(other) is int:
+            return self.km >= other
+        return self.km >= other.km
+
+
+distance = Distance(50)
+print(distance < Distance(60))
