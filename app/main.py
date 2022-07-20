30f7f4c9431dd330dd5c8b581ea3a9d2b0f717c8
Timur <timur.hike@gmail.com>
2022-07-20 12:53:39 +0300

    add isinstatce

--- app/main.py
+++ app/main.py
@@ -9,24 +9,24 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             return Distance(self.km + other)
         return Distance(self.km + other.km)
 
     def __iadd__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             self.km += other
             return self
         self.km += other.km
         return self
 
     def __mul__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             return Distance(self.km * other)
         return Distance(self.km * other.km)
 
     def __truediv__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             return Distance(round(self.km / other, 2))
         return Distance(round(self.km / other.km, 2))
 
@@ -34,26 +34,26 @@ class Distance:
         return self.km
 
     def __lt__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             return self.km < other
         return self.km < other.km
 
     def __gt__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             return self.km > other
         return self.km > other.km
 
     def __eq__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             return self.km == other
         return self.km == other.km
 
     def __le__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             return self.km <= other
         return self.km <= other.km
 
     def __ge__(self, other):
-        if type(other) is int:
+        if isinstance(other, int):
             return self.km >= other
         return self.km >= other.km
