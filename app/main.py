ad0d4a83e2e7d242dfc4429b747f3c2d820c6b76
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2022-07-25 12:56:44 +0300

    change for ALL methods

--- app/main.py
+++ app/main.py
@@ -23,13 +23,22 @@ class Distance:
         return self
 
     def __mul__(self, other):
-        return Distance(self.km * other)
+        if isinstance(other, (int, float)):
+            return Distance(self.km * other)
+        else:
+            return Distance(self.km * other.km)
 
     def __truediv__(self, other):
-        return Distance(round(self.km / other, 2))
+        if isinstance(other, (int, float)):
+            return Distance(round(self.km / other, 2))
+        else:
+            return Distance(round(self.km / other.km, 2))
 
     def __lt__(self, other):
-        return self.km < other
+        if isinstance(other, (int, float)):
+            return self.km < other
+        else:
+            return self.km < other.km
 
     def __gt__(self, other):
         if isinstance(other, (int, float)):
@@ -38,13 +47,22 @@ class Distance:
             return self.km > other.km
 
     def __eq__(self, other):
-        return self.km == other
+        if isinstance(other, (int, float)):
+            return self.km == other
+        else:
+            return self.km == other.km
 
     def __le__(self, other):
-        return self.km <= other
+        if isinstance(other, (int, float)):
+            return self.km <= other
+        else:
+            return self.km <= other.km
 
     def __ge__(self, other):
-        return self.km >= other
+        if isinstance(other, (int, float)):
+            return self.km >= other
+        else:
+            return self.km >= other.km
 
     def __len__(self):
         return self.km
