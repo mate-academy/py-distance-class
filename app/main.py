9f530530a34f4b7e4a054e295e4899502c257a2f
Vlad Baryshnikov <nuhrbuh4uk@gmail.com>
2022-07-25 15:28:02 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -21,24 +21,34 @@ class Distance:
         return self
 
     def __mul__(self, other):
-        return Distance(self.km * other)
+        return Distance(km=self.km * other)
 
     def __truediv__(self, other):
-        return Distance(round(self.km / other, 2))
+        return Distance(km=round(self.km / other, 2))
 
     def __lt__(self, other):
+        if isinstance(other, Distance):
+            return self.km < other.km
         return self.km < other
 
     def __gt__(self, other):
+        if isinstance(other, Distance):
+            return self.km > other.km
         return self.km > other
 
     def __eq__(self, other):
+        if isinstance(other, Distance):
+            return self.km == other.km
         return self.km == other
 
     def __le__(self, other):
+        if isinstance(other, Distance):
+            return self.km <= other.km
         return self.km <= other
 
     def __ge__(self, other):
+        if isinstance(other, Distance):
+            return self.km >= other.km
         return self.km >= other
 
     def __len__(self):
