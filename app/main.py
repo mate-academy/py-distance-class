2e9305f24a6d3622a3f70e11ebdf079b3246ee85
Vladimir Byshevets <texnadzor.info@gmail.com>
2022-08-02 14:24:26 +0300

    Latest fixes

--- app/main.py
+++ app/main.py
@@ -10,63 +10,63 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, distance):
-        if isinstance(distance, int) or isinstance(distance, float):
+        if isinstance(distance, (int, float)):
             dist = self.km + distance
         else:
             dist = self.km + distance.km
         return Distance(dist)
 
     def __iadd__(self, distance):
-        if isinstance(distance, int):
+        if isinstance(distance, (int, float)):
             self.km += distance
         else:
             self.km += distance.km
         return self
 
     def __mul__(self, distance):
-        if isinstance(distance, int):
+        if isinstance(distance, (int, float)):
             self.km = self.km * distance
         else:
             self.km = self.km * distance.km
         return Distance(self.km)
 
     def __truediv__(self, distance):
-        if isinstance(distance, int):
+        if isinstance(distance, (int, float)):
             self.km = round((self.km / distance), 2)
         else:
             self.km = round((self.km / distance.km), 2)
         return Distance(self.km)
 
     def __lt__(self, distance):
-        if isinstance(distance, int):
+        if isinstance(distance, (int, float)):
             dist = distance
         else:
             dist = distance.km
         return self.km < dist
 
     def __gt__(self, distance):
-        if isinstance(distance, int):
+        if isinstance(distance, (int, float)):
             dist = distance
         else:
             dist = distance.km
         return self.km > dist
 
     def __eq__(self, distance):
-        if isinstance(distance, int):
+        if isinstance(distance, (int, float)):
             dist = distance
         else:
             dist = distance.km
-        return self.km is dist
+        return self.km == dist
 
     def __le__(self, distance):
-        if isinstance(distance, int):
+        if isinstance(distance, (int, float)):
             dist = distance
         else:
             dist = distance.km
         return self.km <= dist
 
     def __ge__(self, distance):
-        if isinstance(distance, int):
+        if isinstance(distance, (int, float)):
             dist = distance
         else:
             dist = distance.km
