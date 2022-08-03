316ff6aa1f7ca6a6297394982e3f33302d525aad
Victor <ruravictor@gmail.com>
2022-08-03 11:24:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -9,13 +9,12 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, int) or isinstance(other, float):
             return Distance(self.km + other)
-        else:
-            return Distance(self.km + other.km)
+        return Distance(self.km + other.km)
 
     def __iadd__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, int) or isinstance(other, float):
             self.km += other
             return self
         else:
@@ -31,31 +30,26 @@ class Distance:
         return distance
 
     def __eq__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, int) or isinstance(other, float):
             return self.km == other
-        else:
-            return self.km == other.km
+        return self.km == other.km
 
     def __gt__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, int) or isinstance(other, float):
             return self.km > other
-        else:
-            return self.km > other.km
+        return self.km > other.km
 
     def __ge__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, int) or isinstance(other, float):
             return self.km >= other
-        else:
-            return self.km >= other.km
+        return self.km >= other.km
 
     def __lt__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, int) or isinstance(other, float):
             return self.km < other
-        else:
-            return self.km < other.km
+        return self.km < other.km
 
     def __le__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, int) or isinstance(other, float):
             return self.km <= other
-        else:
-            return self.km <= other.km
+        return self.km <= other.km
