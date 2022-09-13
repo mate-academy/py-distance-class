495e8155e9bbcb053daac4b2f534fb6c62108b80
Artem Vykhovanets <80789163+AVyha@users.noreply.github.com>
2022-09-13 21:08:50 +0300

    Solution
    Fix all problem. I hope that i dont have any other issues)

--- app/main.py
+++ app/main.py
@@ -15,31 +15,47 @@ class Distance:
 
     def __iadd__(self, other):
         if type(other) in (int, float):
-            self.km = self.km + other
+            self.km += other
             return self
-        self.km = self.km + other.km
+        self.km += other.km
         return self
 
     def __mul__(self, other):
-        return Distance(self.km * other)
+        if type(other) in (int, float):
+            return Distance(self.km * other)
+        return Distance(self.km * other.km)
 
     def __truediv__(self, other):
-        return Distance(round(self.km / other, 2))
+        if type(other) in (int, float):
+            return Distance(round(self.km / other, 2))
+        return Distance(round(self.km / other.km, 2))
 
     def __lt__(self, other):
-        return self.km < other
+        if type(other) in (int, float):
+            return self.km < other
+        return self.km < other.km
+
 
     def __gt__(self, other):
-        return self.km > other
+        if type(other) in (int, float):
+            return self.km > other
+        return self.km > other.km
 
     def __eq__(self, other):
-        return self.km == other
+        if type(other) in (int, float):
+            return self.km == other
+        return self.km == other.km
 
     def __le__(self, other):
-        return self.km <= other
+        if type(other) in (int, float):
+            return self.km <= other
+        return self.km <= other.km
 
     def __ge__(self, other):
-        return self.km >= other
+        if type(other) in (int, float):
+            return self.km >= other
+        return self.km >= other.km
 
     def __len__(self):
         return self.km
+    
