613d9fc8e6f05fe6162d56771494bd9920fb2df2
IvanGLS <gladiolus0707@gmail.com>
2022-09-16 10:20:49 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -11,58 +11,63 @@ class Distance:
     def __add__(self, new_distance):
         if isinstance(new_distance, Distance):
             return Distance(self.km + new_distance.km)
-        elif new_distance is not isinstance:
+        if isinstance(new_distance, (int, float)):
             return self + Distance(new_distance)
 
     def __iadd__(self, new_distance):
         if isinstance(new_distance, Distance):
             self.km += new_distance.km
-            return self
-        elif new_distance is not isinstance:
+        if isinstance(new_distance, (int, float)):
             self.km += new_distance
-            return self
+        return self
 
     def __mul__(self, new_distance):
         if isinstance(new_distance, Distance):
             return Distance(self.km * new_distance.km)
-        elif new_distance is not isinstance:
+        if isinstance(new_distance, (int, float)):
             return self * Distance(new_distance)
 
     def __truediv__(self, new_distance):
         if isinstance(new_distance, Distance):
-            return Distance(round((self.km / new_distance.km), 2))
-        elif new_distance is not isinstance:
-            return Distance(round((self.km / new_distance), 2))
+            result = result = self.km / new_distance.km
+        if isinstance(new_distance, (int, float)):
+            result = result = self.km / new_distance
+        return Distance(round(result, 2))
 
     def __lt__(self, new_distance):
         if isinstance(new_distance, Distance):
-            return self.km < new_distance.km
-        elif new_distance is not isinstance:
-            return self.km < new_distance
+            result = self.km < new_distance.km
+        if isinstance(new_distance, (int, float)):
+            result = self.km < new_distance
+        return result
 
     def __gt__(self, new_distance):
         if isinstance(new_distance, Distance):
-            return self.km > new_distance.km
-        elif new_distance is not isinstance:
-            return self.km > new_distance
+            result = self.km > new_distance.km
+        if isinstance(new_distance, (int, float)):
+            result = self.km > new_distance
+        return result
 
     def __eq__(self, new_distance):
         if isinstance(new_distance, Distance):
-            return self.km == new_distance.km
-        elif new_distance is not isinstance:
-            return self.km == new_distance
+            result = self.km == new_distance.km
+        if isinstance(new_distance, (int, float)):
+            result = self.km == new_distance
+        return result
 
     def __le__(self, new_distance):
         if isinstance(new_distance, Distance):
-            return self.km <= new_distance.km
-        elif new_distance is not isinstance:
-            return self.km <= new_distance
+            result = self.km <= new_distance.km
+        if isinstance(new_distance, (int, float)):
+            result = self.km <= new_distance
+        return result
 
     def __ge__(self, new_distance):
         if isinstance(new_distance, Distance):
-            return self.km >= new_distance.km
-        elif new_distance is not isinstance:
-            return self.km >= new_distance
+            result = self.km >= new_distance.km
+        if isinstance(new_distance, (int, float)):
+            result = self.km >= new_distance
+        return result
 
     def __len__(self):
         return self.km
