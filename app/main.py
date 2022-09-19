eb9ec0324b9d55e6701147effba27b3ec80de5eb
igor kosianenko <funkcionalkos@gmail.com>
2022-09-19 16:36:17 +0300

    fixed solution

--- app/main.py
+++ app/main.py
@@ -14,8 +14,8 @@ class Distance:
                if isinstance(other, Distance)
                else Distance(km=self.km + other))
 
-    def __iadd__(self, othe):
-        self.km = self.km + (othe.km if isinstance(othe, Distance) else othe)
+    def __iadd__(self, other):
+        self.km = self.km + self.obj_or_int(other)
         return self
 
     def __mul__(self, mul):
@@ -24,20 +24,23 @@ class Distance:
     def __truediv__(self, div):
         return Distance(km=round((self.km / div), 2))
 
+    def obj_or_int(self, obj):
+        return obj.km if isinstance(obj, Distance) else obj
+
     def __lt__(self, other):
-        return self.km < (other.km if isinstance(other, Distance) else other)
+        return self.km < self.obj_or_int(other)
 
     def __gt__(self, other):
-        return self.km > (other.km if isinstance(other, Distance) else other)
+        return self.km > self.obj_or_int(other)
 
     def __eq__(self, other):
-        return self.km == (other.km if isinstance(other, Distance) else other)
+        return self.km == self.obj_or_int(other)
 
     def __le__(self, other):
-        return self.km <= (other.km if isinstance(other, Distance) else other)
+        return self.km <= self.obj_or_int(other)
 
     def __ge__(self, other):
-        return self.km >= (other.km if isinstance(other, Distance) else other)
+        return self.km >= self.obj_or_int(other)
 
     def __len__(self):
         return range(self.km)
