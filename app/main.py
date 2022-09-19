bb3a4ed842654f2820924cc07d03910595c420cf
Alexander <alexzubkoff123@gmail.com>
2022-09-19 18:08:35 +0300

    Added some magic methods for inbuilt operations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,68 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __radd__(self, other):
+        if isinstance(other, Distance):
+            return Distance(other.km + self.km)
+        return Distance(other + self.km)
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        self.km += other
+        return self
+
+    def __mul__(self, other):
+        if isinstance(other, Distance):
+            self.km *= other.km
+            return self.km
+        self.km *= other
+        return Distance(self.km)
+
+    def __truediv__(self, other):
+        if isinstance(other, Distance):
+            self.km /= other.km
+            return self.km
+        self.km /= other
+        return Distance(round(self.km, 2))
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        return other == self.km
+
+    def __lt__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        return other > self.km
+
+    def __gt__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return other < self.km
+
+    def __le__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        return other >= self.km
+
+    def __ge__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return other <= self.km
+
+    def __len__(self) -> int:
+        return self.km
