f2f11775deedfa3860e43a335e5af3c18419afbc
Taras Andruschenko <And999110@gmail.com>
2022-09-19 13:45:10 +0300

    Annotations added to all methods and simple __le__/__ge__ added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Distance:
 
-    def __init__(self, km):
+    def __init__(self, km: int):
         self.km = km
 
     def __str__(self):
@@ -9,50 +9,48 @@ class Distance:
     def __repr__(self):
         return f'Distance(km={self.km})'
 
-    def __add__(self, other):
+    def __add__(self, other: int):
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: int):
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: int):
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
-        if isinstance(other, Distance):
-            return Distance(self.km / other.km)
+    def __truediv__(self, other: (int, float)):
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other: int):
         if isinstance(other, Distance):
             return self.km < other.km
         return self.km < other
 
-    def __gt__(self, other):
+    def __gt__(self, other: int):
         if isinstance(other, Distance):
             return self.km > other.km
         return self.km > other
 
-    def __eq__(self, other):
+    def __eq__(self, other: int):
         if isinstance(other, Distance):
             return self.km == other.km
         return self.km == other
 
-    def __le__(self, other):
+    def __le__(self, other: int):
         if isinstance(other, Distance):
-            return self.km.__lt__(other.km) or self.km.__le__(other.km)
-        return self.km.__lt__(other) or self.km.__le__(other)
+            return self.km <= other.km
+        return self.km <= other
 
-    def __ge__(self, other):
+    def __ge__(self, other: int):
         if isinstance(other, Distance):
-            return self.km.__gt__(other.km) or self.km.__ge__(other.km)
-        return self.km.__gt__(other) or self.km.__ge__(other)
+            return self.km >= other.km
+        return self.km >= other
 
     def __len__(self):
         return self.km
