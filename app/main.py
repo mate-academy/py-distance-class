e066189c0229ce33defe99739795e87cfea29b45
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-09-19 18:52:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -11,8 +11,12 @@ class Distance:
 
     def __add__(self, other):
         if isinstance(other, Distance):
-            return Distance(self.km + other.km)
-        return Distance(self.km + other)
+            return Distance(
+                self.km + other.km
+            )
+        return Distance(
+            self.km + other
+        )
 
     def __iadd__(self, other):
         if isinstance(other, Distance):
