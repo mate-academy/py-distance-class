e2c21ae8788422ea0bc570e439ec0d1114a0f95e
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-09-20 23:42:56 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,56 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if type(other) == Distance:
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __iadd__(self, other):
+        if type(other) == Distance:
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other):
+        if type(other) == Distance:
+            return Distance(self.km * other.km)
+        else:
+            return Distance(self.km * other)
+
+    def __truediv__(self, other: int):
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other) -> bool:
+        if type(other) == Distance:
+            return self.km < other.km
+        else:
+            return self.km < other
+
+    def __gt__(self, other) -> bool:
+        return not self.__le__(other)
+
+    def __eq__(self, other) -> bool:
+        return self.km == other
+
+    def __le__(self, other) -> bool:
+        if type(other) == Distance:
+            return self.km <= other.km
+        else:
+            return self.km <= other
+
+    def __ge__(self, other) -> bool:
+        return not self.__lt__(other)
+
+    def __len__(self):
+        return self.km
