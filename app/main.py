893a460e15df6b67389d115d2273b02771e57269
Vasyl Poremchuk <nleveryone@gmail.com>
2022-09-21 13:31:59 +0300

    Annotations have been added

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
+from __future__ import annotations
+
+
 class Distance:
-    def __init__(self, km: (int, float)):
+    def __init__(self, km: (int, float)) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
 
         return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
@@ -22,32 +25,32 @@ class Distance:
 
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km * other.km)
 
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other) -> Distance:
         if isinstance(other, Distance):
             return Distance(round(self.km / other.km, 2))
 
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         return self.km < other
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
         return self.km > other
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.km == other
 
-    def __le__(self, other):
+    def __le__(self, other) -> bool:
         return self.km <= other
 
-    def __ge__(self, other):
+    def __ge__(self, other) -> bool:
         return self.km >= other
 
-    def __len(self):
+    def __len(self) -> int:
         return self
