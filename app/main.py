32d5330af95ae5023b88fbdc5c7c971a6211d73e
Roman Kolmakov <cerne313@gmail.com>
2022-09-21 19:52:46 +0300

    add a static method to ckeck for "other" var type, refactor class to use it

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ class Distance:
     def __init__(self, km):
         self.km = km
 
+    @staticmethod
+    def check_type(other):
+        if isinstance(other, Distance):
+            return other.km
+        return other
+
     def __str__(self):
         return f'Distance: {self.km} kilometers.'
 
@@ -9,36 +15,32 @@ class Distance:
         return f'Distance(km={self.km})'
 
     def __add__(self, other):
-        if isinstance(other, Distance):
-            km = self.km + other.km
-        else:
-            km = self.km + other
-        return Distance(km)
+        return Distance(self.km + self.check_type(other))
 
     def __iadd__(self, other):
-        self.km = self.__add__(other).km
+        self.km = self.__add__(other)
         return self
 
     def __mul__(self, other):
-        return Distance(self.km * other)
+        return Distance(self.km * self.check_type(other))
 
     def __truediv__(self, other):
-        return Distance(round(self.km / other, 2))
+        return Distance(round(self.km / self.check_type(other), 2))
 
     def __lt__(self, other):
-        return self.km < other
+        return self.km < self.check_type(other)
 
     def __gt__(self, other):
-        return self.km > other
+        return self.km > self.check_type(other)
 
     def __eq__(self, other):
-        return self.km == other
+        return self.km == self.check_type(other)
 
     def __le__(self, other):
-        return self.km <= other
+        return self.__lt__(other) or self.__eq__(other)
 
     def __ge__(self, other):
-        return self.km >= other
+        return self.__gt__(other) or self.__eq__(other)
 
     def __len__(self):
         return self.km
