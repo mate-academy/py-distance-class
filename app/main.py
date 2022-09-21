9d64d4a693195e4967e753aca87a7c64a5bbc7f4
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-09-21 22:03:00 +0300

    'Solution-version_6'

--- app/main.py
+++ app/main.py
@@ -17,10 +17,10 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other) -> Distance:
+    def __add__(self, other: Distance) -> Distance:
         return Distance(self.km + self.check_type(other))
 
-    def __iadd__(self, other) -> Distance:
+    def __iadd__(self, other: Distance) -> Distance:
         self.km += self.check_type(other)
         return self
 
@@ -32,19 +32,19 @@ class Distance:
             round(self.km / other, 2)
         )
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: Distance) -> bool:
         return self.km < self.check_type(other)
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: Distance) -> bool:
         return self.km > self.check_type(other)
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: Distance) -> bool:
         return self.km == self.check_type(other)
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: Distance) -> bool:
         return self.km <= self.check_type(other)
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: Distance) -> bool:
         return self.km >= self.check_type(other)
 
     def __len__(self) -> int:
