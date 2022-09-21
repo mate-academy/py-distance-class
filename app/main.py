90408d9fa17933018d37d71ff16476eaf4c08a4d
HStakhiv <gsstahiv@gmail.com>
2022-09-21 13:28:08 +0300

    All fixed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: int | float) -> None:
         self.km = km
@@ -8,17 +11,17 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other) -> Distance:
         return Distance(self.km + self.instance_check(other))
 
-    def __iadd__(self, other):
+    def __iadd__(self, other) -> Distance:
         self.km += self.instance_check(other)
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other) -> Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other) -> bool:
@@ -31,14 +34,14 @@ class Distance:
         return self.km == self.instance_check(other)
 
     def __le__(self, other) -> bool:
-        return not self.__gt__(other)
+        return not self > other
 
     def __ge__(self, other) -> bool:
-        return not self.__lt__(other)
+        return not self < other
 
     def __len__(self) -> int:
         return self.km
 
     @staticmethod
-    def instance_check(other) -> int:
+    def instance_check(other) -> int | float:
         return other.km if isinstance(other, Distance) else other
