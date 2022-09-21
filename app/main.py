d5afdf3bc03210fcbaa3c41675372a9508fc061e
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-09-21 11:34:55 +0200

    'Changed'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
+
 class Distance:
 
-    def __init__(self, km) -> None:
+    def __init__(self, km: int) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -9,26 +12,26 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other) -> Distance:
         if type(other) == Distance:
             return Distance(self.km + other.km)
         else:
             return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other) -> Distance:
         if type(other) == Distance:
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> Distance:
         if type(other) == Distance:
             return Distance(self.km * other.km)
         else:
             return Distance(self.km * other)
 
-    def __truediv__(self, other: int):
+    def __truediv__(self, other) -> Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other) -> bool:
@@ -52,5 +55,5 @@ class Distance:
     def __ge__(self, other) -> bool:
         return not self.__lt__(other)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.km
