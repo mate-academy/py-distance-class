59666f142c5b596de13dc70b61e1ed1d38107567
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-09-21 22:42:49 +0300

    Type annotation and one line decisionÑ

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 class Distance:
-    def __init__(self, km):
+    def __init__(self, km: (int, float)) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
     def __add__(self, other):
@@ -20,33 +20,29 @@ class Distance:
         self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: (int, float)):
         return Distance(self.km * other)
 
     def __truediv__(self, other):
         return Distance(round(self.km / other, 2))
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         return self.km == other
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         return self.km > other
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         return self.km < other
 
-    def __ge__(self, other):
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        return self.km >= other
+    def __ge__(self, other) -> bool:
+        return not self < other
 
-    def __le__(self, other):
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        return self.km <= other
+    def __le__(self, other) -> bool:
+        return not self > other
