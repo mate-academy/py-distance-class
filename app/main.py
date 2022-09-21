7b36b091f5485e8fbfdd453b5bc4fd21e305c865
Zakhar Podporinov <zp162162@gmail.com>
2022-09-21 12:11:42 +0300

    '3rSolution'

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 class Distance:
-    def __init__(self, km: int):
+    def __init__(self, km: int) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
     def __add__(self, other):
@@ -21,13 +21,9 @@ class Distance:
         return self
 
     def __mul__(self, other):
-        if isinstance(other, Distance):
-            return Distance(self.km * other.km)
         return Distance(self.km * other)
 
     def __truediv__(self, other):
-        if isinstance(other, Distance):
-            return Distance(round(self.km / other.km, 2))
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other):
@@ -46,14 +42,10 @@ class Distance:
         return self.km == other
 
     def __le__(self, other):
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        return self.km <= other
+        return self.__lt__(other) or self.__eq__(other)
 
     def __ge__(self, other):
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        return self.km >= other
+        return self.__gt__(other) or self.__eq__(other)
 
     def __len_(self):
         return self.km
