5eacfa521730093ce9318b1a5ede9479bbf323c9
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-09-23 00:49:14 +0300

    add staticmethod

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 class Distance:
-    # Write your code here
     def __init__(self, km: float):
         self.km = km
 
@@ -10,52 +9,39 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other):
-        if not isinstance(other, Distance):
-            if type(other) is int or type(other) is float:
-                return Distance(km=self.km + other)
-            raise TypeError(f"unsupported operand type(s) for + :"
-                            f"'Distance' and {type(other)}")
-        return Distance(km=self.km + other.km)
+        return Distance(km=self.km + self.check_argument(other))
 
     def __iadd__(self, other):
-        if type(other) is Distance:
-            self.km += other.km
-            return self
-        elif type(other) is int or type(other) is float:
-            self.km += other
-            return self
-        else:
-            raise TypeError(f"{type(other)} is error type."
-                            f" Need type Distance, int or float.")
+        self.km += self.check_argument(other)
+        return self
 
     def __mul__(self, other):
-        if type(other) is int or type(other) is float:
-            return Distance(self.km * other)
-        else:
-            raise TypeError(f"{type(other)} is error type."
-                            f" Need type int or float.")
+        return Distance(self.km * self.check_argument(other))
 
     def __truediv__(self, other):
-        if type(other) is int or type(other) is float:
-            return Distance(km=round(self.km / other, 2))
-        else:
-            raise TypeError(f"{type(other)} is error type."
-                            f" Need type int or float.")
+        return Distance(km=round(self.km / self.check_argument(other), 2))
 
     def __lt__(self, other):
-        return self.km < other
+        return self.km < self.check_argument(other)
 
     def __gt__(self, other):
-        return self.km > other
+        return self.km > self.check_argument(other)
 
     def __eq__(self, other):
-        return self.km == other
+        return self.km == self.check_argument(other)
 
     def __le__(self, other):
-        return self.km <= other
+        return self.km <= self.check_argument(other)
 
     def __ge__(self, other):
-        return self.km >= other
+        return self.km >= self.check_argument(other)
 
     def __len__(self):
         return self.km
+
+    @staticmethod
+    def check_argument(other):
+        if type(other) is Distance:
+            return other.km
+        elif type(other) is int or type(other) is float:
+            return other
