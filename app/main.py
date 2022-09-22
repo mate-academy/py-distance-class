8b552867f45757cceca77ac85a00779a21dc3e64
Mowka7 <98344661+Mowka7@users.noreply.github.com>
2022-09-22 10:55:14 +0100

    Solution2

--- app/main.py
+++ app/main.py
@@ -45,10 +45,10 @@ class Distance:
         return self.km == other
 
     def __le__(self, other) -> bool:
-        return not self.__gt__(other)
+        return not self > other
 
     def __ge__(self, other) -> bool:
-        return not self.__lt__(other)
+        return not self < other
 
     def __len__(self) -> int:
         return self.km
