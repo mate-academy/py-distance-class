205bd483bc42fb3d23f6df6fee2b6d7b5c48b55c
Edik Marushchak <e.moscow138@gmail.com>
2022-09-22 12:14:31 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,41 +2,41 @@ class Distance:
     def __init__(self, km: int):
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Distance: {self.km} kilometers.'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Distance(km={self.km})'
 
-    def __add__(self, other):
+    def __add__(self, other) -> object:
         return Distance(self.km + self.get_other_km(other))
 
-    def __iadd__(self, other):
+    def __iadd__(self, other) -> object:
         self.km += self.get_other_km(other)
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> object:
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other) -> object:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         return self.km < self.get_other_km(other)
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
         return self.km > self.get_other_km(other)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.km == self.get_other_km(other)
 
-    def __le__(self, other):
+    def __le__(self, other) -> bool:
         return self.km <= self.get_other_km(other)
 
-    def __ge__(self, other):
+    def __ge__(self, other) -> bool:
         return self.km >= self.get_other_km(other)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.km
 
     @staticmethod
