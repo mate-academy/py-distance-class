e0e340e3eb14b66fc5edba518befe7e510b6d33f
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-09-23 11:56:55 +0300

    other without check_argument() and type annotations were edded

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: int | float) -> None:
         self.km = km
@@ -8,39 +11,39 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other) -> Distance:
         return Distance(km=self.km + self.check_argument(other))
 
-    def __iadd__(self, other):
+    def __iadd__(self, other) -> Distance:
         self.km += self.check_argument(other)
         return self
 
-    def __mul__(self, other):
-        return Distance(self.km * self.check_argument(other))
+    def __mul__(self, other) -> Distance:
+        return Distance(self.km * other)
 
-    def __truediv__(self, other):
-        return Distance(km=round(self.km / self.check_argument(other), 2))
+    def __truediv__(self, other) -> Distance:
+        return Distance(km=round(self.km / other, 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         return self.km < self.check_argument(other)
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
         return self.km > self.check_argument(other)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.km == self.check_argument(other)
 
-    def __le__(self, other):
+    def __le__(self, other) -> bool:
         return self.km <= self.check_argument(other)
 
-    def __ge__(self, other):
+    def __ge__(self, other) -> bool:
         return self.km >= self.check_argument(other)
 
-    def __len__(self):
+    def __len__(self) -> int | float:
         return self.km
 
     @staticmethod
-    def check_argument(other):
+    def check_argument(other) -> int | float:
         if type(other) is Distance:
             return other.km
         elif type(other) is int or type(other) is float:
