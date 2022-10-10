91ec0753d32aee86264c90cf1bb973baa97a597a
Dede <specialized8393@gmail.com>
2022-10-10 14:21:59 +0300

    Fix_Solution

--- app/main.py
+++ app/main.py
@@ -16,18 +16,16 @@ class Distance:
             return Distance(
                 km=self.km + other.km
             )
-        else:
-            return Distance(
-                km=self.km + other
-            )
+        return Distance(
+            km=self.km + other
+        )
 
     def __iadd__(self, other: Distance) -> Distance:
         if isinstance(other, Distance):
             self.km = self.km + other.km
             return self
-        else:
-            self.km = self.km + other
-            return self
+        self.km = self.km + other
+        return self
 
     def __mul__(self, other: Distance) -> Distance:
         return Distance(
@@ -42,32 +40,27 @@ class Distance:
     def __lt__(self, other: Distance) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: Distance) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            return self.km > other
+        return self.km > other
 
     def __eq__(self, other: Distance) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            return self.km == other
+        return self.km == other
 
     def __le__(self, other: Distance) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        else:
-            return self.km <= other
+        return self.km <= other
 
     def __ge__(self, other: Distance) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        else:
-            return self.km >= other
+        return self.km >= other
 
     def __len__(self) -> int:
         return self.km
