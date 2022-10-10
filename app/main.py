0265472430e32c9d7b68a4cbcb3c2a1b4b650df5
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-10 13:28:31 +0300

    Solution_v2

--- app/main.py
+++ app/main.py
@@ -9,79 +9,42 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        return Distance(self.km + second)
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return Distance(self.km + other_distance)
 
     def __iadd__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        self.km += second
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        self.km += other_distance
 
         return self
 
     def __mul__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        return Distance(self.km * second)
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return Distance(self.km * other_distance)
 
     def __truediv__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        return Distance(round(self.km / second, 2))
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return Distance(round(self.km / other_distance, 2))
 
     def __lt__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        return self.km < second
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return self.km < other_distance
 
     def __gt__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        return self.km > second
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return self.km > other_distance
 
     def __eq__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        return self.km == second
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return self.km == other_distance
 
     def __le__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        return self.km <= second
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return self.km <= other_distance
 
     def __ge__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        return self.km >= second
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return self.km >= other_distance
 
     def __len__(self) -> object:
         return len(self.km)
-
-
-# __init__
-distance = Distance(20)  # distance.km == 20
-
-# __str__
-# distance = Distance(20)
-# print(distance)  # "Distance: 20 kilometers."
-
-# __repr__
-# distance = Distance(20)
-# repr(distance)  # "Distance(km=20)"
-
-# __add__
-# distance1 = Distance(20)
-# distance2 = Distance(30)
-# distance3 = distance1 + distance2
-# print(distance3)
-#
-# print(isinstance(distance3, Distance))  # == True
-# distance3.km == 50
-
-# distance1 = Distance(20)
-# distance2 = distance1 + 10
-
-# isinstance(distance2, Distance) == True
-# distance2.km == 30
-# both variants ^ are possible
-
-# __iadd__
-distance1 = Distance(20)
-distance2 = Distance(30)
-distance1 += distance2  # distance1.km == 50
-print(distance1)
-distance = Distance(20)
-distance += 30
-print(distance)  # distance.km == 50
