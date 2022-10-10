d1a0a03c76d35196536793dc9d207434f7147dd1
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-10 13:28:31 +0300

    Solution_v2

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ class Distance:
         return Distance(self.km + second)
 
     def __iadd__(self, other: (int, float)) -> object:
-        second = other if isinstance(other, (int, float)) else other.km
-        self.km += second
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        self.km += other_distance
 
         return self
 
@@ -48,40 +48,3 @@ class Distance:
 
     def __len__(self) -> object:
         return len(self.km)
-
-
-# __init__
-distance = Distance(20)  # distance.km == 20
-
-# __str__
-# distance = Distance(20)
-# print(distance)  # "Distance: 20 kilometers."
-
-# __repr__
-# distance = Distance(20)
-# repr(distance)  # "Distance(km=20)"
-
-# __add__
-# distance1 = Distance(20)
-# distance2 = Distance(30)
-# distance3 = distance1 + distance2
-# print(distance3)
-#
-# print(isinstance(distance3, Distance))  # == True
-# distance3.km == 50
-
-# distance1 = Distance(20)
-# distance2 = distance1 + 10
-
-# isinstance(distance2, Distance) == True
-# distance2.km == 30
-# both variants ^ are possible
-
-# __iadd__
-distance1 = Distance(20)
-distance2 = Distance(30)
-distance1 += distance2  # distance1.km == 50
-print(distance1)
-distance = Distance(20)
-distance += 30
-print(distance)  # distance.km == 50
