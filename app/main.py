0dcca866cd56777e066fa6a26e872cc5cbd75430
Paul Maslov <maslovp67@gmail.com>
2022-10-11 16:49:08 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,62 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: object) -> Distance:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return Distance(km=self.km + other.km)
+
+    def __iadd__(self, other: object) -> Distance:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        self.km += other.km
+        return self
+
+    def __mul__(self, other: object) -> Distance:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return Distance(km=self.km * other.km)
+
+    def __truediv__(self, other: object) -> Distance:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return Distance(km=round(self.km / other.km, 2))
+
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km < other.km
+
+    def __gt__(self, other: object) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km > other.km
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km == other.km
+
+    def __le__(self, other: object) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km <= other.km
+
+    def __ge__(self, other: object) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km >= other.km
+
+    def __len__(self) -> float:
+        return self.km
