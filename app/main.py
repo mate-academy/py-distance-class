371f69b2ac6c141ebf2763a1e1ba7db48300144d
Artem Motrunich <motrunich2013@gmail.com>
2022-10-13 11:25:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 class Distance:
-    text_of_error_massage = "The operand must be of type int o float " \
-                            "or an object of class Distance"
-    error_massage = ArithmeticError(text_of_error_massage)
 
     def __init__(self, km: (int, float)) -> None:
         self.km = km
@@ -13,65 +10,47 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: (int, float)) -> object:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
 
         return Distance(self.km + number)
 
     def __iadd__(self, other: (int, float)) -> object:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
         self.km += number
 
         return self
 
     def __mul__(self, other: (int, float)) -> object:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
 
         return Distance(self.km * number)
 
     def __truediv__(self, other: (int, float)) -> object:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
 
         return Distance(round(self.km / number, 2))
 
     def __lt__(self, other: (int, float)) -> bool:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
 
         return self.km < number
 
     def __gt__(self, other: (int, float)) -> bool:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
 
         return self.km > number
 
     def __eq__(self, other: (int, float)) -> bool:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
 
         return self.km == number
 
     def __le__(self, other: (int, float)) -> bool:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
 
         return self.km <= number
 
     def __ge__(self, other: (int, float)) -> bool:
-        if not isinstance(other, (int, float, Distance)):
-            raise self.error_massage
         number = other if isinstance(other, (int, float)) else other.km
 
         return self.km >= number
