c89951b034b64ddcf5774163d1bf490584427357
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2022-10-13 16:16:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -11,11 +11,10 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance) -> Distance:
+    def __add__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        else:
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
     def __iadd__(self, other: Distance) -> Distance:
         if isinstance(other, Distance):
