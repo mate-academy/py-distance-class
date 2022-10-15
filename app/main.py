edc754d4e1c26fc318091ec48cb1ac8a583e5520
Dmitriy Kuzmin <kuzminds@hotmail.com>
2022-10-15 16:52:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,53 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: float) -> float:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance) -> Distance:
+        if not isinstance(other, Distance):
+            distance = Distance(self.km + other)
+        else:
+            distance = Distance(self.km + other.km)
+        return distance
+
+    def __iadd__(self, other: Distance) -> Distance:
+        if not isinstance(other, Distance):
+            self.km += other
+        else:
+            self.km += other.km
+        return self
+
+    def __mul__(self, num: int) -> Distance:
+        distance = Distance(self.km * num)
+        return distance
+
+    def __truediv__(self, num: int) -> Distance:
+        distance = Distance(round(self.km / num, 2))
+        return distance
+
+    def __lt__(self, num: int) -> bool:
+        return self.km < num
+
+    def __gt__(self, num: int) -> bool:
+        return self.km > num
+
+    def __eq__(self, num: int) -> bool:
+        return self.km == num
+
+    def __le__(self, num: int) -> bool:
+        return self.km <= num
+
+    def __ge__(self, num: int) -> bool:
+        return self.km >= num
+
+    def __len__(self) -> int:
+        return self.km
