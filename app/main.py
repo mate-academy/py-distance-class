ca49dd7ed0a6ad085791525999f3236cff99390a
Oleksandr Kolomiiets <jumanje82@gmail.com>
2022-10-21 13:12:59 +0300

    'Solution_#2'

--- app/main.py
+++ app/main.py
@@ -1,70 +1,63 @@
+from __future__ import annotations
+
+
 class Distance:
-    def __init__(self, km):
+    def __init__(self: int, km: int) -> None:
         self.km = km
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __add__(self, other):
-        if type(other) == int or type(other) == float:
+    def __add__(self, other: (int, float)) -> Distance:
+        if isinstance(other, (float, int)):
             return Distance(self.km + other)
-        else:
-            return Distance(self.km + other.km)
+        return Distance(self.km + other.km)
 
-    def __mul__(self, other):
-        if type(other) == int or type(other) == float:
+    def __mul__(self, other: (int, float)) -> Distance:
+        if isinstance(other, (float, int)):
             return Distance(self.km * other)
-        else:
-            return Distance(self.km * other.km)
+        return Distance(self.km * other.km)
 
-    def __truediv__(self, other):
-        if type(other) == int or type(other) == float:
+    def __truediv__(self, other: (int, float)) -> Distance:
+        if isinstance(other, (float, int)):
             return Distance(round((self.km / other), 2))
-        else:
-            return Distance(round((self.km / other.km), 2))
+        return Distance(round((self.km / other.km), 2))
 
-    def __len__(self):
-        return Distance(
-            km=self.km
-        )
+    def __len__(self) -> Distance:
+        return self
 
-    def __iadd__(self, other):
-        if type(other) == int or type(other) == float:
+    def __iadd__(self, other: (int, float)) -> Distance:
+        if isinstance(other, (float, int)):
             self.km += other
             return self
         else:
             self.km += other.km
             return self
 
-    def __eq__(self, other):
-        if type(other) == int or type(other) == float:
+    def __eq__(self, other: (int, float)) -> bool:
+        if isinstance(other, (float, int)):
             return self.km == other
-        else:
-            return self.km == other.km
+        return self.km == other
 
-    def __gt__(self, other):
-        if type(other) == int or type(other) == float:
+    def __gt__(self, other: (int, float)) -> bool:
+        if isinstance(other, (float, int)):
             return self.km > other
-        else:
-            return self.km > other.km
+        return self.km > other
 
-    def __lt__(self, other):
-        if type(other) == int or type(other) == float:
+    def __lt__(self, other: (int, float)) -> bool:
+        if isinstance(other, (float, int)):
             return self.km < other
-        else:
-            return self.km < other.km
+        return self.km < other
 
-    def __ge__(self, other):
-        if type(other) == int or type(other) == float:
+    def __ge__(self, other: (int, float)) -> bool:
+        if isinstance(other, (float, int)):
             return self.km >= other
-        else:
-            return self.km >= other.km
+        return self.km >= other
 
-    def __le__(self, other):
-        if type(other) == int or type(other) == float:
+    def __le__(self, other: (int, float)) -> bool:
+        if isinstance(other, (float, int)):
             return self.km <= other
-        else:
-            return self.km <= other.km
+        return self.km <= other
