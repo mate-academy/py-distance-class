c0ab3fb98d3a56ddc04d47cd5db11ef8026b8d57
Dmitriy Kuzmin <kuzminds@hotmail.com>
2022-10-29 21:58:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Union
 
 
 class Distance:
@@ -12,12 +13,10 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance) -> int or float or Distance:
+    def __add__(self, other: Distance) -> Union[int, float, Distance]:
         if not isinstance(other, Distance):
-            distance = Distance(self.km + other)
-        else:
-            distance = Distance(self.km + other.km)
-        return distance
+            return Distance(self.km + other)
+        return Distance(self.km + other.km)
 
     def __iadd__(self, other: Distance) -> Distance:
         if not isinstance(other, Distance):
@@ -27,29 +26,34 @@ class Distance:
         return self
 
     def __mul__(self, num: int) -> Distance:
-        distance = Distance(self.km * num)
-        return distance
+        return Distance(self.km * num)
 
     def __truediv__(self, num: int) -> Distance:
-        distance = Distance(round(self.km / num, 2))
-        return distance
+        return Distance(round(self.km / num, 2))
 
-    def __lt__(self, num: int or float or Distance) -> bool:
+    def __lt__(self, num: Union[int, float, Distance]) -> bool:
         if isinstance(num, Distance):
             return self.km < num.km
-        else:
-            return self.km < num
+        return self.km < num
 
-    def __gt__(self, num: int) -> bool:
+    def __gt__(self, num: Union[int, float, Distance]) -> bool:
+        if isinstance(num, Distance):
+            return self.km > num.km
         return self.km > num
 
-    def __eq__(self, num: int) -> bool:
+    def __eq__(self, num: Union[int, float, Distance]) -> bool:
+        if isinstance(num, Distance):
+            return self.km == num.km
         return self.km == num
 
-    def __le__(self, num: int) -> bool:
+    def __le__(self, num: Union[int, float, Distance]) -> bool:
+        if isinstance(num, Distance):
+            return self.km <= num.km
         return self.km <= num
 
-    def __ge__(self, num: int) -> bool:
+    def __ge__(self, num: Union[int, float, Distance]) -> bool:
+        if isinstance(num, Distance):
+            return self.km >= num.km
         return self.km >= num
 
     def __len__(self) -> float or int:
