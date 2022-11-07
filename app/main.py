adecdd2ff230203268a0e4d067ae4a56c7b897d5
Tatasha <NatashaLeonidovna319@gmail.com>
2022-11-07 23:25:08 +0200

    Changes type annotations

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[int, float]) -> Distance:
+    def __add__(self, other: Union[int, float, Distance]) -> Distance:
         if type(other) == int or type(other) == float:
             return Distance(
                 km=self.km + other
@@ -28,30 +28,30 @@ class Distance:
             self.km += other.km
         return self
 
-    def __mul__(self, other: Union[int, float]) -> Distance:
+    def __mul__(self, other: Union[int, float, Distance]) -> Distance:
         return Distance(
             km=self.km * other
         )
 
-    def __truediv__(self, other: Union[int, float]) -> Distance:
+    def __truediv__(self, other: Union[int, float, Distance]) -> Distance:
         return Distance(
             km=round(self.km / other, 2)
         )
 
-    def __lt__(self, other: Union[int, float]) -> bool:
+    def __lt__(self, other: Union[int, float, Distance]) -> bool:
         return self.km < other
 
-    def __gt__(self, other: Union[int, float]) -> bool:
+    def __gt__(self, other: Union[int, float, Distance]) -> bool:
         return self.km > other
 
-    def __eq__(self, other: Union[int, float]) -> bool:
+    def __eq__(self, other: Union[int, float, Distance]) -> bool:
         return self.km == other
 
-    def __le__(self, other: Union[int, float]) -> bool:
+    def __le__(self, other: Union[int, float, Distance]) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: Union[int, float]) -> bool:
+    def __ge__(self, other: Union[int, float, Distance]) -> bool:
         return self.km >= other
 
-    def __len__(self) -> Union[int, float]:
+    def __len__(self) -> Union[int, float, Distance]:
         return len(self.km)
