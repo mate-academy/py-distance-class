77f479f18e08adad49b8bffb8d6bf3dd5f99c90b
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-11-07 11:59:08 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,66 @@
+from typing import Union
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: Union[int, float]) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Union[object, int, float]) -> object:
+
+        if isinstance(other, Union[float, int]):
+            result = self.__class__(self.km + other)
+
+        else:
+            result = self.__class__(self.km + other.km)
+
+        return result
+
+    def __iadd__(self, other: Union[object, int, float]) -> object:
+
+        if isinstance(other, Union[int, float]):
+            setattr(self, "km", self.km + other)
+
+        else:
+            setattr(self, "km", self.km + other.km)
+
+        return self
+
+    def __mul__(self, other: Union[int, float]) -> object:
+        return self.__class__(self.km * other)
+
+    def __truediv__(self, other: Union[int, float]) -> object:
+        return self.__class__(round(self.km / other, 2))
+
+    def __lt__(self, other: Union[object, int, float]) -> bool:
+        if isinstance(other, Union[int, float]):
+            return self.km < other
+        return self.km < other.km
+
+    def __gt__(self, other: Union[object, int, float]) -> bool:
+        if isinstance(other, Union[int, float]):
+            return self.km > other
+        return self.km > other.km
+
+    def __eq__(self, other: Union[object, int, float]) -> bool:
+        if isinstance(other, Union[int, float]):
+            return self.km == other
+        return self.km == other.km
+
+    def __le__(self, other: Union[object, int, float]) -> bool:
+        if isinstance(other, Union[int, float]):
+            return self.km <= other
+        return self.km <= other.km
+
+    def __ge__(self, other: Union[object, int, float]) -> bool:
+        if isinstance(other, Union[int, float]):
+            return self.km >= other
+        return self.km >= other.km
+
+    def __len__(self) -> Union[float, int]:
+        return self.km
