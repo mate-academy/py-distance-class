c2f862cd91c024c576db868a092f2b2fc0dfc0cb
Dmytro Prokofiev <forbuffa@gmail.com>
2022-11-09 13:55:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,68 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> callable:
+        self.km = km
+
+    def __str__(self) -> callable:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> callable:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: int | float) -> callable:
+        if isinstance(other, Distance):
+            return Distance(km=self.km + other.km)
+        else:
+            return Distance(km=self.km + other)
+
+    def __iadd__(self, other: int | float) -> callable:
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        else:
+            self.km += other
+            return self
+
+    def __mul__(self, other: int | float) -> callable:
+        if isinstance(other, int | float):
+            return Distance(km=self.km * other)
+        else:
+            raise TypeError("TypeError()")
+
+    def __truediv__(self, other: int | float) -> callable:
+        if isinstance(other, int | float):
+            return Distance(km=round(self.km / other, 2))
+        else:
+            raise TypeError("TypeError()")
+
+    def __lt__(self, other: int | float) -> callable:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        else:
+            return self.km < other
+
+    def __gt__(self, other: int | float) -> callable:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        else:
+            return self.km > other
+
+    def __eq__(self, other: int | float) -> callable:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        else:
+            return self.km == other
+
+    def __le__(self, other: int | float) -> callable:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        else:
+            return self.km <= other
+
+    def __ge__(self, other: int | float) -> callable:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        else:
+            return self.km >= other
+
+    def __len__(self) -> callable:
+        return Distance(len(self.km))
