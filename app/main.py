4dfbed79277fb84a5413966df1f84c3491c867dd
Alex Isaev <lesisv@gmail.com>
2022-11-11 14:17:14 +0200

    Solution fixed

--- app/main.py
+++ app/main.py
@@ -1,46 +1,49 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: Distance) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
         else:
             return Distance(self.km + other.km)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Distance) -> Distance:
         if isinstance(other, (int, float)):
             self.km = self.km + other
         else:
             self.km = self.km + other.km
         return self
 
-    def __mul__(self, other: int):
+    def __mul__(self, other: int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int) -> float:
+    def __truediv__(self, other: int) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other: Distance) -> bool:
         return self.km < other
 
-    def __gt__(self, other):
+    def __gt__(self, other: Distance) -> bool:
         return self.km > other
 
-    def __eq__(self, other):
+    def __eq__(self, other: Distance) -> bool:
         return self.km == other
 
-    def __le__(self, other):
+    def __le__(self, other: Distance) -> bool:
         return self.km <= other
 
-    def __ge__(self, other):
+    def __ge__(self, other: Distance) -> bool:
         return self.km >= other
 
-    def __len__(self):
+    def __len__(self) -> float:
         return self.km
