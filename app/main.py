ef8f4f89a3e8a76c25a7018b73139cd4b0e14071
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2022-11-14 15:14:45 +0200

    'Solution#100500'

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 from __future__ import annotations
+from typing import Union
 
 
 class Distance:
 
-    def __init__(self, km: int or float) -> callable:
+    def __init__(self, km: int or float) -> None:
         self.km = km
 
-    def __str__(self) -> callable:
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self) -> callable:
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int or float) -> Distance(int or float):
+    def __add__(self, other: int or float) -> Union[Distance, int, float]:
         if type(other) == Distance:
             return Distance(
                 self.km + other.km
@@ -21,14 +22,14 @@ class Distance:
             self.km + other
         )
 
-    def __iadd__(self, other: int or float) -> Distance(int or float):
+    def __iadd__(self, other: int or float) -> Union[Distance, int, float]:
         if type(other) == Distance:
             self.km += other.km
             return self
         self.km += other
         return self
 
-    def __mul__(self, other: int or float) -> Distance(int or float):
+    def __mul__(self, other: int or float) -> Union[Distance, int, float]:
         return Distance(
             self.km * other
         )
