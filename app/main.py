a660666e53b7ddd66fb6914e96cd746c9d339872
Tetiana Kozakova <kleotan901@gmail.com>
2022-11-18 17:26:03 +0100

    annotation changed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
 
     def __init__(self, km: [int, float]) -> None:
@@ -12,38 +15,38 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: [int, float]) -> [int, float]:
+    def __add__(self, other: [int, float, Distance]) -> [int, float]:
         if isinstance(other, (int, float)):
             return Distance(km=self.km + other)
         else:
             return Distance(km=self.km + other.km)
 
-    def __iadd__(self, other: [int, float]) -> [int, float]:
+    def __iadd__(self, other: [int, float, Distance]) -> [int, float]:
         if isinstance(other, (int, float)):
             self.km += other
         else:
             self.km += other.km
         return self
 
-    def __mul__(self, other: [int, float]) -> [int, float]:
+    def __mul__(self, other: [int, float, Distance]) -> [int, float]:
         return Distance(km=self.km * other)
 
-    def __truediv__(self, other: [int, float]) -> [int, float]:
+    def __truediv__(self, other: [int, float, Distance]) -> [int, float]:
         return Distance(km=round(self.km / other, 2))
 
-    def __lt__(self, other: [int, float]) -> bool:
+    def __lt__(self, other: [int, float, Distance]) -> bool:
         return self.km < other
 
-    def __gt__(self, other: [int, float]) -> bool:
+    def __gt__(self, other: [int, float, Distance]) -> bool:
         return self.km > other
 
-    def __eq__(self, other: [int, float]) -> bool:
+    def __eq__(self, other: [int, float, Distance]) -> bool:
         return self.km == other
 
-    def __le__(self, other: [int, float]) -> bool:
+    def __le__(self, other: [int, float, Distance]) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: [int, float]) -> bool:
+    def __ge__(self, other: [int, float, Distance]) -> bool:
         return self.km >= other
 
     def __len__(self) -> [int, float]:
