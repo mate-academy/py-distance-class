f006b6e376e6b4015277848d18cc580c739541c5
Anna Kabatova <kabatova.anna@ukr.net>
2022-12-19 18:44:01 +0200

    3rd try solution

--- app/main.py
+++ app/main.py
@@ -35,16 +35,26 @@ class Distance:
         raise TypeError
 
     def __lt__(self, other: Distance | int | float) -> None:
+        if isinstance(other, Distance):
+            return self.km < other.km
         return self.km < other
 
     def __gt__(self, other: Distance | int | float) -> None:
+        if isinstance(other, Distance):
+            return self.km > other.km
         return self.km > other
 
     def __eq__(self, other: Distance | int | float) -> None:
+        if isinstance(other, Distance):
+            return self.km == other.km
         return self.km == other
 
     def __le__(self, other: Distance | int | float) -> None:
+        if isinstance(other, Distance):
+            return self.km <= other.km
         return self.km <= other
 
     def __ge__(self, other: Distance | int | float) -> None:
+        if isinstance(other, Distance):
+            return self.km >= other.km
         return self.km >= other
