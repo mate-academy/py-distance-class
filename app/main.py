e54506cbd14fcf6ee42ec320b8033d29e7128a6a
Alina Boichenko <ilub1108@gmail.com>
2022-12-24 00:20:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -20,9 +20,8 @@ class Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
-        else:
-            self.km += other
-            return self
+        self.km += other
+        return self
 
     def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
@@ -31,28 +30,18 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km < other.km
         return self.km < other
 
     def __gt__(self, other: int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km > other.km
         return self.km > other
 
     def __eq__(self, other: int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km == other.km
         return self.km == other
 
     def __le__(self, other: int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
         return self.km <= other
 
     def __ge__(self, other: int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
         return self.km >= other
 
     def __len__(self) -> Distance:
