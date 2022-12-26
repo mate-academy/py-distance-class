f1ee12f19f3f210cdc17355ad92b9350d2eaa2ea
Sashkoooo <o.denyseko@hotmail.com>
2022-12-27 01:05:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,30 @@
+from __future__ import annotations
+from __future__ import division
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance | int) -> Distance:
+        return Distance(km=self.km + other)
+
+    def __iadd__(self, other: Distance | int) -> Distance:
+        if isinstance(other, int):
+            return Distance(km=self.km + other)
+        return Distance(km=self.km + other.km)
+
+    def __mul__(self, other: int) -> Distance:
+        return Distance(km=self.km * other)
+
+    def __round__(self, n=2):
+        return round(self.km, n)
+
+    def __truediv__(self, other: int) -> float:
+        return round(Distance(km=self.km / other))
