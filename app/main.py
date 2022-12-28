627414eb8fe41a760ee7a83d11b0c46349a5c0d6
Vasyl Borysiuk <jomajok7777@gmail.com>
2022-12-28 23:37:12 +0200

    raise deleted and name of variable changed

--- app/main.py
+++ app/main.py
@@ -12,13 +12,10 @@ class Distance:
         return f"Distance(km={self.km})"
 
     @staticmethod
-    def type_check(elem: Distance | int | float) -> int | float | None:
-        if not isinstance(elem, Distance | int | float):
-            raise TypeError("Other object should be Distance, "
-                            "int or float type!")
-        if isinstance(elem, Distance):
-            elem = elem.km
-        return elem
+    def type_check(other: Distance | int | float) -> int | float:
+        if isinstance(other, Distance):
+            other = other.km
+        return other
 
     def __add__(self, other: Distance | int | float) -> Distance:
         other = Distance.type_check(other)
@@ -30,14 +27,10 @@ class Distance:
         return self
 
     def __mul__(self, other: int | float) -> Distance:
-        if not isinstance(other, int | float):
-            raise TypeError("Other object should be int or float type!")
         self.km = self.km * other
         return self
 
     def __truediv__(self, other: int | float) -> Distance:
-        if not isinstance(other, int | float):
-            raise TypeError("Other object should be int or float type!")
         self.km = round(self.km / other, 2)
         return self
 
