0be3be395f925816e7ed39945eeddb88f6e32bb7
Kristina <tina.vasylenko@gmail.com>
2023-01-10 14:47:28 +0200

    Corrected solution

--- app/main.py
+++ app/main.py
@@ -5,30 +5,30 @@ class Distance:
     def __init__(self, km: int | float) -> None:
         self.km = km
 
-    def __str__(self) -> Any:
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self) -> Any:
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float) -> Any:
+    def __add__(self, other: int | float | object) -> object:
         if isinstance(other, Distance):
             return Distance(km=self.km + other.km)
         return Distance(km=self.km + other)
 
-    def __iadd__(self, other: int | float) -> Any:
+    def __iadd__(self, other: int | float | object) -> Any:
         if isinstance(other, Distance):
             self.km += other.km
             return self
         self.km += other
         return self
 
-    def __mul__(self, other: int | float) -> Any:
+    def __mul__(self, other: int | float) -> object:
         return Distance(
             km=self.km * other
         )
 
-    def __truediv__(self, other: int | float) -> Any:
+    def __truediv__(self, other: int | float) -> object:
         return Distance(km=round(self.km / other, 2))
 
     def __lt__(self, other: int | float) -> bool:
