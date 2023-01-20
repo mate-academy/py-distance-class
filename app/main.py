a9c467982af03d0c7c08fc0ac7b999683153ec0c
Zoriana Dvulit <zdvulit@ukr.net>
2023-01-20 19:37:03 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,61 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, distance: int) -> Distance:
+        distance3 = Distance(distance + self.km)
+        return distance3
+
+    def __iadd__(self, distance: int) -> Distance:
+        self.km = Distance(distance + self.km)
+        return self
+
+    def __mul__(self, number: float) -> Distance:
+        self.km = self.km * number
+        return self
+
+    def __truediv__(self, number: float) -> Distance:
+        self.km = round(self.km / number, 2)
+        return self
+
+    def __lt__(self, distance: int) -> bool:
+        if self.km < distance:
+            return True
+        else:
+            return False
+
+    def __gt__(self, distance: int) -> bool:
+        if self.km > distance:
+            return True
+        else:
+            return False
+
+    def __eq__(self, distance: int) -> bool:
+        if self.km == distance:
+            return True
+        elif self.km != distance:
+            return False
+
+    def __le__(self, distance: int) -> bool:
+        if self.km <= distance:
+            return True
+        else:
+            return False
+
+    def __ge__(self, distance: int) -> bool:
+        if self.km >= distance:
+            return True
+        else:
+            return False
+
+    def __round__(self, end: int) -> float:
+        return round(self.km, end)
