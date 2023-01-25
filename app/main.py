cffaf0755099b5f29218e74972267757100d5ca6
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-01-25 19:08:16 +0200

    Update solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import operator
 from typing import Union
 
 
@@ -13,45 +12,35 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[int, Distance]) -> Distance:
+    def __add__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: Union[int, Distance]) -> Distance:
+    def __iadd__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: int) -> Distance:
+    def __mul__(self, other: Union[int, float]) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int) -> Distance:
+    def __truediv__(self, other: Union[int, float]) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def _validate(
-            self,
-            other: Union[int, Distance],
-            operator: operator
-    ) -> operator:
-        if isinstance(other, Distance):
-            return operator(self.km, other.km)
-        else:
-            return operator(self.km, other)
+    def __lt__(self, other: Union[int, float, Distance]) -> bool:
+        return self.km < other
 
-    def __lt__(self, other: Union[int, Distance]) -> bool:
-        return self._validate(other, operator.lt)
+    def __gt__(self, other: Union[int, float, Distance]) -> bool:
+        return self.km > other
 
-    def __gt__(self, other: Union[int, Distance]) -> bool:
-        return self._validate(other, operator.gt)
+    def __eq__(self, other: Union[int, float, Distance]) -> bool:
+        return self.km == other
 
-    def __eq__(self, other: Union[int, Distance]) -> bool:
-        return self._validate(other, operator.eq)
+    def __le__(self, other: Union[int, float, Distance]) -> bool:
+        return self.km <= other
 
-    def __le__(self, other: Union[int, Distance]) -> bool:
-        return self._validate(other, operator.le)
-
-    def __ge__(self, other: Union[int, Distance]) -> bool:
-        return self._validate(other, operator.ge)
+    def __ge__(self, other: Union[int, float, Distance]) -> bool:
+        return self.km >= other
