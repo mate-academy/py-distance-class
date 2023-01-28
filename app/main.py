80e5e5bdb6ad93fe5eea627f7dbb5809bee3c7ad
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-01-28 18:57:13 +0200

    Change

--- app/main.py
+++ app/main.py
@@ -24,15 +24,11 @@ class Distance:
         self.km += other.km
         return self
 
-    def __mul__(self, other: Distance | int | float) -> Distance:
-        if self is not Distance:
-            return Distance(self.km * other)
-        return Distance(self.km * other.km)
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
 
-    def __truediv__(self, other: Distance | int | float) -> Distance:
-        if self is not Distance:
-            return Distance(round(self.km / other, 2))
-        return Distance(round(self.km / other.km, 2))
+    def __truediv__(self, other: int | float) -> Distance:
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < other
