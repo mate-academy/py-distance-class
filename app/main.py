54c13ef4ef7628aa9f78a2692f459b26582ff3bd
Natalie Goriela <natalie.goriela@gmail.com>
2023-01-30 14:30:14 +0200

    'Soluton'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from typing import Any
 
 
 class Distance:
@@ -12,39 +11,39 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Any) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km = self.km + other.km
         else:
             self.km = self.km + other
         return self
 
-    def __iadd__(self, other: Any) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: Any) -> Distance:
+    def __mul__(self, other: Distance | int | float) -> Distance:
         self.km = self.km * other
         return self
 
-    def __truediv__(self, other: Any) -> Distance:
+    def __truediv__(self, other: Distance | int | float) -> Distance:
         self.km = round(self.km / other, 2)
         return self
 
-    def __lt__(self, other: Any) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < other
 
-    def __gt__(self, other: Any) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > other
 
-    def __eq__(self, other: Any) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == other
 
-    def __le__(self, other: Any) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: Any) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= other
