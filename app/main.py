1eb0fdbf0b9c68a7d507d406c6a7da3ab76d4991
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-05 12:02:38 +0200

    add annotations

--- app/main.py
+++ app/main.py
@@ -16,40 +16,40 @@ class Distance:
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: (int, float, Distance)) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: (int, float)) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: (int, float)) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other: (int, float, Distance)) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         return self.km < other
 
-    def __gt__(self, other):
+    def __gt__(self, other: (int, float, Distance)) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         return self.km > other
 
-    def __eq__(self, other):
+    def __eq__(self, other: (int, float, Distance)) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         return self.km == other
 
-    def __le__(self, other):
+    def __le__(self, other: (int, float, Distance)) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         return self.km <= other
 
-    def __ge__(self, other):
+    def __ge__(self, other: (int, float, Distance)) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         return self.km >= other
