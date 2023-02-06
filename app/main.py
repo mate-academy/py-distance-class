035f35ee52a24280a5b7bf1e276519362b31c350
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-06 21:24:00 +0200

    Fixed isinstances

--- app/main.py
+++ app/main.py
@@ -28,40 +28,38 @@ class Distance:
 
         return self
 
-    def __mul__(self, value: int) -> Distance:
-        return Distance(km=self.km * value)
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
 
-    def __truediv__(self, value: int | float) -> Distance:
-        real_distance = round(self.km / value, 2)
-
-        return Distance(km=real_distance)
+    def __truediv__(self, other: int | float) -> Distance:
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | float | int) -> bool:
-        if other is Distance:
+        if isinstance(other, Distance):
             return self.km < other.km
 
         return self.km < other
 
     def __gt__(self, other: Distance | float | int) -> bool:
-        if other is Distance:
+        if isinstance(other, Distance):
             return self.km > other.km
 
         return self.km > other
 
     def __eq__(self, other: Distance | float | int) -> bool:
-        if other is Distance:
+        if isinstance(other, Distance):
             return self.km == other.km
 
         return self.km == other
 
     def __le__(self, other: Distance | float | int) -> bool:
-        if other is Distance:
+        if isinstance(other, Distance):
             return self.km <= other.km
 
         return self.km <= other
 
     def __ge__(self, other: Distance | float | int) -> bool:
-        if other is Distance:
+        if isinstance(other, Distance):
             return self.km >= other.km
 
         return self.km >= other
