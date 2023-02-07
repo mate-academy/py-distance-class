c5290dfa42274b2ca46acf4c769d0ecd54b03591
Max Bilokur <bilmaxim@gmail.com>
2023-02-07 22:14:54 +0200

    revised le and ge methods(but not sure)

--- app/main.py
+++ app/main.py
@@ -55,13 +55,7 @@ class Distance:
             return self.km == other.km
 
     def __le__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, (int, float)):
-            return self.km <= other
-        elif isinstance(other, Distance):
-            return self.km <= other.km
+        return not self.__gt__(other)
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, (int, float)):
-            return self.km >= other
-        elif isinstance(other, Distance):
-            return self.km >= other.km
+        return not self.__lt__(other)
