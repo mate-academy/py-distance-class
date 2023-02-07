8167a5176d98aeef12221d92750dc9cf2dbd3b25
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-02-07 21:30:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,52 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: int | float) -> None:
+        if isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other: int | float) -> None:
+        if isinstance(other, (int, float)):
+            self.km += other
+            return self
+        self.km += other.km
+        return self
+
+    def __mul__(self, number: int | float) -> None:
+        return Distance(self.km * number)
+
+    def __truediv__(self, number: int | float) -> None:
+        return Distance(round(self.km / number, 2))
+
+    def __lt__(self, number: int) -> bool:
+        if self.km < number:
+            return True
+        return False
+
+    def __gt__(self, number: int) -> bool:
+        if self.km > number:
+            return True
+        return False
+
+    def __eq__(self, number: int) -> bool:
+        if self.km == number:
+            return True
+        return False
+
+    def __le__(self, number: int) -> bool:
+        if self.km <= number:
+            return True
+        return False
+
+    def __ge__(self, number: int) -> bool:
+        if self.km >= number:
+            return True
+        return False
