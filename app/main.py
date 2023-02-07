0eb4769bbb23072b16c51ddc5ad8be83eb5b4cbb
Dmytry <donchykd@gmail.com>
2023-02-07 07:09:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,66 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __add__(self, other: int | float | Distance) -> Distance:
+        if type(other) is Distance:
+            return Distance(
+                self.km + other.km
+            )
+        self.km += other
+
+        return self
+
+    def __str__(self) -> str:
+
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+
+        return f"Distance(km={self.km})"
+
+    def __iadd__(self, other: int | float | Distance) -> Distance:
+        if type(other) == Distance:
+            self.km += other.km
+            return self
+
+        self.km += other
+
+        return self
+
+    def __mul__(self, other: int | float) -> Distance:
+        self.km = self.km * other
+
+        return self
+
+    def __truediv__(self, other: int | float | Distance) -> Distance:
+        if type(other) == Distance:
+            Distance(
+                self.km / other.km
+            )
+        self.km = round((self.km / other), 2)
+
+        return self
+
+    def __lt__(self, other: int | float) -> bool:
+
+        return self.km < other
+
+    def __gt__(self, other: int | float) -> bool:
+
+        return self.km > other
+
+    def __eq__(self, other: int | float) -> bool:
+
+        return self.km == other
+
+    def __le__(self, other: int | float) -> bool:
+
+        return self.km <= other
+
+    def __ge__(self, other: int | float) -> bool:
+
+        return self.km >= other
