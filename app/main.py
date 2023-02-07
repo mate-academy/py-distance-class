4c7b060e47c201d40faca060b9ddca65c7d07219
Eugene <eugene.lot77@gmail.com>
2023-02-07 09:56:38 +0200

    Changed types and removed new line

--- app/main.py
+++ app/main.py
@@ -14,7 +14,6 @@ class Distance:
     def __add__(self, other: Distance | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-
         return Distance(self.km + other)
 
     def __iadd__(self, other: Distance | float) -> Distance:
@@ -25,23 +24,23 @@ class Distance:
         self.km += other
         return self
 
-    def __mul__(self, other: float) -> Distance:
+    def __mul__(self, other: Distance | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float) -> Distance:
+    def __truediv__(self, other: Distance | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: float) -> bool:
+    def __lt__(self, other: Distance | float) -> bool:
         return self.km < other
 
-    def __gt__(self, other: float) -> bool:
+    def __gt__(self, other: Distance | float) -> bool:
         return self.km > other
 
-    def __eq__(self, other: float) -> bool:
+    def __eq__(self, other: Distance | float) -> bool:
         return self.km == other
 
-    def __le__(self, other: float) -> bool:
+    def __le__(self, other: Distance | float) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: float) -> bool:
+    def __ge__(self, other: Distance | float) -> bool:
         return self.km >= other
