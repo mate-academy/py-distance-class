ed972667eed065c37b49a37e59009c1cca6eaeb6
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-02-08 20:33:33 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -34,29 +34,20 @@ class Distance:
     def __lt__(self, other: Distance | float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: Distance | float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            return self.km > other
+        return self.km > other
 
     def __eq__(self, other: Distance | float) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            return self.km == other
+        return self.km == other
 
     def __le__(self, other: Distance | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        else:
-            return self.km <= other
+        return not self > other
 
     def __ge__(self, other: Distance | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        else:
-            return self.km >= other
+        return not self < other
