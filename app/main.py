13bf2d8ae51d66806623e5d6b44df24fbaf80553
Max Bilokur <bilmaxim@gmail.com>
2023-02-08 13:26:44 +0200

    removed explicit magic calls

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ class Distance:
             return self.km == other.km
 
     def __le__(self, other: Distance | int | float) -> bool:
-        return not self.__gt__(other)
+        return not self > other
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        return not self.__lt__(other)
+        return not self < other
