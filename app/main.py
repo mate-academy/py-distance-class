43084c09f21980e06ce650bf963c49f00671fc85
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-02-09 12:46:02 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -24,10 +24,12 @@ class Distance:
         return self
 
     def __mul__(self, other: int | float) -> Distance:
-        return Distance(self.km * other)
+        if not isinstance(other, Distance):
+            return Distance(self.km * other)
 
     def __truediv__(self, other: int | float) -> Distance:
-        return Distance(round(self.km / other, 2))
+        if not isinstance(other, Distance):
+            return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
@@ -45,11 +47,7 @@ class Distance:
         return self.km == other
 
     def __le__(self, other: int | float | Distance) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        return self.km <= other
+        return self < other or self == other
 
     def __ge__(self, other: int | float | Distance) -> bool:
-        if isinstance(other, Distance):
-            self.km >= other.km
-        return self.km >= other
+        return self > other or self == other
