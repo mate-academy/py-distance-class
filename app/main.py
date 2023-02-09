2a2f157ada107d218d88d824104c4b4df8db25ea
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-02-09 13:46:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,54 @@
+from typing import Union, Any
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    @staticmethod
+    def verify_data(other: Union[int, float]) -> Union[int, float]:
+        if isinstance(other, Distance):
+            return other.km
+        return other
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Union[int, float, Any]) -> Any:
+        value = self.verify_data(other)
+        return Distance(self.km + value)
+
+    def __iadd__(self, other: Union[int, float, Any]) -> Any:
+        value = self.verify_data(other)
+        self.km = self + value
+        return self
+
+    def __mul__(self, other: Union[int, float]) -> Any:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: Union[int, float]) -> Any:
+        return Distance(round(self.km / other, 2))
+
+    def __eq__(self, other: Union[int, float]) -> bool:
+        value = self.verify_data(other)
+        return self.km == value
+
+    def __lt__(self, other: Union[int, float]) -> bool:
+        value = self.verify_data(other)
+        return self.km < value
+
+    def __gt__(self, other: Union[int, float]) -> bool:
+        value = self.verify_data(other)
+        return self.km > value
+
+    def __le__(self, other: Union[int, float]) -> bool:
+        value = self.verify_data(other)
+        return self.km <= value
+
+    def __ge__(self, other: Union[int, float]) -> bool:
+        value = self.verify_data(other)
+        return self.km >= value
