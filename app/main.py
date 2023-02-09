c70a9c9203292e0fbef90533b0f004808f2deda6
Artem Dubinin <artemdubinin.ad@gmail.com>
2023-02-09 19:55:13 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from typing import Union
 
 
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,7 +12,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[Distance, int]) -> Distance:
+    def __add__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
