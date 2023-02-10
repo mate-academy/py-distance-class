89e6541d5d97629d4014148b12b305f7a6dbb934
Misha Vovchok <mivvaw@gmail.com>
2023-02-10 17:38:47 +0200

    took Dist off(anotation)&elses off

--- app/main.py
+++ app/main.py
@@ -15,16 +15,14 @@ class Distance:
     def __add__(self, other: [int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        else:
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
     def __iadd__(self, other: [int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
-        else:
-            self.km += other
-            return self
+        self.km += other
+        return self
 
     def __mul__(self, other: [int, float, Distance]) -> Distance:
         return Distance(self.km * other)
@@ -32,17 +30,17 @@ class Distance:
     def __truediv__(self, other: [int, float, Distance]) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: [int, float, Distance]) -> bool:
+    def __lt__(self, other: [int, float]) -> bool:
         return self.km < other
 
-    def __gt__(self, other: [int, float, Distance]) -> bool:
+    def __gt__(self, other: [int, float]) -> bool:
         return self.km > other
 
-    def __eq__(self, other: [int, float, Distance]) -> bool:
+    def __eq__(self, other: [int, float]) -> bool:
         return self.km == other
 
-    def __le__(self, other: [int, float, Distance]) -> bool:
+    def __le__(self, other: [int, float]) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: [int, float, Distance]) -> bool:
+    def __ge__(self, other: [int, float]) -> bool:
         return self.km >= other
