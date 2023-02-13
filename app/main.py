c9be407765c7518503ac4676e12234d4b3b705d3
Artur Tytomyr <art.tytomyr@gmail.com>
2023-02-13 20:52:15 +0200

    revised

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 class Distance:
 
-    def __init__(self, km: int | float | Distance) -> (int, float):
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -21,29 +21,29 @@ class Distance:
             self.km = self.km + other
         return self
 
-    def __truediv__(self, other: int | float | Distance) -> Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(km=round((self.km / other), 2))
 
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __mul__(self, other: int | float | Distance) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(km=self.km * other)
 
-    def __eq__(self, other: int | float | Distance) -> bool:
+    def __eq__(self, other: Distance) -> bool:
         return self.km == other
 
-    def __ne__(self, other: int | float | Distance) -> bool:
+    def __ne__(self, other: Distance) -> bool:
         return self.km != other
 
-    def __gt__(self, other: int | float | Distance) -> bool:
+    def __gt__(self, other: Distance) -> bool:
         return self.km > other
 
-    def __lt__(self, other: int | float | Distance) -> bool:
+    def __lt__(self, other: Distance) -> bool:
         return self.km < other
 
-    def __ge__(self, other: int | float | Distance) -> bool:
+    def __ge__(self, other: Distance) -> bool:
         return self.km > other or self.km == other
 
-    def __le__(self, other: int | float | Distance) -> bool:
+    def __le__(self, other: Distance) -> bool:
         return self.km < other or self.km == other
