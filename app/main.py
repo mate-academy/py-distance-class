97230e21924323ee8a81911751cc22c433ca0a93
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-15 17:55:34 +0200

    fixed as per comments

--- app/main.py
+++ app/main.py
@@ -13,37 +13,43 @@ class Distance:
 
     def __add__(self, number: int | float) -> Distance:
         if isinstance(number, Distance):
-            return Distance(km=self.km + number.km)
-        else:
-            number = Distance(number)
-            return Distance(km=self.km + number.km)
+            return Distance(self.km + number.km)
+        return Distance(self.km + number)
 
     def __iadd__(self, number: int | float) -> Distance:
         if isinstance(number, Distance):
             self.km += number.km
-            return self
         else:
-            number = Distance(number)
-            self.km += number.km
-            return self
+            self.km += number
+        return self
 
     def __mul__(self, number: int | float) -> Distance:
         return Distance(self.km * number)
 
     def __truediv__(self, number: int | float) -> Distance:
-        return Distance(km=round((self.km / number), 2))
+        return Distance(round((self.km / number), 2))
 
     def __lt__(self, number: int | float) -> bool:
+        if isinstance(number, Distance):
+            return self.km < number.km
         return self.km < number
 
     def __gt__(self, number: int | float) -> bool:
+        if isinstance(number, Distance):
+            return self.km > number.km
         return self.km > number
 
     def __eq__(self, number: int | float) -> bool:
+        if isinstance(number, Distance):
+            return self.km == number.km
         return self.km == number
 
     def __le__(self, number: int | float) -> bool:
+        if isinstance(number, Distance):
+            return self.km <= number.km
         return self.km <= number
 
     def __ge__(self, number: int | float) -> bool:
+        if isinstance(number, Distance):
+            return self.km >= number.km
         return self.km >= number
