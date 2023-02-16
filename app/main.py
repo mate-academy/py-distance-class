afc8e0ddec8ee4fc7bd9d7ab0f99ab801f098f27
Ruslan <noezyboy1@gmail.com>
2023-02-16 16:49:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -8,23 +8,23 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int) -> bool:
+    def __add__(self, other: int) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
 
         return Distance(self.km + other)
 
-    def __iadd__(self, other: int) -> bool:
+    def __iadd__(self, other: int) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: int) -> bool:
+    def __mul__(self, other: int) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int) -> bool:
+    def __truediv__(self, other: float) -> "Distance":
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int) -> bool:
