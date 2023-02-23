deb57441f0abdd35c32eeca954ca495470382736
Yegor Turenko <ytferma@gmail.com>
2023-02-23 11:58:01 +0200

    All type hints and elif fixed

--- app/main.py
+++ app/main.py
@@ -1,69 +1,70 @@
-from typing import Any
+from __future__ import annotations
+from typing import Union
 
 
 class Distance:
-    def __init__(self, km: float) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
         self.km = km
 
-    def __str__(self) -> Any:
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self) -> Any:
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Any) -> Any:
+    def __add__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             return Distance(
                 km=self.km + other.km
             )
-        elif type(other) is float or type(other) is int:
+        if type(other) is float or type(other) is int:
             return Distance(
                 km=self.km + other
             )
 
-    def __iadd__(self, other: Any) -> Any:
+    def __iadd__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
-        elif type(other) is float or type(other) is int:
+        if type(other) is float or type(other) is int:
             self.km += other
         return self
 
-    def __mul__(self, other: int) -> Any:
+    def __mul__(self, other: Union[int, float]) -> Distance:
         return Distance(
             km=self.km * other
         )
 
-    def __truediv__(self, other: int) -> Any:
+    def __truediv__(self, other: Union[int, float]) -> Distance:
         return Distance(
             km=round(self.km / other, 2)
         )
 
-    def __lt__(self, other: Any) -> Any:
+    def __lt__(self, other: Union[int, float, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif type(other) is float or type(other) is int:
+        if type(other) is float or type(other) is int:
             return self.km < other
 
-    def __gt__(self, other: Any) -> Any:
+    def __gt__(self, other: Union[int, float, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif type(other) is float or type(other) is int:
+        if type(other) is float or type(other) is int:
             return self.km > other
 
-    def __eq__(self, other: Any) -> Any:
+    def __eq__(self, other: Union[int, float, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif type(other) is float or type(other) is int:
+        if type(other) is float or type(other) is int:
             return self.km == other
 
-    def __le__(self, other: Any) -> Any:
+    def __le__(self, other: Union[int, float, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif type(other) is float or type(other) is int:
+        if type(other) is float or type(other) is int:
             return self.km <= other
 
-    def __ge__(self, other: Any) -> Any:
+    def __ge__(self, other: Union[int, float, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif type(other) is float or type(other) is int:
+        if type(other) is float or type(other) is int:
             return self.km >= other
