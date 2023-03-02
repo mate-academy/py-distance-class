09a9db3ed74e7f4360d781b1574608b3de6c0c47
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-02 21:28:27 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,57 @@
+from __future__ import annotations
+
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: Any) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, distance1: Any) -> Distance:
+        if isinstance(distance1, Distance):
+            total_dictance = self.km + distance1.km
+        else:
+            total_dictance = self.km + distance1
+        return Distance(total_dictance)
+
+    def __iadd__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        else:
+            self.km += other
+            return self
+
+    def __mul__(self, other: (int, float)) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int) -> Distance:
+        return Distance(round((self.km / other), 2))
+
+    def __lt__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        else:
+            return self.km < other
+
+    def __qt__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        else:
+            return self.km > other
+
+    def __eq__(self, other: Distance) -> bool:
+        return self.km == other
+
+    def __le__(self, other: Distance) -> bool:
+        return self.km <= other
+
+    def __ge__(self, other: Distance) -> bool:
+        return self.km >= other
