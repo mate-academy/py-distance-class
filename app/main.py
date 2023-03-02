b7d0354ab1997353d619334903c5b6063eae0f3d
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-02 19:49:48 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
 from __future__ import annotations
 
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
 
-    def __str__(self) ->str:
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers"
 
-    def __repr__(self) ->str:
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: (float)) -> float:
-        if  isinstance(other, Distance):
+        if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, float):
             return Distance(self.km + other)
 
-    def __iadd__(self, other: (Distance, float)) -> Distance: 
+    def __iadd__(self, other: (Distance, float)) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
