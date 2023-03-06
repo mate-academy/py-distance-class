fe4e81003a21b5ca1654e32a03812fa7b3cff455
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-03-06 16:20:06 +0200

    'Solution6'

--- app/main.py
+++ app/main.py
@@ -14,25 +14,22 @@ class Distance:
 
     def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
-            self.km += other.km
+            return Distance(self.km + other.km)
         else:
-            self.km += + other
-        return self
+            return Distance(self.km + other)
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, (int, float)):
-            self.km += other
-        else:
+        if isinstance(other, Distance):
             self.km += other.km
+        else:
+            self.km += other
         return self
 
     def __mul__(self, other: int | float | Distance) -> Distance:
-        self.km = self.km * other
-        return self
+        return Distance(self.km * other)
 
     def __truediv__(self, other: int | float | Distance) -> Distance:
-        self.km = round(self.km / other, 2)
-        return self
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, (int, float)):
