780afa0b0c7ec1f7adf9fb35270ca9aa4f601113
Petro Havrysh <petro.havrysh@gmail.com>
2023-03-17 10:54:45 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ class Distance:
     def __repr__(self) -> Distance:
         return f"Distance(km={self.km})"  # "Distance(km=20)"
 
-    def __add__(self, other: int) -> __class__:
+    def __add__(self, other: int) -> Distance:
         if isinstance(other, Distance):
             return self.km + other.km
         elif isinstance(other, (int, float)):
@@ -22,34 +22,34 @@ class Distance:
             self.km += other
         return self
 
-    def __mul__(self, other: int) -> __class__:
+    def __mul__(self, other: int) -> Distance:
         return self.km * other
 
-    def __truediv__(self, other: int) -> __class__:
+    def __truediv__(self, other: int) -> Distance:
         return round(self.km / other, 2)
 
-    def __lt__(self, other: int) -> __class__:
+    def __lt__(self, other: int) -> Distance:
         if isinstance(other, Distance):
             return self.km < other.km
         else:
             return self.km < other
 
-    def __gt__(self, other: int) -> __class__:
+    def __gt__(self, other: int) -> Distance:
         if isinstance(other, Distance):
             return self.km > other.km
         else:
             return self.km > other
 
-    def __eq__(self, other: int) -> __class__:
+    def __eq__(self, other: int) -> Distance:
         return self.km == other
 
-    def __le__(self, other: int) -> __class__:
+    def __le__(self, other: int) -> Distance:
         if isinstance(other, Distance):
             return self.km <= other.km
         else:
             return self.km <= other
 
-    def __ge__(self, other: int) -> __class__:
+    def __ge__(self, other: int) -> Distance:
         if isinstance(other, Distance):
             return self.km >= other.km
         else:
