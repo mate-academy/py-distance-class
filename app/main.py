c878c46dd89fb012acefba09766c6f81ca393a48
Bohdan Marko <goldenbohdanm@gmail.com>
2023-03-18 11:52:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 class Distance:
 
-    def __init__(self, km: float) -> None:
+    def __init__(self, km: float | int) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,81 +12,47 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | float) -> Distance:
+    def __add__(self, other: Distance | float | int) -> Distance:
         if isinstance(other, (float, int)):
             return Distance(self.km + other)
-        elif isinstance(other, Distance):
-            return Distance(self.km + other.km)
-        else:
-            raise TypeError(f"Unsupported operation '+'"
-                            f" ({type(self)}) and ({type(other)})")
+        return Distance(self.km + other.km)
 
-    def __iadd__(self, other: Distance | float) -> Distance:
+    def __iadd__(self, other: Distance | float | int) -> Distance:
         if isinstance(other, (float, int)):
             self.km += other
             return self
-        elif isinstance(other, Distance):
-            self.km += other.km
-            return self
-        else:
-            raise TypeError(f"Unsupported operation '+='"
-                            f" ({type(self)}) and ({type(other)})")
+        self.km += other.km
+        return self
 
-    def __mul__(self, other: float) -> Distance:
+    def __mul__(self, other: float | int) -> Distance:
         if isinstance(other, (float, int)):
             return Distance(self.km * other)
-        else:
-            raise TypeError(f"Unsupported operation '*'"
-                            f" ({type(self)}) and ({type(other)})")
 
-    def __truediv__(self, other: float) -> Distance:
+    def __truediv__(self, other: float | int) -> Distance:
         if isinstance(other, (float, int)):
             return Distance(round(self.km / other, 2))
-        else:
-            raise TypeError(f"Unsupported operation '/'"
-                            f" ({type(self)}) and ({type(other)})")
 
     def __eq__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, (int, float)):
-            return self.km == other
-        else:
-            raise TypeError(f"Unsupported operation '=='"
-                            f" ({type(self)}) and ({type(other)})")
+        return self.km == other
 
     def __le__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif isinstance(other, (int, float)):
-            return self.km <= other
-        else:
-            raise TypeError(f"Unsupported operation '<='"
-                            f" ({type(self)}) and ({type(other)})")
+        return self.km <= other
 
     def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif isinstance(other, (int, float)):
-            return self.km < other
-        else:
-            raise TypeError(f"Unsupported operation '<'"
-                            f" ({type(self)}) and ({type(other)})")
+        return self.km < other
 
     def __ge__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif isinstance(other, (int, float)):
-            return self.km >= other
-        else:
-            raise TypeError(f"Unsupported operation '>=' "
-                            f"({type(self)}) and ({type(other)})")
+        return self.km >= other
 
     def __gt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif isinstance(other, (int, float)):
-            return self.km > other
-        else:
-            raise TypeError(f"Unsupported operation '>'"
-                            f" ({type(self)}) and ({type(other)})")
+        return self.km > other
