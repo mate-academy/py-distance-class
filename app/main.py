380e45d3e402a1f297d9ff8afabf8bf93441cb4d
Влад <vladearth7@gmail.com>
2023-03-21 05:09:03 +0200

    Add typehints

--- app/main.py
+++ app/main.py
@@ -1,3 +1,58 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometres."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other) -> "Distance":
+        if isinstance(other, int):
+            other = Distance(other)
+        return Distance(km=self.km + other.km)
+
+    def __iadd__(self, other) -> "Distance":
+        if isinstance(other, int):
+            other = Distance(other)
+        self.km += other.km
+        return self
+
+    def __mul__(self, other) -> "Distance":
+        if isinstance(other, int):
+            other = Distance(other)
+        return Distance(km=self.km * other.km)
+
+    def __truediv__(self, other) -> "Distance":
+        if isinstance(other, int):
+            other = Distance(other)
+        return Distance(km=round(self.km / other.km, 2))
+
+    def __lt__(self, other) -> bool:
+        if isinstance(other, int):
+            other = Distance(other)
+        return self.km < other.km
+
+    def __gt__(self, other) -> bool:
+        if isinstance(other, int):
+            other = Distance(other)
+        return self.km > other.km
+
+    def __le__(self, other) -> bool:
+        if isinstance(other, int):
+            other = Distance(other)
+        return self.km <= other.km
+
+    def __ge__(self, other) -> bool:
+        if isinstance(other, int):
+            other = Distance(other)
+        return self.km >= other.km
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, int):
+            other = Distance(other)
+        return self.km == other.km
+
+    def __len__(self) -> int:
+        return self.km
