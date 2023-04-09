420624226e55659359d781f052f9744d16331e30
Denys Belei <denysbelei96>
2023-04-09 16:34:09 +0100

    Solution Distance

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from typing import Union
+
+
 class Distance:
-    def __init__(self, km: float) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -8,36 +11,35 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: ("Distance", int , float)) -> "Distance":
+    def __add__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
-    def __iadd__(self, other: "Distance") -> "Distance":
-        if isinstance(other, Distance):
-            self.km += other.km
-        elif isinstance(other, (int, float)):
+    def __iadd__(self, other: Union[int, float, "Distance"]) -> "Distance":
+        if isinstance(other, (int, float)):
             self.km += other
+        else:
+            self.km += other.km
         return self
 
-    def __mul__(self, other: float) -> "Distance":
+    def __mul__(self, other: Union[int, float]) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other: (int, float)) -> float:
+    def __truediv__(self, other: Union[int, float]) -> float:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: (int, float)) -> "Distance":
+    def __lt__(self, other: Union[int, float]) -> "Distance":
         return self.km < other
 
-    def __gt__(self, other: (int, float)) -> "Distance":
+    def __gt__(self, other: Union[int, float]) -> "Distance":
         return self.km > other
 
-    def __eq__(self, other: (int, float)) -> "Distance":
+    def __eq__(self, other: Union[int, float]) -> "Distance":
         return self.km == other
 
-    def __le__(self, other: (int, float)) -> "Distance":
+    def __le__(self, other: Union[int, float]) -> "Distance":
         return self.km <= other
 
-    def __ge__(self, other: (int, float)) -> "Distance":
+    def __ge__(self, other: Union[int, float]) -> "Distance":
         return self.km >= other
