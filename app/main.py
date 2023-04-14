5ca55e85e34db8fe2e385191574b1f1820b417d4
laskarj <koks.adekvat@gmail.com>
2023-04-14 22:10:41 +0200

    Work on misstakes

--- app/main.py
+++ app/main.py
@@ -15,19 +15,32 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Union[Distance, int, float]) -> Distance:
-        return self._math_operation(other, lambda a, b: a + b)
+        return self._math_operation(
+            other,
+            lambda a, b: a + b,
+            new=True
+        )
 
     def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
-        return self._math_operation(other, lambda a, b: a + b)
+        return self._math_operation(
+            other,
+            lambda a, b: a + b
+        )
 
     def __mul__(self, other: Union[int, float]) -> Distance:
         if isinstance(other, (int, float)):
-            return self._math_operation(other, lambda a, b: a * b)
+            return self._math_operation(
+                other,
+                lambda a, b: a * b,
+                new=True
+            )
 
     def __truediv__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, (int, float)):
             return self._math_operation(
-                other, lambda a, b: a / b, rounded=True
+                other,
+                lambda a, b: round(a / b, 2),
+                new=True
             )
 
     def __lt__(self, other: Union[Distance, int, float]) -> bool:
@@ -45,20 +58,27 @@ class Distance:
     def __ge__(self, other: Union[Distance, int, float]) -> bool:
         return self._bool_operations(other, lambda a, b: a >= b)
 
-    def _math_operation(self, other: Union[Distance, int],
-                        operations: Callable,
-                        rounded: bool = False) -> Distance:
+    def _math_operation(
+            self,
+            other: Union[Distance, int, float],
+            operations: Callable,
+            new: bool = False
+    ) -> Distance:
 
         other_km = other.km if isinstance(other, Distance) else other
-        if rounded:
-            self.km = round(operations(self.km, other_km), 2)
-            return self
+        km = operations(self.km, other_km)
 
-        self.km = operations(self.km, other_km)
+        if new:
+            return Distance(km=km)
+
+        self.km = km
         return self
 
-    def _bool_operations(self, other: Union[Distance, int],
-                         operations: Callable) -> bool:
+    def _bool_operations(
+            self,
+            other: Union[Distance, int, float],
+            operations: Callable
+    ) -> bool:
 
         other_km = other.km if isinstance(other, Distance) else other
         return operations(self.km, other_km)
