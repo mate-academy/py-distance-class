c0e09aa330d318ea3eac8f75b0a90c4e703f3bcc
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-04-20 21:11:51 +0300

    Solution to problems

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Union
 
 
 class Distance:
-    Validator = Union["Distance", int]
+    Object_or_a_number = Union["Distance", int | float]
 
     def __init__(self, km: int | float) -> None:
         self.km = km
@@ -15,52 +15,49 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Validator) -> Distance:
+    def __add__(self, other: Object_or_a_number) -> Distance:
         if isinstance(other, Distance):
             return Distance(km=self.km + other.km)
         return Distance(km=self.km + other)
 
-    def __iadd__(self, other: Validator) -> Distance:
+    def __iadd__(self, other: Object_or_a_number) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    @staticmethod
-    def is_a_instance(other: Validator) -> None:
+    def __mul__(self, other: int | float) -> Distance | None:
         if isinstance(other, Distance):
             return None
-
-    def __mul__(self, other: Validator) -> Distance:
-        self.is_a_instance(other)
         return Distance(km=self.km * other)
 
-    def __truediv__(self, other: Validator) -> Distance | None:
-        self.is_a_instance(other)
+    def __truediv__(self, other: int | float) -> Distance | None:
+        if isinstance(other, Distance):
+            return None
         return Distance(km=round(self.km / other, 2))
 
-    def __lt__(self, other: Validator) -> bool:
+    def __lt__(self, other: Object_or_a_number) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         return self.km < other
 
-    def __gt__(self, other: Validator) -> bool:
+    def __gt__(self, other: Object_or_a_number) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         return self.km > other
 
-    def __eq__(self, other: Validator) -> bool:
+    def __eq__(self, other: Object_or_a_number) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         return self.km == other
 
-    def __le__(self, other: Validator) -> bool:
+    def __le__(self, other: Object_or_a_number) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         return self.km <= other
 
-    def __ge__(self, other: Validator) -> bool:
+    def __ge__(self, other: Object_or_a_number) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         return self.km >= other
