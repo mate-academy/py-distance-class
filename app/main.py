6066b11482b4b9d479649272eb96ae008b4c9387
N1khto <112682735+N1khto@users.noreply.github.com>
2023-05-11 20:03:21 +0300

    Update app/main.py
    Co-authored-by: ArsenPidhoretskyi <65166203+ArsenPidhoretskyi@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ class Distance:
             return self.km < other
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        return self.__gt__(other) or self.__eq__(other)
+        return self > other
 
     def __le__(self, other: Distance | int | float) -> bool:
         return self.__lt__(other) or self.__eq__(other)
