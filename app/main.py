8ab5f8c16027b5d76595de48abfa4e6881c2746e
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-05-17 13:07:36 +0300

    Distance class implementation

--- app/main.py
+++ app/main.py
@@ -1,3 +1,47 @@
+from __future__ import annotations
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __float__(self) -> float:
+        return float(self.km)
+
+    def __radd__(self, rho: Any) -> Distance:
+        return self.__add__(rho)
+
+    def __add__(self, rho: Any) -> Distance:
+        return Distance(self.km + float(rho))
+
+    def __iadd__(self, rho: Any) -> Distance:
+        self.km += float(rho)
+        return self
+
+    def __mul__(self, rho: float) -> Distance:
+        return Distance(self.km * rho)
+
+    def __truediv__(self, rho: float) -> Distance:
+        return Distance(round(self.km / rho, 2))
+
+    def __lt__(self, rho: Any) -> bool:
+        return self.km < float(rho)
+
+    def __gt__(self, rho: Any) -> bool:
+        return self.km > float(rho)
+
+    def __eq__(self, rho: Any) -> bool:
+        return self.km == float(rho)
+
+    def __le__(self, rho: Any) -> bool:
+        return self.km <= float(rho)
+
+    def __ge__(self, rho: Any) -> bool:
+        return self.km >= float(rho)
