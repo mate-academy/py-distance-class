2bddd01668b8cdd66f463d41463b10dd2b34f227
maccarets <maccarets@gmail.com>
2023-05-29 22:18:01 +0300

    done

--- app/main.py
+++ app/main.py
@@ -1,57 +1,55 @@
+from typing import Union
+
+
 class Distance:
-    def __init__(self, km):
+    def __init__(self, km: Union[float, int]) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, dist):
+    def __add__(self, dist: Union["Distance", float, int]) -> "Distance":
         if isinstance(dist, Distance):
             return Distance(self.km + dist.km)
         return Distance(self.km + dist)
 
-    def __iadd__(self, dist):
+    def __iadd__(self, dist: Union["Distance", float, int]) -> "Distance":
         if isinstance(dist, Distance):
             self.km += dist.km
             return self
         self.km += dist
         return self
 
-    def __mul__(self, dist):
+    def __mul__(self, dist: Union[float, int]) -> "Distance":
         return Distance(self.km * dist)
 
-    def __truediv__(self, dist):
+    def __truediv__(self, dist: Union[float, int]) -> "Distance":
         return Distance(round(self.km / dist, 2))
 
-    def __lt__(self, dist):
+    def __lt__(self, dist: Union["Distance", float, int]) -> bool:
         if isinstance(dist, Distance):
-            return True if self.km < dist.km else False
-        return True if self.km < dist else False
+            return self.km < dist.km
+        return self.km < dist
 
-    def __gt__(self, dist):
+    def __gt__(self, dist: Union["Distance", float, int]) -> bool:
         if isinstance(dist, Distance):
-            return True if self.km > dist.km else False
-        return True if self.km > dist else False
+            return self.km > dist.km
+        return self.km > dist
 
-    def __eq__(self, dist):
+    def __eq__(self, dist: Union["Distance", float, int]) -> bool:
         if isinstance(dist, Distance):
-            return True if self.km == dist.km else False
-        return True if self.km ==dist else False
+            return self.km == dist.km
+        return self.km == dist
 
-    def __le__(self, dist):
+    def __le__(self, dist: Union["Distance", float, int]) -> bool:
         if isinstance(dist, Distance):
-            return True if self.km <= dist.km else False
-        return True if self.km <= dist else False
+            return self.km <= dist.km
+        return self.km <= dist
 
-    def __ge__(self, dist):
+    def __ge__(self, dist: Union["Distance", float, int]) -> bool:
         if isinstance(dist, Distance):
-            return True if self.km >= dist.km else False
-        return True if self.km >= dist else False
-
-
-d = Distance(15)
-d += 30
-print(d)
+            return self.km >= dist.km
+        return self.km >= dist
