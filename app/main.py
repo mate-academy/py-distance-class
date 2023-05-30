ca02b94da00d4a763df26d4498061905c32808a4
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-05-30 16:21:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,53 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: float | Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+
+        return Distance(self.km + other)
+
+    def __iadd__(self, other: float | Distance) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+
+        return self
+
+    def __mul__(self, other: float) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: float) -> Distance:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+
+        return self.km < other
+
+    def __gt__(self, other: float | Distance) -> bool:
+        return not self.__lt__(other) and not self.__eq__(other)
+
+    def __eq__(self, other: float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+
+        return self.km == other
+
+    def __le__(self, other: float | Distance) -> bool:
+        return self.__eq__(other) or self.__lt__(other)
+
+    def __ge__(self, other: float | Distance) -> bool:
+        return self.__eq__(other) or not self.__lt__(other)
