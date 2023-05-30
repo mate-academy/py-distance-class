637cbade7c4e72a63d48a0c53473a610bc073d3c
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-05-30 22:15:27 +0300

    My solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,55 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance | int) -> Distance:
+        if type(other) == int or type(other) == float:
+            return Distance(self.km + other)
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other: Distance | int) -> Distance:
+        if type(other) == int or type(other) == float:
+            self.km += other
+        else:
+            self.km += other.km
+        return self
+
+    def __mul__(self, other: int) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: float) -> Distance:
+        return Distance(round((self.km / other), 2))
+
+    def __lt__(self, other: Distance | float) -> bool:
+        if type(other) == float or type(other) == int:
+            return self.km < other
+        return self.km < other.km
+
+    def __gt__(self, other: Distance | float) -> bool:
+        if type(other) == float or type(other) == int:
+            return self.km > other
+        return self.km > other.km
+
+    def __eq__(self, other: Distance | float) -> bool:
+        if type(other) == float or type(other) == int:
+            return self.km == other
+        return self.km == other.km
+
+    def __le__(self, other: Distance | float) -> bool:
+        if type(other) == float or type(other) == int:
+            return self.km <= other
+        return self.km <= other.km
+
+    def __ge__(self, other: Distance | float) -> bool:
+        if type(other) == float or type(other) == int:
+            return self.km >= other
+        return self.km >= other.km
