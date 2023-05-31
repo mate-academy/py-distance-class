dd3ab75d4a4b3827c1ffcc9ca2021817671b2b7c
Rostyslav <rostyslav.furman@gmail.com>
2023-05-31 21:20:34 +0300

    my_solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,53 @@
+from __future__ import annotations
+
+
 class Distance:
     # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other_distance: float | Distance) -> Distance:
+        if isinstance(other_distance, (int, float)):
+            return Distance(self.km + other_distance)
+        return Distance(self.km + other_distance.km)
+
+    def __iadd__(self, other_distance: float | Distance) -> Distance:
+        if isinstance(other_distance, (int, float)):
+            self.km += other_distance
+            return self
+        self.km += other_distance.km
+        return self
+
+    def __mul__(self, number: float) -> Distance:
+        if isinstance(number, (int, float)):
+            return Distance(self.km * number)
+
+    def __truediv__(self, number: float) -> Distance:
+        return Distance(round(self.km / number, 2))
+
+    def __eq__(self, other_distance: float | Distance) -> bool:
+        if isinstance(other_distance, Distance):
+            return self.km == other_distance.km
+        return self.km == other_distance
+
+    def __lt__(self, other_distance: float | Distance) -> bool:
+        if isinstance(other_distance, Distance):
+            return self.km < other_distance.km
+        return self.km < other_distance
+
+    def __le__(self, other_distance: float | Distance) -> bool:
+        return not self > other_distance
+
+    def __gt__(self, other_distance: float | Distance) -> bool:
+        if isinstance(other_distance, Distance):
+            return self.km > other_distance.km
+        return self.km > other_distance
+
+    def __ge__(self, other_distance: float | Distance) -> bool:
+        return not self.km < other_distance
