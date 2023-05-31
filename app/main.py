f67b10c4cc11c80c6c46b2e2007a22692c5553f6
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-05-31 17:06:02 +0300

    Rewrote methods using operators instead of calling methods. Linebreakers were removed

--- app/main.py
+++ app/main.py
@@ -40,11 +40,10 @@ class Distance:
         return self.km < other
 
     def __gt__(self, other: int | float | Distance) -> bool:
-        return not Distance.__eq__(self, other)\
-            and not Distance.__lt__(self, other)
+        return not (self == other) and not (self < other)
 
     def __le__(self, other: int | float | Distance) -> bool:
-        return not Distance.__gt__(self, other)
+        return not (self > other)
 
     def __ge__(self, other: int | float | Distance) -> bool:
-        return not Distance.__lt__(self, other)
+        return not (self < other)
