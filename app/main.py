c1f05eb92079ebb1ded68a919b8423dde88a8336
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-05-31 17:43:56 +0300

    Combine checks

--- app/main.py
+++ app/main.py
@@ -12,12 +12,12 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Distance | int) -> Distance:
-        if isinstance(other, float) or isinstance(other, int):
+        if isinstance(other, (float, int)):
             return Distance(self.km + other)
         return Distance(self.km + other.km)
 
     def __iadd__(self, other: Distance | int) -> Distance:
-        if isinstance(other, float) or isinstance(other, int):
+        if isinstance(other, (float, int)):
             self.km += other
         else:
             self.km += other.km
@@ -30,22 +30,22 @@ class Distance:
         return Distance(round((self.km / other), 2))
 
     def __lt__(self, other: Distance | float) -> bool:
-        if isinstance(other, float) or isinstance(other, int):
+        if isinstance(other, (float, int)):
             return self.km < other
         return self.km < other.km
 
     def __gt__(self, other: Distance | float) -> bool:
-        if isinstance(other, float) or isinstance(other, int):
+        if isinstance(other, (float, int)):
             return self.km > other
         return self.km > other.km
 
     def __eq__(self, other: Distance | float) -> bool:
-        if isinstance(other, float) or isinstance(other, int):
+        if isinstance(other, (float, int)):
             return self.km == other
         return self.km == other.km
 
     def __le__(self, other: Distance | float) -> bool:
-        return not (self > other)
+        return not self > other
 
     def __ge__(self, other: Distance | float) -> bool:
-        return not (self < other)
+        return not self < other
