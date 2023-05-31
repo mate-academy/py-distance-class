7fc79030f26d6c465297ee5f12fd24e3d43ec612
YOUR_NAME <YOUR_EMAIL>
2023-05-31 20:11:01 +0300

    Distance (third: * fix else) * fix __le__, __ge__ (again)

--- app/main.py
+++ app/main.py
@@ -15,8 +15,7 @@ class Distance:
     def __add__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        else:
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
     def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, Distance):
@@ -26,33 +25,28 @@ class Distance:
         return self
 
     def __mul__(self, other: [int, float]) -> Distance:
-        mul_result = self.km * other
-        return Distance(mul_result)
+        return Distance(self.km * other)
 
     def __truediv__(self, number: [int, float]) -> Distance:
-        truediv_result = round(self.km / number, 2)
-        return Distance(truediv_result)
+        return Distance(round(self.km / number, 2))
 
     def __lt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            return self.km > other
+        return self.km > other
 
     def __eq__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            return self.km == other
+        return self.km == other
 
     def __le__(self, other: Union[Distance, int, float]) -> bool:
-        return self.__lt__(other) or self.__eq__(other)
+        return not self.km > other  # <
 
     def __ge__(self, other: Union[Distance, int, float]) -> bool:
-        return self.__gt__(other) or self.__eq__(other)
+        return not self.km < other
