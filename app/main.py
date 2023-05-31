eecb362dd9a8956cac487317ce929c281bfeca15
Anton Komarov <anton_komarov_qa@ukr.net>
2023-05-31 16:51:48 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ class Distance:
         return self.km == other
 
     def __le__(self, other: Distance | int | float) -> bool:
-        return not (self > other)
+        return not self > other
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        return not (self < other)
+        return not self < other
