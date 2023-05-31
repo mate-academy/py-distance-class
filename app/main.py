9b1d8735db02f034a56c17f956cf7e49ae0a7c41
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-05-31 16:51:41 +0300

    Using operators instead colling methods

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ class Distance:
         return self.km == other.km
 
     def __le__(self, other: Distance | float) -> bool:
-        return not self.__gt__(other)
+        return not (self > other)
 
     def __ge__(self, other: Distance | float) -> bool:
-        return not self.__lt__(other)
+        return not (self < other)
