aeb3c41ed8fb9461ff7c33311759a2a7ad2a7e46
Erik Agayan <erik.agayan1@gmail.com>
2023-05-31 14:43:26 +0300

    Solution #4

--- app/main.py
+++ app/main.py
@@ -11,10 +11,10 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int) -> "Distance" | float:
+    def __add__(self, other: int | Distance) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
+        else:
             return Distance(self.km + other)
 
     def __iadd__(self, other: int) -> "Distance":
@@ -54,4 +54,4 @@ class Distance:
         return self.km <= other
 
     def __ge__(self, other: int) -> int:
-        return self.__gt__(other) or self.__eq__(other)
+        return self.km >= other
