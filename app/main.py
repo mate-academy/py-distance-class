6a85ace8c0f760a8433aca6b25d0a56a92247125
Yaryk Konoplyov <kannabis252@gmail.com>
2023-05-31 19:08:08 +0300

    comparsion optimized

--- app/main.py
+++ app/main.py
@@ -48,7 +48,7 @@ class Distance:
         return self.km == other
 
     def __le__(self, other: Distance | int) -> bool:
-        return self < other or self == other
+        return not self > other
 
     def __ge__(self, other: Distance) -> bool:
-        return self > other or self == other
+        return not self < other
