98efb360e7bec81d9a46a568577f0131675a9b58
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2023-06-01 19:18:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,57 @@
+from __future__ import annotations
+from typing import Union
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Union[float, int, Distance]) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        else:
+            return Distance(km=self.km + other)
+
+    def __iadd__(self, other: Union[float, int, Distance]) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other: float | int) -> Distance:
+        return Distance(km=self.km * other)
+
+    def __truediv__(self, other: float | int) -> Distance:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: Union[float, int, Distance]) -> bool:
+        if self.km < other:
+            return True
+        return False
+
+    def __gt__(self, other: Union[float, int, Distance]) -> bool:
+        if self.km > other:
+            return True
+        return False
+
+    def __eq__(self, other: Union[float, int, Distance]) -> bool:
+        if self.km == other:
+            return True
+        return False
+
+    def __le__(self, other: Union[float, int, Distance]) -> bool:
+        if self.km <= other:
+            return True
+        return False
+
+    def __ge__(self, other: Union[float, int, Distance]) -> bool:
+        if self.km >= other:
+            return True
+        return False
