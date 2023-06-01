bdb150c0218c993df96c4bddff3d1b81b41496fc
Andrii <anrij.lazoryk@gmail.com>
2023-06-01 14:24:10 +0300

    soluton5

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ class Distance:
             return self.km <= other.km
         return self.km <= other
 
-    def __ge__(self, other: "Distance") -> bool:
+    def __ge__(self, other: ("Distance", int, float)) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         return self.km >= other
