839c042b3f0a00e1fdbb11bbc6da552afc3b9f21
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2023-06-02 14:13:13 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -33,34 +33,24 @@ class Distance:
     def __lt__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif self.km < other:
-            return True
-        return False
+        return self.km < other
 
     def __gt__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif self.km > other:
-            return True
-        return False
+        return self.km > other
 
     def __eq__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif self.km == other:
-            return True
-        return False
+        return self.km == other
 
     def __le__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif self.km <= other:
-            return True
-        return False
+        return self.km <= other
 
     def __ge__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif self.km >= other:
-            return True
-        return False
+        return self.km >= other
