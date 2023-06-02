9625d25f0f39a7f214a530dc8f715aa738640e2e
Vlad <vladislav.zhyla@gmail.com>
2023-06-02 19:16:31 +0300

    Fixed some errors

--- app/main.py
+++ app/main.py
@@ -25,20 +25,10 @@ class Distance:
 
         return self
 
-    def __mul__(self, other: int | float | Distance) -> Distance:
-        if isinstance(other, Distance):
-            res = Distance(self.km * other.km)
-        else:
-            res = Distance(self.km * other)
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
 
-        return res
-
-    def __truediv__(self, other: int | Distance) -> Distance:
-        if isinstance(other, Distance):
-            res = Distance(round(self.km / other.km, 2))
-        else:
-            res = Distance(round(self.km / other, 2))
-
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int | float | Distance) -> bool:
