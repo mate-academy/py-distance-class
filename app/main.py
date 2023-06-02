3f8b0c14e5d34b9a47c59d10fc6b582798120a6f
Vlad <vladislav.zhyla@gmail.com>
2023-06-02 11:10:06 +0300

    Added float type for other type annotation

--- app/main.py
+++ app/main.py
@@ -12,12 +12,12 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: Distance | int) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km = self.km + other.km
         else:
@@ -25,23 +25,23 @@ class Distance:
 
         return self
 
-    def __mul__(self, other: int) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
     def __truediv__(self, other: int) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: int) -> bool:
+    def __lt__(self, other: int | float) -> bool:
         return self.km < other
 
-    def __le__(self, other: int) -> bool:
+    def __le__(self, other: int | float) -> bool:
         return self.km <= other
 
-    def __gt__(self, other: int) -> bool:
+    def __gt__(self, other: int | float) -> bool:
         return self.km > other
 
-    def __ge__(self, other: int) -> bool:
+    def __ge__(self, other: int | float) -> bool:
         return self.km >= other
 
-    def __eq__(self, other: int) -> bool:
+    def __eq__(self, other: int | float) -> bool:
         return self.km == other
