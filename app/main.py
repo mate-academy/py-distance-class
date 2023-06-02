16f21cec3ef15d342dd9f087cfe8b84ef3ae90c0
Vlad <vladislav.zhyla@gmail.com>
2023-06-02 19:07:49 +0300

    Added Distance cases operations

--- app/main.py
+++ app/main.py
@@ -26,22 +26,52 @@ class Distance:
         return self
 
     def __mul__(self, other: int | float | Distance) -> Distance:
-        return Distance(self.km * other)
+        if isinstance(other, Distance):
+            res = Distance(self.km * other.km)
+        else:
+            res = Distance(self.km * other)
+
+        return res
 
     def __truediv__(self, other: int | Distance) -> Distance:
+        if isinstance(other, Distance):
+            res = Distance(round(self.km / other.km, 2))
+        else:
+            res = Distance(round(self.km / other, 2))
+
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int | float | Distance) -> bool:
-        return self.km < other
+        if isinstance(other, Distance):
+            res = self.km < other.km
+        else:
+            res = self.km < other
+
+        return res
 
     def __le__(self, other: int | float | Distance) -> bool:
         return self.km <= other
 
     def __gt__(self, other: int | float | Distance) -> bool:
-        return self.km > other
+        if isinstance(other, Distance):
+            res = self.km > other.km
+        else:
+            res = self.km > other
+
+        return res
 
     def __ge__(self, other: int | float | Distance) -> bool:
-        return self.km >= other
+        if isinstance(other, Distance):
+            res = self.km >= other.km
+        else:
+            res = self.km >= other
+
+        return res
 
     def __eq__(self, other: int | float | Distance) -> bool:
-        return self.km == other
+        if isinstance(other, Distance):
+            res = self.km == other.km
+        else:
+            res = self.km == other
+
+        return res
