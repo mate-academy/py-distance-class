0af171d61634b9b4ea29ebbf3f1ae184050db0b2
Mikhail <tiunovmixs@gmail.com>
2023-06-14 20:08:36 +0300

    Fixed flake8 Distance class

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float ) -> Distance:
+    def __add__(self, other: int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
@@ -25,24 +25,24 @@ class Distance:
 
         return self
 
-    def __mul__(self, other) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other) -> Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         if not isinstance(other, Distance):
             return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: int | float) -> bool:
         return self.km < other
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: int | float) -> bool:
         return self.km > other
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: int | float) -> bool:
         return self.km == other
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: int | float) -> bool:
         return self.km <= other
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: int | float) -> bool:
         return self.km >= other
