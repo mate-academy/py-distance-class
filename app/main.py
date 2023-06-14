9aa66ce1d4531abb2abc3debf20e0d257741151a
MichaelKyiv <michaeltwins@gmail.com>
2023-06-14 17:31:21 +0300

    Solution1406

--- app/main.py
+++ app/main.py
@@ -22,28 +22,27 @@ class Distance:
     def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
-
         elif isinstance(other, (int, float)):
             self.km += other
         return self
 
-    def __mul__(self, other: Distance | int | float) -> float:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: Distance | int | float) -> float:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: Distance | int | float) -> int:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < other
 
-    def __gt__(self, other: Distance | int | float) -> int:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > other
 
-    def __eq__(self, other: Distance | int | float) -> int:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == other
 
-    def __le__(self, other: Distance | int | float) -> int:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: Distance | int | float) -> int:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= other
