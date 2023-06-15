d3b5391019d6965847824eaa19ad4ab821fc642a
Dima Khrystenko <hrystanko.dmytro@outlook.com>
2023-06-15 10:08:46 +0200

    Solution correction 1

--- app/main.py
+++ app/main.py
@@ -12,31 +12,31 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         return Distance(self.km + (other.km
                         if isinstance(other, Distance) else other))
 
-    def __iadd__(self, other: Distance | int) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += (other.km if isinstance(other, Distance) else other)
         return self
 
-    def __mul__(self, other: int) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int) -> Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: Distance | int) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < (other.km if isinstance(other, Distance) else other)
 
-    def __gt__(self, other: Distance | int) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > (other.km if isinstance(other, Distance) else other)
 
-    def __eq__(self, other: Distance | int) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == (other.km if isinstance(other, Distance) else other)
 
-    def __le__(self, other: Distance | int) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= (other.km if isinstance(other, Distance) else other)
 
-    def __ge__(self, other: Distance | int) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= (other.km if isinstance(other, Distance) else other)
