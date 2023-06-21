2e33872ebdee1ecb1a8427b76ad32b27c8c93291
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-06-21 16:47:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,134 @@
+from __future__ import annotations
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    @staticmethod
+    def _distance_types_validator(
+            obj: Any,
+            valid_types: tuple,
+            operator: str
+    ) -> None:
+        if not isinstance(obj, valid_types):
+            raise TypeError(
+                f"TypeError: unsupported operand type(s) for {operator}: "
+                f"'Distance' and '{type(obj)}'"
+            )
+
+    def __add__(self, other: Distance | int | float) -> Distance:
+
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(Distance, int, float),
+            operator="+"
+        )
+
+        if isinstance(other, Distance):
+            other = other.km
+
+        return Distance(km=self.km + other)
+
+    def __iadd__(self, other: Distance | int | float) -> Distance:
+
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(Distance, int, float),
+            operator="+="
+        )
+
+        self.km = self + other
+
+        return self
+
+    def __mul__(self, other: int | float) -> Distance:
+
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(int, float),
+            operator="*"
+        )
+
+        return Distance(km=self.km * other)
+
+    def __truediv__(self, other: int | float) -> Distance:
+
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(int, float),
+            operator="/"
+        )
+
+        km = self.km / other
+        round_km = int(km) if int(km) == km else float("{:.2f}".format(km))
+        return Distance(km=round_km)
+
+    def __lt__(self, other: Distance | int | float) -> bool:
+
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(Distance, int, float),
+            operator="<"
+        )
+
+        if isinstance(other, Distance):
+            other = other.km
+
+        return self.km < other
+
+    def __gt__(self, other: Distance | int | float) -> bool:
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(Distance, int, float),
+            operator=">"
+        )
+
+        if isinstance(other, Distance):
+            other = other.km
+
+        return self.km > other
+
+    def __eq__(self, other: Distance | int | float) -> bool:
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(Distance, int, float),
+            operator="=="
+        )
+
+        if isinstance(other, Distance):
+            other = other.km
+
+        return self.km == other
+
+    def __le__(self, other: Distance | int | float) -> bool:
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(Distance, int, float),
+            operator="<="
+        )
+
+        if isinstance(other, Distance):
+            other = other.km
+
+        return self.km <= other
+
+    def __ge__(self, other: Distance | int | float) -> bool:
+        self._distance_types_validator(
+            obj=other,
+            valid_types=(Distance, int, float),
+            operator=">="
+        )
+
+        if isinstance(other, Distance):
+            other = other.km
+
+        return self.km >= other
