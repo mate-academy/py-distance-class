eca5dcb6e3e1fcb04b79505216b70ae43bb94587
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-06-25 22:45:28 +0300

    created distance class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,50 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __add_eval_value__(self, simbl: str,
+                           value: Distance | int | float):
+        if isinstance(value, Distance):
+            return eval(f"{self.km} {simbl} {value.km}")
+        return eval(f"{self.km} {simbl} {value}")
+
+    def __str__(self) -> str:
+        return f"{Distance.__name__}: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        item = self.__dict__.items()
+        for key, value in item:
+            return f"{Distance.__name__}({key}={value})"
+
+    def __add__(self, other: Distance) -> Distance:
+        return Distance(self.__add_eval_value__(simbl="+",
+                                                value=other))
+
+    def __iadd__(self, other: Distance) -> Distance:
+        self.km = self.__add_eval_value__(simbl="+",
+                                          value=other)
+        return self
+
+    def __mul__(self, value: int) -> Distance:
+        return Distance(round(self.km * value, 3))
+
+    def __truediv__(self, value: int) -> Distance:
+        return Distance(round(self.km / value, 2))
+
+    def __lt__(self, other: Distance | int | float) -> bool:
+        return self.__add_eval_value__(simbl="<", value=other)
+
+    def __gt__(self, other: Distance | int | float) -> bool:
+        return self.__add_eval_value__(simbl=">", value=other)
+
+    def __eq__(self, other: Distance | int | float) -> bool:
+        return self.__add_eval_value__(simbl="==", value=other)
+
+    def __le__(self, other: Distance | int | float) -> bool:
+        return self.__add_eval_value__(simbl="<=", value=other)
+
+    def __ge__(self, other: Distance | int | float) -> bool:
+        return self.__add_eval_value__(simbl=">=", value=other)
