6a2b8c76620c1581603a05452729ead7f600e81d
Oleksandr Tarnavsky <sasha.tarnavsky@gmail.com>
2023-06-26 20:11:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,57 @@
+from __future__ import annotations
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: [int, float]) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: list[Distance, int, float]) -> Any:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __iadd__(self, other: list[Distance, int, float]) -> Any:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other: list[int, float]) -> Any:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: list[Distance, int, float]) -> Any:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: list[Distance, int, float]) -> Any:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        return self.km < other
+
+    def __gt__(self, other: list[Distance, int, float]) -> Any:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
+
+    def __eq__(self, other: list[Distance, int, float]) -> Any:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        return self.km == other
+
+    def __le__(self, other: list[Distance, int, float]) -> Any:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        return self.km <= other
+
+    def __ge__(self, other: list[Distance, int, float]) -> Any:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return self.km >= other
