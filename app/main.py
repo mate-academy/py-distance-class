4d5909dc76448f8deee3f4e8f91fce9c1312366e
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 11:42:58 +0300

    Create __ge__ in Distance class

--- app/main.py
+++ app/main.py
@@ -42,3 +42,8 @@ class Distance:
         if isinstance(other, Distance):
             return self.km > other.km
         return self.km > other
+
+    def __ge__(self, other: Distance | int) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return self.km >= other
