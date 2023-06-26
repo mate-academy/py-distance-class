da0905be250cad2c9e573652c8903903f5574c1e
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 11:35:07 +0300

    Create __iadd__ in Distance class

--- app/main.py
+++ app/main.py
@@ -14,3 +14,10 @@ class Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
+    
+    def __iadd__(self, other: int | Distance) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
