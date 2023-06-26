109af65a0f713e2e3c64fdf29498296663f185b2
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 11:31:15 +0300

    Create __add__ in Distance class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
@@ -7,3 +9,8 @@ class Distance:
 
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
+
+    def __add__(self, other: int | Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
