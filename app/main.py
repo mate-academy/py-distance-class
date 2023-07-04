73408b4f20fb263f94d7891cc5c49fef437df154
Alla Kuksa <alla.kuksa92@gmail.com>
2023-07-04 20:52:56 +0300

    Changed mistake

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: int | float) -> None:
         self.km = km
@@ -8,22 +11,22 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float):
+    def __add__(self, other: int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: int | float):
+    def __iadd__(self, other: int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: float | int):
+    def __mul__(self, other: float | int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float | int):
+    def __truediv__(self, other: float | int) -> Distance:
         return Distance(round((self.km / other), 2))
 
     def __lt__(self, other: int | float) -> bool:
