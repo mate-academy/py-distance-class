ec6f97cdd0c18142dffe79a0228c555a380c7a03
Shadowtion <bands.shady@gmail.com>
2023-07-09 15:30:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,59 @@
+from __future__ import annotations
+from typing import Any, Union
+
+
+OtherInputType = Union[float, int]
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: OtherInputType) -> Distance:
+        return Distance(
+            self.km + self.number_return(other)
+        )
+
+    def __iadd__(self, other: OtherInputType) -> Distance:
+        self.km += self.number_return(other)
+        return self
+
+    def __mul__(self, other: int | float) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(
+                self.km * self.number_return(other)
+            )
+
+    def __truediv__(self, other: float | int) -> Distance:
+        return Distance(
+            round((self.km / other), 2)
+        )
+
+    def __lt__(self, other: OtherInputType) -> bool:
+        return self.km < self.number_return(other)
+
+    def __gt__(self, other: OtherInputType) -> bool:
+        return self.km > self.number_return(other)
+
+    def __eq__(self, other: OtherInputType) -> bool:
+        return self.km == self.number_return(other)
+
+    def __le__(self, other: OtherInputType) -> bool:
+        return self.km <= self.number_return(other)
+
+    def __ge__(self, other: OtherInputType) -> bool:
+        return self.km >= self.number_return(other)
+
+    @staticmethod
+    def number_return(number: Any) -> int | float | None:
+        if isinstance(number, Distance):
+            return number.km
+        elif isinstance(number, (int, float)):
+            return number
