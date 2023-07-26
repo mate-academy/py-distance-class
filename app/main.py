bc878cede243eca602fb0890a33d9226b6fd62ae
Юрий <you@exampleabc1234567892777@gmail.com>
2023-07-26 19:13:18 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -8,7 +8,9 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    from typing import Union
+
+    def __add__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -16,7 +18,7 @@ class Distance:
         else:
             raise TypeError("Unsupported operand type for +")
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -77,27 +79,3 @@ class Distance:
             return self.km >= other
         else:
             raise TypeError("Unsupported operand type for >=")
-
-
-distance1 = Distance(20)
-distance2 = Distance(30)
-distance3 = distance1 + distance2
-print(distance3)  # Output: "Distance: 50 kilometers."
-
-distance1 += distance2
-print(distance1)  # Output: "Distance: 50 kilometers."
-
-distance4 = distance1 + 10
-print(distance4)  # Output: "Distance: 60 kilometers."
-
-distance5 = distance1 * 5
-print(distance5)  # Output: "Distance: 250 kilometers."
-
-distance6 = distance1 / 7
-print(distance6)  # Output: "Distance: 7.14 kilometers."
-
-print(distance1 < 60)  # Output: True
-print(distance1 > 120)  # Output: False
-print(distance1 == 100)  # Output: False
-print(distance1 <= 49)  # Output: False
-print(distance1 >= 50)  # Output: True
