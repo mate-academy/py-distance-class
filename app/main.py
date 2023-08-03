897988b3264a492513eab5fb8d524c750f7070b6
Roman Podolets <romanpyu@gmail.com>
2023-08-03 19:22:30 +0300

    Added Union & changed __iadd__

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
+from typing import Union
+
+distTypes = Union[int, float]
 
 
 class Distance:
-    def __init__(self, km: float | int) -> None:
+    def __init__(self, km: distTypes) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -11,39 +14,38 @@ class Distance:
     def __repr__(self) -> str:
         return "Distance(km={})".format(self.km)
 
-    def __add__(self, other: Distance | int | float) -> Distance:
-        value = other if isinstance(other, (int, float)) else other.km
-        return Distance(self.km + value)
-
-    def __iadd__(self, other: Distance | int | float) -> Distance:
-        value = other if isinstance(other, (int, float)) else other.km
+    def __add__(self, other: Distance | distTypes) -> Distance:
+        value = other if isinstance(other, distTypes) else other.km
         self.km += value
         return self
 
-    def __mul__(self, other: int | float) -> Distance:
+    def __iadd__(self, other: Distance | distTypes) -> Distance:
+        return self + other
+
+    def __mul__(self, other: distTypes) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int | float) -> Distance:
+    def __truediv__(self, other: distTypes) -> Distance:
         if other == 0:
             raise Exception("Division by zero")
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: Distance | int | float) -> bool:
-        value = other if isinstance(other, (int, float)) else other.km
+    def __lt__(self, other: Distance | distTypes) -> bool:
+        value = other if isinstance(other, distTypes) else other.km
         return self.km < value
 
-    def __gt__(self, other: Distance | int | float) -> bool:
-        value = other if isinstance(other, (int, float)) else other.km
+    def __gt__(self, other: Distance | distTypes) -> bool:
+        value = other if isinstance(other, distTypes) else other.km
         return self.km > value
 
-    def __eq__(self, other: Distance | int | float) -> bool:
-        value = other if isinstance(other, (int, float)) else other.km
+    def __eq__(self, other: Distance | distTypes) -> bool:
+        value = other if isinstance(other, distTypes) else other.km
         return self.km == value
 
-    def __le__(self, other: Distance | int | float) -> bool:
-        value = other if isinstance(other, (int, float)) else other.km
+    def __le__(self, other: Distance | distTypes) -> bool:
+        value = other if isinstance(other, distTypes) else other.km
         return self.km <= value
 
-    def __ge__(self, other: Distance | int | float) -> bool:
-        value = other if isinstance(other, (int, float)) else other.km
+    def __ge__(self, other: Distance | distTypes) -> bool:
+        value = other if isinstance(other, distTypes) else other.km
         return self.km >= value
