d166735118f23a9a3781704bc65a76d2543627ca
Taras Borshch <tarasborshch4@gmail.com>
2023-08-07 00:03:55 +0300

    changed some else statements

--- app/main.py
+++ app/main.py
@@ -14,16 +14,14 @@ class Distance:
     def __add__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        else:
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
     def __iadd__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             self.km = self.km + other.km
             return self
-        else:
-            self.km = self.km + other
-            return self
+        self.km = self.km + other
+        return self
 
     def __mul__(self, other: Distance | int) -> Distance:
         return Distance(self.km * other)
@@ -34,29 +32,24 @@ class Distance:
     def __lt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            return self.km > other
+        return self.km > other
 
     def __eq__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            return self.km == other
+        return self.km == other
 
     def __le__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        else:
-            return self.km <= other
+        return self.km <= other
 
     def __ge__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        else:
-            return self.km >= other
+        return self.km >= other
