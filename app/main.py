397b13d0b62d209221771d8d24448eecd8c5eafb
oreny <orenycdota@gmail.com>
2023-08-07 23:59:19 +0300

    Redefined base class methods to solve the problem. To work with both elements and numbers, a check for the type of the class "isinstance" was used

--- app/main.py
+++ app/main.py
@@ -1,3 +1,89 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance | int) -> Distance:
+        if isinstance(other, Distance):
+            distance_to_add = other.km
+        else:
+            distance_to_add = other
+        return Distance(
+            self.km + distance_to_add
+        )
+
+    def __iadd__(self, other: Distance | int) -> Distance:
+        if isinstance(other, Distance):
+            distance_to_add = other.km
+        else:
+            distance_to_add = other
+        self.km += distance_to_add
+        return self
+
+    def __mul__(self, other: int) -> Distance:
+        return Distance(
+            km=self.km * other
+        )
+
+    def __truediv__(self, other: int) -> Distance:
+        return Distance(
+            km=round(self.km / other, 2)
+        )
+
+    def __lt__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            distance_to_compare = other.km
+        else:
+            distance_to_compare = other
+        if self.km < distance_to_compare:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            distance_to_compare = other.km
+        else:
+            distance_to_compare = other
+        if self.km > distance_to_compare:
+            return True
+        else:
+            return False
+
+    def __eq__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            distance_to_compare = other.km
+        else:
+            distance_to_compare = other
+        if self.km == distance_to_compare:
+            return True
+        else:
+            return False
+
+    def __le__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            distance_to_compare = other.km
+        else:
+            distance_to_compare = other
+        if self.km <= distance_to_compare:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            distance_to_compare = other.km
+        else:
+            distance_to_compare = other
+        if self.km >= distance_to_compare:
+            return True
+        else:
+            return False
