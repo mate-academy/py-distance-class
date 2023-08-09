f7c585e3499bc82cecf5f8a3912647e55c3ba4a4
Denys <deibukdenyscloud@gmail.com>
2023-08-09 22:50:14 +0300

    Minor improvements have been added

--- app/main.py
+++ app/main.py
@@ -13,13 +13,13 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self: Self, distance: Union[Self, int, float]) -> Self:
+    def __add__(self: Self, distance: Union["Distance", int, float]) -> Self:
         if isinstance(distance, (int, float)):
             return self.__class__(self.km + distance)
         return self.__class__(self.km + distance.km)
 
     def __iadd__(self: Self, distance: Union[Self, int, float]) -> Self:
-        self.km = self.__add__(distance).km
+        self.km = (self + distance).km
         return self
 
     def __mul__(self: Self, num: Union[int, float]) -> Self:
@@ -29,26 +29,16 @@ class Distance:
         return self.__class__(round(self.km / num, 2))
 
     def __eq__(self, distance: Union[Self, int, float]) -> bool:
-        if isinstance(distance, (int, float)):
-            return self.km == distance
-        return self.km == distance.km
+        return self.km == distance
 
     def __gt__(self, distance: Union[Self, int, float]) -> bool:
-        if isinstance(distance, (int, float)):
-            return self.km > distance
-        return self.km > distance.km
+        return self.km > distance
 
     def __lt__(self, distance: Union[Self, int, float]) -> bool:
-        if isinstance(distance, (int, float)):
-            return self.km < distance
-        return self.km < distance.km
+        return self.km < distance
 
     def __ge__(self, distance: Union[Self, int, float]) -> bool:
-        if isinstance(distance, (int, float)):
-            return self.km >= distance
-        return self.km >= distance.km
+        return self.km >= distance
 
     def __le__(self, distance: Union[Self, int, float]) -> bool:
-        if isinstance(distance, (int, float)):
-            return self.km <= distance
-        return self.km <= distance.km
+        return self.km <= distance
