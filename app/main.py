a7f290b2834fcb5445aee1685b7bccdf5d27f7f2
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-09 18:54:11 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -35,8 +35,16 @@ class Distance:
             return self.km < other.km
         return self.km < other
 
+    def __gt__(self, other: Union[int, Distance]) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
+
     def __le__(self, other: Union[int, Distance]) -> bool:
         return not self > other
+
+    def __ge__(self, other: Union[int, Distance]) -> bool:
+        return not self < other
 
     def __eq__(self, other: Union[int, Distance]) -> bool:
         if isinstance(other, Distance):
