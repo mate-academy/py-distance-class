f81ab78bdadfc2a75625119059d4262daa3e96f8
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-09 18:43:55 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from typing import Union
 
 
 class Distance:
-    def __init__(self, km: float) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,12 +12,12 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[int, Distance]) -> Distance:
+    def __add__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: Union[int, Distance]) -> Distance:
+    def __iadd__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
@@ -44,13 +44,3 @@ class Distance:
         if isinstance(other, Distance):
             return self.km == other.km
         return self.km == other
-
-    def __gt__(self, other: Union[int, Distance]) -> bool:
-        if isinstance(other, Distance):
-            return self.km > other.km
-        return self.km > other
-
-    def __ge__(self, other: Union[int, Distance]) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        return self.km >= other
