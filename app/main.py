dcee14eea3fa3c7eed5f936291e464a378471f8b
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-08-10 12:53:52 +0200

    update after comments

--- app/main.py
+++ app/main.py
@@ -23,11 +23,13 @@ class Distance:
             self.km += other
         return self
 
-    def __mul__(self, other: Distance | int | float) -> Distance:
-        return Distance(self.km * other)
+    def __mul__(self, other: int | float) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(self.km * other)
 
-    def __truediv__(self, other: Distance | int | float) -> Distance:
-        return Distance(round(self.km / other, 2))
+    def __truediv__(self, other: int | float) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
@@ -45,11 +47,7 @@ class Distance:
         return self.km == other
 
     def __le__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        return self.km <= other
+        return not self > other
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        return self.km >= other
+        return not self < other
