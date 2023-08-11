1d659a5bd319017cdbcd55a5efba6ed80aa44472
Ostap Ivakh <o.ivakh95@gmail.com>
2023-08-11 16:31:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -48,10 +48,7 @@ class Distance:
         return self.km == other.km
 
     def __le__(self, other: int | float | Distance) -> bool:
-        if isinstance(other, (int, float)):
-            return self.km <= other
-
-        return self.km <= other.km
+        return not self > other
 
     def __ge__(self, other: int | float | Distance) -> bool:
         if isinstance(other, (int, float)):
