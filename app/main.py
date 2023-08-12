46024a9c8c45ab43f54945fff55be6eeda4118f4
Ostap <ostapbojko04@gmail.com>
2023-08-12 21:10:39 +0300

    py-distance(fixed3)

--- app/main.py
+++ app/main.py
@@ -14,9 +14,7 @@ class Distance:
     def __add__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
-            return Distance(self.km + other)
-        # raise TypeError("Unsupported operand type for +")
+        return Distance(self.km + other)
 
     def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
@@ -24,49 +22,41 @@ class Distance:
         elif isinstance(other, (int, float)):
             self.km += other
         return self
-        # raise TypeError("Unsupported operand type for +=")
 
     def __mul__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        # raise TypeError("Unsupported operand type for *")
 
     def __truediv__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
-        # raise TypeError("Unsupported operand type for /")
 
     def __lt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
-        # raise TypeError("Unsupported operand type for <")
 
     def __gt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
-        # raise TypeError("Unsupported operand type for >")
 
     def __eq__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
-        # raise TypeError("Unsupported operand type for ==")
 
     def __le__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
-        # raise TypeError("Unsupported operand type for <=")
 
     def __ge__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
             return self.km >= other
-        # raise TypeError("Unsupported operand type for >=")
