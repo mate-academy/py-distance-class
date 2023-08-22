ae29eee9523802e8ef715ca0e74fae51eed78980
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-08-22 17:37:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,58 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    @classmethod
+    def checkvalue(cls, other: "Distance") -> float:
+        if not isinstance(other, (int, float, Distance)):
+            raise TypeError
+        return other if isinstance(other, (int, float)) else other.km
+
+    def __add__(self, other: "Distance") -> "Distance":
+        sc = self.checkvalue(other)
+        return Distance(self.km + sc)
+
+    def __iadd__(self, other: "Distance") -> "Distance":
+        sc = self.checkvalue(other)
+        self.km += sc
+        return self
+
+    def __mul__(self, other: "Distance") -> "Distance":
+        if isinstance(other, (int, float)):
+            return Distance(self.km * other)
+        return None
+
+    def __truediv__(self, other: "Distance") -> "Distance":
+        if isinstance(other, (int, float)) and other != 0:
+            return Distance(round((self.km / other), 2))
+        return None
+
+    def __eq__(self, other: "Distance") -> bool:
+        sc = self.checkvalue(other)
+        return self.km == sc
+
+    def __gt__(self, other: "Distance") -> bool:
+        sc = self.checkvalue(other)
+        return self.km > sc
+
+    def __ge__(self, other: "Distance") -> bool:
+        sc = self.checkvalue(other)
+        return self.km >= sc
+
+    def __ne__(self, other: "Distance") -> bool:
+        sc = self.checkvalue(other)
+        return self.km != sc
+
+    def __lt__(self, other: "Distance") -> bool:
+        sc = self.checkvalue(other)
+        return self.km < sc
+
+    def __le__(self, other: "Distance") -> bool:
+        sc = self.checkvalue(other)
+        return self.km <= sc
