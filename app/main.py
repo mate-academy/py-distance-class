f897a219e098ef078f9d02076d1fabf9173955d3
Даня Пономаренко <you@example.com>
2023-08-29 18:22:10 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -15,25 +15,22 @@ class Distance:
         if isinstance(sec, int) or isinstance(sec, float):
             self.km += sec
             return self
-        else:
-            self.km += sec.km
-            return self
+        self.km += sec.km
+        return self
 
     def __iadd__(self, sec: int) -> Distance:
         if isinstance(sec, int) or isinstance(sec, float):
             self.km += sec
             return self
-        else:
-            self.km += sec.km
-            return self
+        self.km += sec.km
+        return self
 
     def __mul__(self, other: int) -> Distance:
         if isinstance(other, float):
             self.km *= other
             return self
-        else:
-            self.km *= other
-            return self
+        self.km *= other
+        return self
 
     def __truediv__(self, other: int) -> Distance:
         if isinstance(other, int) or isinstance(other, float):
@@ -53,8 +50,7 @@ class Distance:
     def __eq__(self, other: float) -> bool:
         if isinstance(other, int) or isinstance(other, float):
             return self.km == other
-        else:
-            return self.km == other.km
+        return self.km == other.km
 
     def __le__(self, other: float) -> bool:
         if isinstance(other, int) or isinstance(other, float):
