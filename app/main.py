22003678ca0beefc8780272b81bc5f9be6ae9443
Oleksandr Asauliak <varmen67@gmail.com>
2023-09-07 07:17:20 +0300

    try1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
 class Distance:
-    # Write your code here
+
+    def __init__(self, km: int):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers"
+
+    def __repr__(self):
+        return f"Distance({self.km} km)"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            sum_km = self.km + other.km
+            return Distance(sum_km)
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+
     pass
