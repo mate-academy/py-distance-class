3ccf8606c5ea1a4854b38d3c1bb46fd6bdde6e4a
asdadaversa <peikqudakeus@gmail.com>
2023-09-08 15:36:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ class Distance:
         return self.km is other
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        return not self.__lt__(other)
+        return not self < other
 
     def __le__(self, other: Distance | int | float) -> bool:
-        return not self.__gt__(other)
+        return not self > other
