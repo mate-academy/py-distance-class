41e31346f41f6600d2c478b685fc6aa43a1af884
Andrey Britov <abritov82@gmail.com>
2023-09-11 13:36:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,82 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other) -> object:
+        if isinstance(other, Distance):
+            return Distance(km=self.km + other.km)
+        else:
+            return Distance(km=self.km + other)
+
+    def __iadd__(self, other) -> object:
+        if isinstance(other, Distance):
+            self.km = self.km + other.km
+            return self.km
+        else:
+            self.km = self.km + other
+            return self.km
+
+    def __mul__(self, other) -> object:
+        return Distance(km=self.km * other)
+
+    def __truediv__(self, other) -> object:
+        if other != 0:
+            res_km = round((self.km / other), 2)
+            return Distance(km=res_km)
+
+    def __lt__(self, other):
+        if isinstance(other, int):
+            return self.km < other
+        else:
+            return self.km < other.km
+
+    def __gt__(self, other):
+        if isinstance(other, int):
+            return self.km > other
+        else:
+            return self.km > other.km
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.km == other
+        else:
+            return self.km == other.km
+
+    def __le__(self, other):
+        if isinstance(other, int):
+            return self.km <= other
+        else:
+            return self.km <= other.km
+
+    def __ge__(self, other):
+        if isinstance(other, int):
+            return self.km >= other
+        else:
+            return self.km >= other.km
+
+
+distance = Distance(20)
+print(distance)
+print(repr(distance))
+distance1 = Distance(30)
+distance2 = distance1 + distance
+print(distance2.km)
+distance3 = distance + 10
+print(distance3.km)
+distance += distance1
+print(distance)
+distance += 30
+print(distance)
+distance4 = distance * 5
+print(distance4)
+distance5 = distance / 10
+print(distance5)
+distance = Distance(20)
+print(distance < Distance(10))
