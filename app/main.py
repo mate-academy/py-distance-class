71264ea705d0501e6913c11e32e551e5d4c1523d
Roman Shupak <romanshupak808@gmail.com>
2023-09-18 20:38:37 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,77 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance | int | float) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        else:
+            raise TypeError(
+                f"can't multiply sequence by non-int of type"
+                f" 'Distance': {other}"
+            )
+
+    def __iadd__(self, other: int | float | Distance) -> Distance:
+        if isinstance(other, (int, float)):
+            self.km += other
+            return self
+        elif isinstance(other, Distance):
+            self.km += other.km
+            return self
+        else:
+            raise TypeError(
+                f"unsupported operand type(s)"
+                f" for +=: 'Distance' and {type(other).__name__}"
+            )
+
+    def __mul__(self, other: int | float) -> Distance:
+        if isinstance(other, int | float):
+            return Distance(self.km * other)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km * other)
+        raise TypeError(
+            f"unsupported operand type(s)"
+            f" for *: 'Distance' and '{type(other)}'"
+        )
+
+    def __truediv__(self, other: int | float | Distance) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        elif isinstance(other, (int, float)):
+            return self.km < other
+
+    def __gt__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        elif isinstance(other, (int, float)):
+            return self.km > other
+
+    def __eq__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        elif isinstance(other, (int, float)):
+            return self.km == other
+
+    def __le__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        elif isinstance(other, (int, float)):
+            return self.km <= other
+
+    def __ge__(self, other: float) -> bool:
+        return self.km >= other
