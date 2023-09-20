8e4aa3118d83de77df607d9d666c247fb3d511b8
evgenijnozdrenko <evgennozdrenko@MacBook-Pro-Evgen.local>
2023-09-20 12:49:10 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,74 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: (int, float)) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: (Distance, int, float)) -> Distance:
+        if isinstance(other, Distance):
+            new_km = self.km + other.km
+            return Distance(new_km)
+        elif isinstance(other, (int, float)):
+            new_km = self.km + other
+            return Distance(new_km)
+        else:
+            raise TypeError("Неможливо додати об'єкт.")
+
+    def __iadd__(self, other: (Distance, int, float)) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        elif isinstance(other, (int, float)):
+            self.km += other
+            return self
+        else:
+            raise TypeError("Неможливо виконати операцію")
+
+    def __mul__(self, other: (Distance, int, float)) -> Distance:
+        if isinstance(other, (int, float)):
+            new_km = self.km * other
+            return Distance(new_km)
+        else:
+            raise TypeError("Неможливо перемножити об'єкт.")
+
+    def __truediv__(self, other: (int, float)) -> Distance:
+        if isinstance(other, (int, float)):
+            if other == 0:
+                raise ZeroDivisionError("Ділення на нуль неможливе.")
+            new_km = self.km / other
+            return Distance(round(new_km, 2))
+        else:
+            raise TypeError("Неможливо поділити об'єкт.")
+
+    def __eq__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        elif isinstance(other, (int, float)):
+            return self.km == other
+        else:
+            return False
+
+    def __lt__(self, other: (Distance, int, float)) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        if isinstance(other, (int, float)):
+            return self.km < other
+
+    def __le__(self, other: (Distance, int, float)) -> bool:
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other: (Distance, int, float)) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        if isinstance(other, (int, float)):
+            return self.km > other
+
+    def __ge__(self, other: (Distance, int, float)) -> bool:
+        return self.__gt__(other) or self.__eq__(other)
