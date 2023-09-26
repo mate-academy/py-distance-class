ab0e1c6edab79298eb1a8638be4477a573aa0c26
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-09-26 14:08:15 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,46 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: "Distance" or int) -> "Distance":
+        try:
+            return Distance(self.km + other)
+        except TypeError:
+            return Distance(self.km + other.km)
+
+    def __iadd__(self, other: "Distance" or int) -> "Distance":
+
+        try:
+            self.km += other
+        except TypeError:
+            self.km += other.km
+
+        return self
+
+    def __mul__(self, other: float or int) -> "Distance":
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: float or int) -> "Distance":
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: int or "Distance") -> bool:
+        return self.km < other
+
+    def __gt__(self, other: int or "Distance") -> bool:
+        return self.km > other
+
+    def __eq__(self, other: int or "Distance") -> bool:
+        return self.km == other
+
+    def __le__(self, other: int or "Distance") -> bool:
+        return self.km <= other
+
+    def __ge__(self, other: int or "Distance") -> bool:
+        return self.km >= other
