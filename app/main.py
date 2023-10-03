cc7c1a6357fa320ed5f7b4d4d0a3beb7fea7240e
Ruslan <135342393+MiskoRuslan@users.noreply.github.com>
2023-10-03 22:25:49 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -8,12 +11,12 @@ class Distance:
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __add__(self, other: "Distance" | int) -> "Distance":
+    def __add__(self, other: Union["Distance", int]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: "Distance" | int) -> "Distance":
+    def __iadd__(self, other: Union["Distance", int]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
