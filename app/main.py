f5e4247e3d74cc11650b1ba264dbc2564c47f6e8
Vladyslav Pidborskyi <pidborskii17@gmail.com>
2023-10-03 14:39:53 +0300

    Init commit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: "Distance") -> "Distance":
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other: "Distance") -> "Distance":
+        return self + other
+
