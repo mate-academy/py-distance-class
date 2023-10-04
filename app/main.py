a367c772648bb9f19e47981f0723f3d8af49b093
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-04 21:38:34 +0300

    add isinstance

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
 
     def __init__(self, km: int) -> None:
@@ -9,38 +12,38 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance" or int) -> "Distance":
+    def __add__(self, other: Distance or int) -> Distance:
         try:
             return Distance(self.km + other)
         except TypeError:
             return Distance(self.km + other.km)
 
-    def __iadd__(self, other: "Distance" or int) -> "Distance":
-
-        try:
-            self.km += other
-        except TypeError:
+    def __iadd__(self, other: Distance or int or float) -> Distance:
+        if isinstance(other, Distance):
             self.km += other.km
-
+        else:
+            self.km += other
         return self
 
-    def __mul__(self, other: float or int) -> "Distance":
+    def __mul__(self, other: float or int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float or int) -> "Distance":
+    def __truediv__(self, other: float or int) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: int or "Distance") -> bool:
+    def __lt__(self, other: int or Distance) -> bool:
         return self.km < other
 
-    def __gt__(self, other: int or "Distance") -> bool:
+    def __gt__(self, other: int or Distance) -> bool:
         return self.km > other
 
-    def __eq__(self, other: int or "Distance") -> bool:
+    def __eq__(self, other: int or float) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
         return self.km == other
 
-    def __le__(self, other: int or "Distance") -> bool:
+    def __le__(self, other: int or Distance) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: int or "Distance") -> bool:
+    def __ge__(self, other: int or Distance) -> bool:
         return self.km >= other
