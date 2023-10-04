d0fa7c8260d09a76b09b0095d49b9bd893241729
Hryhoriy Bilous <hryhoriy.bilous@gmail.com>
2023-10-04 15:29:33 +0300

    Solution resolution v2.0

--- app/main.py
+++ app/main.py
@@ -11,16 +11,14 @@ class Distance:
     def __add__(self, other: int) -> None:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        else:
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
     def __iadd__(self, other: int) -> None:
         if isinstance(other, Distance):
             self.km += other.km
             return self
-        else:
-            self.km += other
-            return self
+        self.km += other
+        return self
 
     def __mul__(self, other: int) -> None:
         if isinstance(other, (int, float)):
@@ -32,32 +30,27 @@ class Distance:
     def __lt__(self, other: int) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: int) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            return self.km > other
+        return self.km > other
 
     def __eq__(self, other: int) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            return self.km == other
+        return self.km == other
 
     def __le__(self, other: int) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        else:
-            return self.km <= other
+        return self.km <= other
 
     def __ge__(self, other: int) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        else:
-            return self.km >= other
+        return self.km >= other
 
 
 distance1 = Distance(20)
