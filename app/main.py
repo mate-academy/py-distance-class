0980f0474de2c21fd5f6a5d133bb002e54b70bc5
Danylo Sheremeta <danylo3002@gmail.com>
2023-10-05 16:28:20 +0300

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -31,21 +31,26 @@ class Distance:
         return Distance(km=round(self.km / other, 2))
 
     def __eq__(self, other: float | int | Distance) -> bool:
-        return self.km == other.km if isinstance(other, Distance) \
-            else self.km == other
+        return (self.km == other.km
+                if isinstance(other, Distance)
+                else self.km == other)
 
     def __lt__(self, other: float | int | Distance) -> bool:
-        return self.km < other.km if isinstance(other, Distance) \
-            else self.km < other
+        return (self.km < other.km
+                if isinstance(other, Distance)
+                else self.km < other)
 
     def __le__(self, other: float | int | Distance) -> bool:
-        return self.km <= other.km if isinstance(other, Distance) \
-            else self.km <= other
+        return (self.km <= other.km
+                if isinstance(other, Distance)
+                else self.km <= other)
 
     def __gt__(self, other: float | int | Distance) -> bool:
-        return self.km > other.km if isinstance(other, Distance) \
-            else self.km > other
+        return (self.km > other.km
+                if isinstance(other, Distance)
+                else self.km > other)
 
     def __ge__(self, other: float | int | Distance) -> bool:
-        return self.km >= other.km if isinstance(other, Distance) \
-            else self.km >= other
+        return (self.km >= other.km
+                if isinstance(other, Distance)
+                else self.km >= other)
