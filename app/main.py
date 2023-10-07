f8266ac968fe3c5e40267fa2c15bb5cf9b308fa3
Aeros72 <pashamelnyk2004@gmail.com>
2023-10-07 13:15:08 +0300

    Implemented py-distance-class

--- app/main.py
+++ app/main.py
@@ -45,11 +45,7 @@ class Distance:
         return self.km == other
 
     def __le__(self, other: Distance | float | int) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        return self.km <= other
+        return not self > other
 
     def __ge__(self, other: Distance | float | int) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        return self.km >= other
+        return not self < other
