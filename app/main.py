cc0b0149e64f3f2ddd4f737c3f96755a3044aa43
Dima <dima.purig@gmail.com>
2023-10-10 13:28:18 +0300

    Removed no need annotations

--- app/main.py
+++ app/main.py
@@ -24,13 +24,11 @@ class Distance:
             self.km += other
         return self
 
-    def __mul__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, (int, float)):
-            return Distance(self.km * other)
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
 
-    def __truediv__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, (int, float)):
-            return Distance(round((self.km / other), 2))
+    def __truediv__(self, other: int | float) -> Distance:
+        return Distance(round((self.km / other), 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
