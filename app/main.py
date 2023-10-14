53494b8d34cf858ac8011083ca48b38cb1e71c5b
Yuliia Dudych <juliadudych@gmail.com>
2023-10-14 11:11:34 +0300

    Define Distance method and its string representation methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
