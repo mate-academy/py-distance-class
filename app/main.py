826ba0b1574a914ec7b37847138dad95337b3a8c
Ivan <mozharivan17@gmail.com>
2023-10-17 11:25:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,59 @@
+from __future__ import annotations
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Any) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __iadd__(self, other: Any) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        elif isinstance(other, int | float):
+            self.km += other
+        return self
+
+    def __mul__(self, factor: int | float) -> Distance:
+        if isinstance(factor, (int, float)):
+            return Distance(self.km * factor)
+
+    def __truediv__(self, divisor: int | float) -> Distance:
+        if isinstance(divisor, (int, float)):
+            result_km = self.km / divisor
+            return Distance(round(result_km, 2))
+
+    def __lt__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        return self.km < other
+
+    def __gt__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        return self.km == other
+
+    def __le__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        return self.km <= other
+
+    def __ge__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return self.km >= other
