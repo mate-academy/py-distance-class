71052f6efc6116e13afa5eb8e74a0540a90fffdd
Hermesvlad <hermesvlad@gmail.com>
2023-10-21 13:42:08 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -24,40 +24,33 @@ class Distance:
             self.km += other
         return self
 
-    def __mul__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, (int, float)):
-            return Distance(self.km * other)
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
 
-    def __truediv__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, (int, float)):
-            return Distance(round(self.km / other, 2))
+    def __truediv__(self, other: int | float) -> Distance:
+        return Distance(round(self.km / other, 2))
 
     def __eq__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, (int, float)):
-            return self.km == other
+        return self.km == other
 
     def __lt__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif isinstance(other, (int, float)):
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif isinstance(other, (int, float)):
-            return self.km > other
+        return self.km > other
 
     def __le__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif isinstance(other, (int, float)):
-            return self.km <= other
+        return self.km <= other
 
     def __ge__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif isinstance(other, (int, float)):
-            return self.km >= other
+        return self.km >= other
