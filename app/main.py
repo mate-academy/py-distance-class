344488ffc3831ecfe1b6d9e66b3a3f1fa7bddbd5
Grigoriy Gesiod <g.gesiod@gmail.com>
2023-10-24 17:26:27 +0300

    start

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 class Distance:
-    # Write your code here
-    pass
+    def __int__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+
