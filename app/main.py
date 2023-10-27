142f1be0e33028b1463fe32352729f233e0924c2
xboris <boris.tikhonov.21@gmail.com>
2023-10-27 13:07:55 +0300

    fix annotation for other

--- app/main.py
+++ app/main.py
@@ -12,19 +12,19 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __lt__(self, other: int) -> bool:
+    def __lt__(self, other: int | float | Distance) -> bool:
         return True if self.km < other else False
 
-    def __gt__(self, other: int) -> bool:
+    def __gt__(self, other: int | float | Distance) -> bool:
         return True if self.km > other else False
 
-    def __le__(self, other: int) -> bool:
+    def __le__(self, other: int | float | Distance) -> bool:
         return True if self.km <= other else False
 
-    def __ge__(self, other: int) -> bool:
+    def __ge__(self, other: int | float | Distance) -> bool:
         return True if self.km >= other else False
 
-    def __eq__(self, other: int) -> bool:
+    def __eq__(self, other: int | float | Distance) -> bool:
         return True if self.km == other else False
 
     def __add__(self, other: int | float | Distance) -> Distance:
@@ -41,8 +41,8 @@ class Distance:
             self.km += other.km
             return self
 
-    def __truediv__(self, other: int) -> Distance:
+    def __truediv__(self, other: int | float | Distance) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __mul__(self, other: int) -> Distance:
+    def __mul__(self, other: int | float | Distance) -> Distance:
         return Distance(self.km * other)
