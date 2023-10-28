3be652dcd6792c4da83685528c3de7ad19e5d184
Marinel444 <380978907444m@gmail.com>
2023-10-28 16:09:56 +0300

    add not in __ge__ and __le__

--- app/main.py
+++ app/main.py
@@ -20,10 +20,9 @@ class Distance:
     def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
-            return self
         elif isinstance(other, (int, float)):
             self.km += other
-            return self
+        return self
 
     def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
@@ -53,12 +52,12 @@ class Distance:
 
     def __le__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
-            return self.km <= other.km
+            return not self.km > other.km
         elif isinstance(other, (int, float)):
-            return self.km <= other
+            return not self > other
 
     def __ge__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
-            return self.km >= other.km
+            return not self.km < other.km
         elif isinstance(other, (int, float)):
-            return self.km >= other
+            return not self.km < other
