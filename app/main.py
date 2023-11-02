1d6b6adbe049d4a2e1cc12ea1130cca0d80f6461
Many <many.fasy@gmail.com>
2023-11-02 18:18:17 +0200

    'fix_logic_type'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -8,30 +11,33 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: float) -> object:
+    def __add__(self, other: Union[int, float, "Distance"]) -> object:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: float) -> object:
+    def __mul__(self, other: int | float) -> object:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float) -> object:
+    def __truediv__(self, other: int | float) -> object:
         return Distance(round(self.km / other, 2))
 
-    def __eq__(self, other: float) -> bool:
-        return self.km == other
+    def __eq__(self, other: Union[int, float, "Distance"]) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        elif isinstance(other, (int, float)):
+            return self.km == other
 
-    def __lt__(self, other: float) -> bool:
+    def __lt__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km < other
 
-    def __gt__(self, other: float) -> bool:
+    def __gt__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km > other
 
-    def __le__(self, other: float) -> bool:
+    def __le__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: float) -> bool:
+    def __ge__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km >= other
