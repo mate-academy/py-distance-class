40015ce85c7fd90d221da6ca81c43f5130cd5ff5
Many <many.fasy@gmail.com>
2023-11-03 15:24:23 +0200

    'SolutionFixTypeAnnotation'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from __future__ import annotations
 
 
 class Distance:
@@ -11,33 +11,33 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[int, float, "Distance"]) -> object:
+    def __add__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: int | float) -> object:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int | float) -> object:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __eq__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __eq__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
 
-    def __lt__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __lt__(self, other: int | float | Distance) -> bool:
         return self.km < other
 
-    def __gt__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __gt__(self, other: int | float | Distance) -> bool:
         return self.km > other
 
-    def __le__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __le__(self, other: int | float | Distance) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: Union[int, float, "Distance"]) -> bool:
+    def __ge__(self, other: int | float | Distance) -> bool:
         return self.km >= other
