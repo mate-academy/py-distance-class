60b3c817a57789a3ca950f4e0dcabfe2f3bde99b
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2023-11-03 22:08:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
+
 class Distance:
-    def __init__(self, km: float) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -8,7 +11,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -16,28 +19,28 @@ class Distance:
         else:
             raise TypeError("Unsupported operand type")
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
             self.km += other
         else:
             raise TypeError("Unsupported operand type")
-        return self  # Вернем тот же экземпляр
+        return self
 
-    def __mul__(self, other: "Distance") -> "Distance":
+    def __mul__(self, other: Distance | int) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         else:
             raise TypeError("Unsupported operand type")
 
-    def __truediv__(self, other: "Distance") -> "Distance":
+    def __truediv__(self, other: Distance | int) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
         else:
             raise TypeError("Unsupported operand type")
 
-    def __lt__(self, other: "Distance") -> bool:
+    def __lt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
@@ -45,7 +48,7 @@ class Distance:
         else:
             raise TypeError("Unsupported operand type")
 
-    def __gt__(self, other: "Distance") -> bool:
+    def __gt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
@@ -53,7 +56,7 @@ class Distance:
         else:
             raise TypeError("Unsupported operand type")
 
-    def __eq__(self, other: "Distance") -> bool:
+    def __eq__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
@@ -61,7 +64,7 @@ class Distance:
         else:
             raise TypeError("Unsupported operand type")
 
-    def __le__(self, other: "Distance") -> bool:
+    def __le__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
@@ -69,7 +72,7 @@ class Distance:
         else:
             raise TypeError("Unsupported operand type")
 
-    def __ge__(self, other: "Distance") -> bool:
+    def __ge__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
