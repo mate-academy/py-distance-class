476191509f1f068a97015cb91a2c1b6cccdf4404
Vlad <mr.darmstadtium@gmail.com>
2023-11-04 11:00:42 +0100

    pass all test's

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
+
 class Distance:
 
     def __init__(self, km: int) -> None:
+
         self.km = km
 
     def __str__(self) -> str:
+
         return f"Distance: {self.km} kilometers."
 
     def __repr__(self) -> str:
+
         return f"Distance(km={self.km})"
 
     def __add__(self, other: (Distance, int)) -> Distance:
@@ -16,46 +20,59 @@ class Distance:
         if isinstance(other, Distance):
             other = other.km
 
-        return Distance(self.km + other)
+        new_km_amount = self.km + other
 
-    def __iadd__(self, other: (Distance, int)) -> None:
+        return Distance(new_km_amount)
+
+    def __iadd__(self, other: (Distance, int, float)) -> (Distance, int, float):
 
         if isinstance(other, Distance):
             other = other.km
 
         self.km += other
 
+        return self
+
     def __mul__(self, other: int) -> Distance:
-        return Distance(self.km * other)
+
+        new_km_amount = self.km * other
+
+        return Distance(new_km_amount)
 
     def __truediv__(self, other: (int, float)) -> Distance:
+
         return Distance(round((self.km / other), 2))
 
     def __lt__(self, other: (Distance, int)) -> bool:
+
         if isinstance(other, Distance):
             other = other.km
 
         return self.km < other
 
     def __gt__(self, other: (Distance, int)) -> bool:
+
         if isinstance(other, Distance):
             other = other.km
 
         return self.km > other
 
     def __eq__(self, other: (Distance, int)) -> bool:
+
         if isinstance(other, Distance):
             other = other.km
 
         return self.km == other
 
     def __le__(self, other: (Distance, int)) -> bool:
+
         if isinstance(other, Distance):
             other = other.km
 
         return self.km <= other
 
     def __ge__(self, other: (Distance, int)) -> bool:
+
         if isinstance(other, Distance):
             other = other.km
 
