4a88af689eee69baa546d3b1bd02fe9e915ffedf
Mikola Rozhansky <bymikola@gmail.com>
2023-11-15 21:03:26 +0200

    not work 1

--- app/main.py
+++ app/main.py
@@ -8,7 +8,70 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return Distance(km=self.km + other.km)
+        return Distance(km=self.km + other)
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km += other.km
+            return Distance(km=self.km)
+        self.km += other
+        return Distance(km=self.km)
+
+    def __mul__(self, other):
+        # if isinstance(other, Distance):
+        #     return Distance(km=self.km * other.km)
+        return Distance(km=self.km * other)
+
+    def __truediv__(self, other):
+        # if isinstance(other, Distance):
+        #     return Distance(km=self.km * other.km)
+        return Distance(km=round(self.km / other, 2))
+
+    def __lt__(self, other):
+        if isinstance(other, Distance):
+            return self.km < other.km
+        return self.km < other
+
+    def __gt__(self, other):
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
+
+    def __le__(self, other):
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        return self.km <= other
+
+    def __ge__(self, other):
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return self.km >= other
+
+    def __eq__(self, other):
+        if isinstance(other, Distance):
+            return self.km == other.km
+        return self.km == other
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
