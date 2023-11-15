1bf7375d666715375a24caa7c4fc9f5402ba6a70
Mikola Rozhansky <bymikola@gmail.com>
2023-11-15 19:59:30 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def
+
+
+
+
+
+
