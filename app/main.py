9984fe0f17818ee13d6fc138379e88d9f3deb3bf
Viktor Shcherbak <dev.psychologist@gmail.com>
2023-11-26 12:00:11 +0200

    Class magic methods!

--- app/main.py
+++ app/main.py
@@ -1,3 +1,62 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: None) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return "Distance(km=" + str(self.km) + ")"
+
+    def __add__(self, other: None) -> object:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __iadd__(self, other: None) -> object:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other: None) -> object:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: None) -> object:
+        result = self.km / other
+        if isinstance(other, Distance):
+            result = self.km / other.km
+        return Distance(round(result, 2))
+
+    def __lt__(self, other: None) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        else:
+            return self.km < other
+
+    def __gt__(self, other: None) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        else:
+            return self.km > other
+
+    def __eq__(self, other: None) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        else:
+            return self.km == other
+
+    def __le__(self, other: None) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        else:
+            return self.km <= other
+
+    def __ge__(self, other: None) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        else:
+            return self.km >= other
