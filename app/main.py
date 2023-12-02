0c825979243bf60e5f2a7fdcbeefa117f1af1a26
Dmytro Semenets <dmitriymine@gmail.com>
2023-12-02 17:25:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -9,18 +9,13 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, second: any) -> any:
-        if type(second) == int:
-            return Distance(self.km + second)
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             return Distance(self.km + second)
         else:
             return Distance(self.km + second.km)
 
     def __iadd__(self, second: any) -> any:
-        if type(second) == int:
-            self.km += second
-            return self
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             self.km += second
             return self
         else:
@@ -28,57 +23,43 @@ class Distance:
             return self
 
     def __mul__(self, second: any) -> any:
-        if type(second) == int:
-            return Distance(self.km * second)
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             return Distance(self.km * second)
         else:
             pass
 
     def __truediv__(self, second: any) -> any:
-        if type(second) == int:
-            return Distance(round(self.km / second, 2))
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             return Distance(round(self.km / second, 2))
         else:
             pass
 
     def __eq__(self, second: any) -> any:
-        if type(second) == int:
-            return self.km == second
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             return self.km == second
         else:
             return self.km == second.km
 
     def __lt__(self, second: any) -> any:
-        if type(second) == int:
-            return self.km < second
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             return self.km < second
         else:
             return self.km < second.km
 
     def __gt__(self, second: any) -> any:
-        if type(second) == int:
-            return self.km > second
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             return self.km > second
         else:
             return self.km > second.km
 
     def __le__(self, second: any) -> any:
-        if type(second) == int:
-            return self.km <= second
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             return self.km <= second
         else:
             return self.km <= second.km
 
     def __ge__(self, second: any) -> any:
-        if type(second) == int:
-            return self.km >= second
-        elif type(second) == float:
+        if type(second) == int or type(second) == float:
             return self.km >= second
         else:
             return self.km >= second.km
