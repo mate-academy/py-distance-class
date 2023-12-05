edafe83d50ddff99a6c8bdaa51622b42da04689c
Oleksandr <binsasha97@gmail.com>
2023-12-05 12:21:21 +0200

    'one-more'

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ class Distance:
     def __mul__(self, other: int | float) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other:  int | float) -> "Distance":
+    def __truediv__(self, other: int | float) -> "Distance":
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: type["Distance"] | int | float) -> bool:
