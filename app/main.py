6b092393b033c57aabdac019b626e19e849ce9b3
Max <bidun.max@gmail.com>
2023-12-07 23:12:44 +0200

    Solutin

--- app/main.py
+++ app/main.py
@@ -1,3 +1,55 @@
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Any) -> callable:
+        if not isinstance(other, Distance):
+            return Distance(self.km + other)
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other: Any) -> callable:
+        if not isinstance(other, Distance):
+            self.km += other
+            return self
+        self.km += other.km
+        return self
+
+    def __mul__(self, other: Any) -> callable:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: Any) -> callable:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: Any) -> callable:
+        if not isinstance(other, Distance):
+            return self.km < other
+        return self.km < other.km
+
+    def __gt__(self, other: Any) -> callable:
+        if not isinstance(other, Distance):
+            return self.km > other
+        return self.km > other.km
+
+    def __eq__(self, other: Any) -> callable:
+        if not isinstance(other, Distance):
+            return self.km == other
+        return self.km == other.km
+
+    def __le__(self, other: Any) -> callable:
+        if not isinstance(other, Distance):
+            return self.km <= other
+        return self.km <= other.km
+
+    def __ge__(self, other: Any) -> callable:
+        if not isinstance(other, Distance):
+            return self.km >= other
+        return self.km >= other.km
