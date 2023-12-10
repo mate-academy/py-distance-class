819be42648815f4daed35127545621386e185372
Andrii Zubchenko <dissom1987@gmail.com>
2023-12-10 17:06:08 +0200

    Changed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
 
     def __init__(self, km: int | float) -> None:
@@ -9,20 +12,20 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float) -> "Distance":
+    def __add__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
 
-    def __iadd__(self, other: int | float) -> "Distance":
+    def __iadd__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
             self.km += other
         return self
 
-    def __mul__(self, other: int | float) -> "Distance":
+    def __mul__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
 
@@ -56,7 +59,7 @@ class Distance:
         elif isinstance(other, (int, float)):
             return self.km >= other
 
-    def __truediv__(self, other: int | float) -> "Distance":
+    def __truediv__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, (int, float)):
             if other != 0:
                 return Distance(round((self.km / other), 2))
