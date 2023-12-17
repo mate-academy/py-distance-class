1986c34eda677850ca4c28939363ca3a9adf21f5
cyb1ove <cyb1ove.ua@gmail.com>
2023-12-17 12:38:22 +0200

    task completed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,45 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+    
+    @staticmethod
+    def get_addend(other) -> int:
+        return other.km if isinstance(other, Distance) else other
+    
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance | int) -> Distance:
+        return Distance(self.km + Distance.get_addend(other))
+
+    def __iadd__(self, other: Distance | int) -> Distance:
+        self.km += Distance.get_addend(other)
+        return self
+
+    def __mul__(self, factor: int) -> Distance:
+        return Distance(self.km * factor)
+
+    def __truediv__(self, divider: int) -> Distance:
+        return Distance(round(self.km / divider, 2))
+
+    def __lt__(self, other: int) -> bool:
+        return self.km < Distance.get_addend(other)
+
+    def __gt__(self, other: int) -> bool:
+        return self.km > Distance.get_addend(other)
+
+    def __eq__(self, other: int) -> bool:
+        return self.km == Distance.get_addend(other)
+
+    def __le__(self, other: int) -> bool:
+        return self.km <= Distance.get_addend(other)
+
+    def __ge__(self, other: int) -> bool:
+        return self.km >= Distance.get_addend(other)
+
