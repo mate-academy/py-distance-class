71a3349f50be15bf4e459872c64776fbcae90430
Arsen Rudnytskyy <acikz.33@google.com>
2023-12-18 21:43:58 +0100

    Remove redundant else

--- app/main.py
+++ app/main.py
@@ -15,8 +15,7 @@ class Distance:
     def __add__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(km=self.km + other.km)
-        else:
-            return Distance(km=self.km + other)
+        return Distance(km=self.km + other)
 
     def __iadd__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
@@ -34,29 +33,24 @@ class Distance:
     def __eq__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            return self.km == other
+        return self.km == other
 
     def __lt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            return self.km > other
+        return self.km > other
 
     def __le__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        else:
-            return self.km <= other
+        return self.km <= other
 
     def __ge__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        else:
-            return self.km >= other
+        return self.km >= other
