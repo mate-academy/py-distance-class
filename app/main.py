2793d153a71e9398bd9fdc098313ebd72c1be287
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2023-12-25 17:17:46 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,67 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance | int) -> Distance:
+        if isinstance(other, Distance):
+            total = self.km + other.km
+        else:
+            total = self.km + other
+        return Distance(total)
+
+    def __iadd__(self, other: Distance | int) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other: int) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int) -> Distance:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: Distance | int) -> bool:
+        if isinstance(other, Distance):
+            result = self.km < other.km
+        else:
+            result = self.km < other
+        return result
+
+    def __gt__(self, other: Distance | int) -> bool:
+        if isinstance(other, Distance):
+            result = self.km > other.km
+        else:
+            result = self.km > other
+        return result
+
+    def __eq__(self, other: Distance | int) -> bool:
+        if isinstance(other, Distance):
+            result = self.km == other.km
+        else:
+            result = self.km == other
+        return result
+
+    def __le__(self, other: Distance | int) -> bool:
+        if isinstance(other, Distance):
+            result = self.km <= other.km
+        else:
+            result = self.km <= other
+        return result
+
+    def __ge__(self, other: Distance | int) -> bool:
+        if isinstance(other, Distance):
+            result = self.km >= other.km
+        else:
+            result = self.km >= other
+        return result
