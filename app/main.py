94b08a5c5ceaab78e1bb070a04f9dfb2df3dbdf2
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-01-07 00:10:42 +0200

    flake 8 edit

--- app/main.py
+++ app/main.py
@@ -27,31 +27,31 @@ class Distance:
     def __truediv__(self, divisor: float) -> "Distance":
         return Distance(round(self.km / divisor, 2))
 
-    def __lt__(self, other: 'Distance') -> bool:
+    def __lt__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
 
-    def __gt__(self, other: 'Distance') -> bool:
+    def __gt__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
 
-    def __eq__(self, other: 'Distance') -> bool:
+    def __eq__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
 
-    def __le__(self, other: 'Distance') -> bool:
+    def __le__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
 
-    def __ge__(self, other: 'Distance') -> bool:
+    def __ge__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
