0245cf5368776ead17530e1a98f9977b0fead124
Stepan Stelmakhovych <stepan.stelmakhovych@gmail.com>
2024-01-06 11:49:57 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ class Distance:
     def __lt__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        return self.km < other
+        if isinstance(other, (int, float)):
+            return self.km < other
 
     def __le__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
