c6b957055ddee7e7dd7ffecd597e5fc9a5390948
PythonZem <pythonzem@gmail.com>
2024-01-10 15:38:10 +0200

    implement comment from mentor

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 class Distance:
-    def __init__(self, km: float) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -11,44 +11,36 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance) -> Distance:
-        if isinstance(other, Distance):
-            other = other.km
-        return Distance(km=self.km + other)
+    @staticmethod
+    def convertToNumber(obj: Distance | float | int) -> float | int:
+        if isinstance(obj, Distance):
+            obj = obj.km
+        return obj
 
-    def __iadd__(self, other: Distance) -> Distance:
-        if isinstance(other, Distance):
-            other = other.km
-        self.km += other
+    def __add__(self, other: Distance | float | int) -> Distance:
+        return Distance(km=self.km + self.convertToNumber(obj=other))
+
+    def __iadd__(self, other: Distance | float) -> Distance:
+        self.km += self.convertToNumber(obj=other)
         return self
 
     def __mul__(self, other: float) -> Distance:
         return Distance(km=self.km * other)
 
-    def __truediv__(self, other: float) -> Distance:
+    def __truediv__(self, other: float | int) -> Distance:
         return Distance(km=round(self.km / other, 2))
 
-    def __lt__(self, other: Distance) -> bool:
-        if isinstance(other, Distance):
-            other = other.km
-        return self.km < other
+    def __lt__(self, other: Distance | float) -> bool:
+        return self.km < self.convertToNumber(obj=other)
 
-    def __gt__(self, other: Distance) -> bool:
-        if isinstance(other, Distance):
-            other = other.km
-        return self.km > other
+    def __gt__(self, other: Distance | float) -> bool:
+        return self.km > self.convertToNumber(obj=other)
 
-    def __eq__(self, other: Distance) -> bool:
-        if isinstance(other, Distance):
-            other = other.km
-        return self.km == other
+    def __eq__(self, other: Distance | float) -> bool:
+        return self.km == self.convertToNumber(obj=other)
 
-    def __le__(self, other: Distance) -> bool:
-        if isinstance(other, Distance):
-            other = other.km
-        return self.km <= other
+    def __le__(self, other: Distance | float) -> bool:
+        return not self.__gt__(other=other)
 
-    def __ge__(self, other: Distance) -> bool:
-        if isinstance(other, Distance):
-            other = other.km
-        return self.km >= other
+    def __ge__(self, other: Distance | float) -> bool:
+        return not self.__lt__(other=other)
