db4110f1db35c8c553af53debecdfbf26c3bb4eb
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-01-11 20:00:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,45 @@
+from __future__ import annotations
+from functools import total_ordering
+@total_ordering
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int):
+        self.km = km
+    @staticmethod
+    def is_instance_check(other: Distance) -> bool:
+        return isinstance(other, Distance)
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+    def __add__(self, other: Distance):
+        if Distance.is_instance_check(other):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+    def __iadd__(self, other: Distance):
+        if Distance.is_instance_check(other):
+            self.km += other.km
+            return self
+        self.km += other
+        return self
+
+    def __mul__(self, other: Distance):
+        return  Distance(self.km *other)
+
+    def __truediv__(self, other):
+        return  Distance(round((self.km / other), 2))
+    def __lt__(self, other: Distance):
+        if Distance.is_instance_check(other):
+            return self.km < other.km
+        return  self.km < other
+    def __gt__(self, other):
+        if Distance.is_instance_check(other):
+            return self.km > other.km
+        return self.km > other
+    def __eq__(self, other):
+        if Distance.is_instance_check(other):
+            return self.km == other.km
+        return self.km == other
+
+
+
