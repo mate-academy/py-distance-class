42e4cde8fd6d6280309b2b4dfff0577ad5872a36
Maksym Operchuk <operlast529@gmail.com>
2024-01-11 17:56:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -45,9 +45,7 @@ class Distance:
         return self.km == other.km
 
     def __le__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, (int, float)):
-            return self.km <= other
-        return self.km <= other.km
+        return self.km <= self.validation(other)
 
     def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= self.validation(other)
