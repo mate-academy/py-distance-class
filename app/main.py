ba64de00a74c1bc16f4df5a670d71228349a6363
Nick Illich <nickillich96@gmail.com>
2024-01-11 14:13:19 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,60 @@
+from functools import total_ordering
+
+
+@total_ordering
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km):
+        self.km = km
+
+    def __str__(self) -> str:
+
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+
+            return Distance(km=self.km + other.km)
+
+        return Distance(km=self.km + other)
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km = self.km + other.km
+
+            return self
+
+        self.km = self.km + other
+
+        return self
+
+    def __mul__(self, other: int):
+
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int):
+
+        return Distance(km=round((self.km / other), 2))
+
+    def __lt__(self, other):
+
+        if isinstance(other, Distance):
+
+            return self.km < other.km
+
+        return self.km < other
+
+    def __eq__(self, other):
+
+        if isinstance(other, Distance):
+
+            return self.km == other.km
+
+        return self.km == other
+
+
+dist = Distance(20)
