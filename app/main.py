db7c3a73925fd6db1b95d9311d502dcf00635342
Nick Illich <nickillich96@gmail.com>
2024-01-11 14:49:12 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from functools import total_ordering
+from typing import Any
 
 
 @total_ordering
 class Distance:
 
-    def __init__(self, km):
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -15,14 +16,14 @@ class Distance:
 
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: int | float | Any) -> Any:
         if isinstance(other, Distance):
 
             return Distance(km=self.km + other.km)
 
         return Distance(km=self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: int | float | Any) -> Any:
         if isinstance(other, Distance):
             self.km = self.km + other.km
 
@@ -32,15 +33,15 @@ class Distance:
 
         return self
 
-    def __mul__(self, other: int):
+    def __mul__(self, other: int | float | Any) -> Any:
 
-        return Distance(self.km * other)
+        return Distance(km=self.km * other)
 
-    def __truediv__(self, other: int):
+    def __truediv__(self, other: int | float) -> Any:
 
         return Distance(km=round((self.km / other), 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other: int | float | Any) -> bool:
 
         if isinstance(other, Distance):
 
@@ -48,13 +49,10 @@ class Distance:
 
         return self.km < other
 
-    def __eq__(self, other):
+    def __eq__(self, other: int | float | Any) -> bool:
 
         if isinstance(other, Distance):
 
             return self.km == other.km
 
         return self.km == other
-
-
-dist = Distance(20)
