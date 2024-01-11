9e67d61b089c147b3810941ec41964841400fcc3
PythonZem <pythonzem@gmail.com>
2024-01-11 13:04:03 +0200

    add anatation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from functools import total_ordering
 
 
 class Distance:
@@ -19,27 +18,27 @@ class Distance:
     def __add__(self, other: Distance | float | int) -> Distance:
         return Distance(km=self.km + self.get_km(obj=other))
 
-    def __iadd__(self, other: Distance | float) -> Distance:
+    def __iadd__(self, other: Distance | float | int) -> Distance:
         self.km += self.get_km(obj=other)
         return self
 
-    def __mul__(self, other: float) -> Distance:
+    def __mul__(self, other: float | int) -> Distance:
         return Distance(km=self.km * other)
 
     def __truediv__(self, other: float | int) -> Distance:
         return Distance(km=round(self.km / other, 2))
 
-    def __lt__(self, other: Distance | float) -> bool:
+    def __lt__(self, other: Distance | float | int) -> bool:
         return self.km < self.get_km(obj=other)
 
-    def __gt__(self, other: Distance | float) -> bool:
+    def __gt__(self, other: Distance | float | int) -> bool:
         return self.km > self.get_km(obj=other)
 
-    def __eq__(self, other: Distance | float) -> bool:
+    def __eq__(self, other: Distance | float | int) -> bool:
         return self.km == self.get_km(obj=other)
 
-    def __le__(self, other: Distance | float) -> bool:
+    def __le__(self, other: Distance | float | int) -> bool:
         return not self.__gt__(other=other)
 
-    def __ge__(self, other: Distance | float) -> bool:
+    def __ge__(self, other: Distance | float | int) -> bool:
         return not self.__lt__(other=other)
