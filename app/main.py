1b478fcedb8d1ff49e690d5ec812865fe10c1e4e
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-01-11 14:43:20 +0200

    Added static methods & raise error

--- app/main.py
+++ app/main.py
@@ -3,6 +3,14 @@ from typing import Union
 
 class Distance:
 
+    @staticmethod
+    def check_other_number(other: Union[int, float]) -> bool:
+        return isinstance(other, (int, float))
+
+    @staticmethod
+    def check_other_instance(other: "Distance") -> bool:
+        return isinstance(other, Distance)
+
     def __init__(self, km: int) -> None:
         self.km = km
 
@@ -13,58 +21,61 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Union["Distance", int, float]) -> "Distance":
-        if isinstance(other, (int, float)):
-            return Distance(self.km + other)
-        elif isinstance(other, Distance):
-            return Distance(self.km + other.km)
-        else:
-            return NotImplemented
+        try:
+            if Distance.check_other_number(other):
+                return Distance(self.km + other)
+            elif Distance.check_other_instance(other):
+                return Distance(self.km + other.km)
+        except TypeError:
+            "'Other' must be type int or float or belong to Distance class"
 
     def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
-        if isinstance(other, Distance):
-            self.km += other.km
-        elif isinstance(other, (int, float)):
-            self.km += other
-        else:
-            return NotImplemented
+        try:
+            if Distance.check_other_instance(other):
+                self.km += other.km
+            elif Distance.check_other_number(other):
+                self.km += other
+        except TypeError:
+            "'Other' must be type int or float or belong to Distance class"
         return self
 
     def __mul__(self, other: Union["Distance", int, float]) -> "Distance":
-        if isinstance(other, (int, float)):
+        if Distance.check_other_number(other):
             return Distance(self.km * other)
 
     def __truediv__(self, other: Union["Distance", int, float]) -> "Distance":
-        if isinstance(other, (int, float)):
-            return Distance(round(self.km / other, 2))
-        else:
-            return NotImplemented
+        try:
+            if Distance.check_other_number(other):
+                return Distance(round(self.km / other, 2))
+        except TypeError:
+            "'Other' must be type int or float or belong to Distance class"
 
     def __lt__(self, other: Union["Distance", int, float]) -> bool:
-        if isinstance(other, Distance):
+        if Distance.check_other_instance(other):
             return self.km < other.km
-        elif isinstance(other, (int, float)):
+        elif Distance.check_other_number(other):
             return self.km < other
 
     def __gt__(self, other: Union["Distance", int, float]) -> bool:
-        if isinstance(other, Distance):
+        if Distance.check_other_instance(other):
             return self.km > other.km
-        elif isinstance(other, (int, float)):
+        elif Distance.check_other_number(other):
             return self.km > other
 
     def __eq__(self, other: Union["Distance", int, float]) -> bool:
-        if isinstance(other, Distance):
+        if Distance.check_other_instance(other):
             return self.km == other.km
-        elif isinstance(other, (int, float)):
+        elif Distance.check_other_number(other):
             return self.km == other
 
     def __le__(self, other: Union["Distance", int, float]) -> bool:
-        if isinstance(other, Distance):
+        if Distance.check_other_instance(other):
             return self.km <= other.km
-        elif isinstance(other, (int, float)):
+        elif Distance.check_other_number(other):
             return self.km <= other
 
     def __ge__(self, other: Union["Distance", int, float]) -> bool:
-        if isinstance(other, Distance):
+        if Distance.check_other_instance(other):
             return self.km >= other.km
-        elif isinstance(other, (int, float)):
+        elif Distance.check_other_number(other):
             return self.km >= other
