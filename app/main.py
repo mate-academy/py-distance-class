1815e35885965a2a21dc07f6a9bf0698bb715a3c
Mykola <mykola22dubyna@gmail.com>
2024-01-12 11:07:14 +0200

    Solution fix type annotation and DRY

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ from functools import total_ordering
 
 @total_ordering
 class Distance:
-    def __init__(self, km: int) -> None:
-        self.km = km
+    def __init__(self, km: int | float) -> None:
+        self.real = km
+        self.km = self.real
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
@@ -13,30 +14,21 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(km=self.km + other.km)
-        return Distance(km=self.km + other)
+    def __add__(self, other: Distance | int | float) -> Distance:
+        return Distance(km=self.km + other.real)
 
-    def __iadd__(self, other: Distance | int) -> Distance:
-        if isinstance(other, Distance):
-            self.km += other.km
-            return self
-        self.km += other
+    def __iadd__(self, other: Distance | int | float) -> Distance:
+        self.km += other.real
         return self
 
-    def __mul__(self, other: int) -> Distance:
-        return Distance(km=self.km * other)
+    def __mul__(self, multiplier: int | float) -> Distance:
+        return Distance(km=self.km * multiplier)
 
-    def __truediv__(self, other: int) -> Distance:
-        return Distance(km=round(self.km / other, 2))
+    def __truediv__(self, divider: int | float) -> Distance:
+        return Distance(km=round(self.km / divider, 2))
 
-    def __eq__(self, other: Distance | int) -> bool:
-        if isinstance(other, Distance):
-            return self.km == other.km
-        return self.km == other
+    def __eq__(self, other: Distance | int | float) -> bool:
+        return self.km == other.real
 
-    def __lt__(self, other: Distance | int) -> bool:
-        if isinstance(other, Distance):
-            return self.km < other.km
-        return self.km < other
+    def __lt__(self, other: Distance | int | float) -> bool:
+        return self.km < other.real
