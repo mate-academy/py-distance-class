931252db6c26c5d75d33e0f69066cc505500a069
Maksym Operchuk <operlast529@gmail.com>
2024-01-15 12:13:10 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
+from functools import total_ordering
 
-
+@total_ordering
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -12,10 +13,10 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Distance | int | float) -> Distance:
-        return Distance(self.km + self.validation(other))
+        return Distance(self.km + self.extract_distance(other))
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
-        self.km += self.validation(other)
+        self.km += self.extract_distance(other)
         return self
 
     def __mul__(self, other: int | float) -> Distance:
@@ -25,22 +26,16 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
-        return self.km < self.validation(other)
-
-    def __gt__(self, other: Distance | int | float) -> bool:
-        return self.km > self.validation(other)
+        return self.km < self.extract_distance(other)
 
     def __eq__(self, other: Distance | int | float) -> bool:
-        return self.km == self.validation(other)
-
-    def __le__(self, other: Distance | int | float) -> bool:
-        return self.km <= self.validation(other)
-
-    def __ge__(self, other: Distance | int | float) -> bool:
-        return self.km >= self.validation(other)
+        return self.km == self.extract_distance(other)
 
     @staticmethod
-    def validation(other: Distance | int | float) -> int | float:
+    def extract_distance(other: Distance | int | float) -> int | float:
         if isinstance(other, Distance):
             other = other.km
         return other
+
+
+print(Distance('123') >= Distance("12"))
