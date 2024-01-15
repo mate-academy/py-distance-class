7201a2cf2e0367243661483a9ac60fbecef8fde5
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-01-15 12:34:23 +0200

    added staticmethod

--- app/main.py
+++ app/main.py
@@ -4,7 +4,13 @@ from functools import total_ordering
 
 @total_ordering
 class Distance:
-    def __init__(self, km: int) -> None:
+    @staticmethod
+    def get_distance(other: Distance | int | float) -> int | float:
+        if isinstance(other, Distance):
+            other = other.km
+        return other
+
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -13,30 +19,22 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(self.km + other.km)
-        return Distance(self.km + other)
+    def __add__(self, other: Distance | int | float) -> Distance:
 
-    def __iadd__(self, other: Distance | int) -> Distance:
-        if isinstance(other, Distance):
-            self.km += other.km
-            return self
-        self.km += other
+        return Distance(self.km + self.get_distance(other))
+
+    def __iadd__(self, other: Distance | int | float) -> Distance:
+        self.km += self.get_distance(other)
         return self
 
-    def __mul__(self, other: int) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int) -> Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: Distance | int) -> bool:
-        if isinstance(other, Distance):
-            return self.km < other.km
-        return self.km < other
+    def __lt__(self, other: Distance | int | float) -> bool:
+        return self.km < self.get_distance(other)
 
-    def __eq__(self, other: Distance | int) -> bool:
-        if isinstance(other, Distance):
-            return self.km == other.km
-        return self.km == other
+    def __eq__(self, other: Distance | int | float) -> bool:
+        return self.km == self.get_distance(other)
