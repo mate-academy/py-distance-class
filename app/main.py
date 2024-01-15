57c3a70bc5fc0ee542ef1259b98366540c768300
Ihor Bondarenko <igorb6766@gmail.com>
2024-01-15 12:48:41 +0200

    changed Distance.to_number -> self.to_number

--- app/main.py
+++ app/main.py
@@ -15,10 +15,10 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Distance | int | float) -> Distance:
-        return Distance(self.km + Distance.to_number(other))
+        return Distance(self.km + self.to_number(other))
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
-        self.km += Distance.to_number(other)
+        self.km += self.to_number(other)
         return self
 
     def __mul__(self, other: int | float) -> Distance:
@@ -28,10 +28,10 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __eq__(self, other: Distance | int | float) -> bool:
-        return self.km == Distance.to_number(other)
+        return self.km == self.to_number(other)
 
     def __lt__(self, other: Distance | int | float) -> bool:
-        return self.km < Distance.to_number(other)
+        return self.km < self.to_number(other)
 
     @staticmethod
     def to_number(number: Distance | int | float) -> int | float:
