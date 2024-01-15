4acc03da6d08ce95332bfcbc463a00b326b66491
AntonSkyrda <mr.skyrda@gmail.com>
2024-01-15 22:10:36 +0200

    fix methods annotations types

--- app/main.py
+++ app/main.py
@@ -13,23 +13,23 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: object) -> Distance:
+    def __add__(self, other: object) -> int | float | Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
 
         return Distance(self.km + other)
 
-    def __iadd__(self, other: object) -> Distance:
+    def __iadd__(self, other: object) -> int | float | Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, number: int) -> Distance:
+    def __mul__(self, number: int) -> int | float | Distance:
         return Distance(self.km * number)
 
-    def __truediv__(self, other: int) -> Distance:
+    def __truediv__(self, other: int) -> int | float | Distance:
         return Distance(round((self.km / other), 2))
 
     def __eq__(self, other: object) -> bool:
