d156604af4d767c5f451d4a408d5b6781ac417b0
unknown <artem.sapiens@gmail.com>
2024-01-16 14:15:30 +0200

    'Change-code-according-to-comments'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from __future__ import annotations
+from typing import Any
+from functools import total_ordering
 
 
+@total_ordering
 class Distance:
 
     def __init__(self, km: int | float) -> None:
         self.km = km
+
+    @staticmethod
+    def check_distance(other: Any) -> int | float:
+        if isinstance(other, Distance):
+            return other.km
+        return other
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
@@ -12,21 +21,11 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
-            return Distance(self.km + other)
-        else:
-            raise TypeError("Unsupported type for addition with Distance")
+    def __add__(self, other: Any) -> Distance:
+        return Distance(self.km + self.check_distance(other))
 
-    def __iadd__(self, other: Distance) -> int | float:
-        if isinstance(other, Distance):
-            self.km += other.km
-        elif isinstance(other, (int, float)):
-            self.km += other
-        else:
-            raise TypeError("Unsupported type")
+    def __iadd__(self, other: Any) -> Distance:
+        self.km += self.check_distance(other)
         return self
 
     def __mul__(self, times: int) -> Distance:
@@ -35,22 +34,22 @@ class Distance:
     def __truediv__(self, divisor: int) -> Distance:
         return Distance(round(self.km / divisor, 2))
 
-    def __lt__(self, other: Distance) -> int | float:
-        other_km = other.km if isinstance(other, Distance) else other
+    def __lt__(self, other: Any) -> bool:
+        other_km = self.check_distance(other)
         return self.km < other_km
 
-    def __gt__(self, other: Distance) -> int | float:
-        other_km = other.km if isinstance(other, Distance) else other
+    def __gt__(self, other: Any) -> bool:
+        other_km = self.check_distance(other)
         return self.km > other_km
 
-    def __eq__(self, other: Distance) -> int | float:
-        other_km = other.km if isinstance(other, Distance) else other
+    def __eq__(self, other: Any) -> bool:
+        other_km = self.check_distance(other)
         return self.km == other_km
 
-    def __le__(self, other: Distance) -> int | float:
-        other_km = other.km if isinstance(other, Distance) else other
+    def __le__(self, other: Any) -> bool:
+        other_km = self.check_distance(other)
         return self.km <= other_km
 
-    def __ge__(self, other: Distance) -> int | float:
-        other_km = other.km if isinstance(other, Distance) else other
+    def __ge__(self, other: Any) -> bool:
+        other_km = self.check_distance(other)
         return self.km >= other_km
