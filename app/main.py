c7573dea0b977fddee133610fe9810c6ca11affd
sudakolha <sudakolha@outlook.com>
2024-01-17 14:52:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,40 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> None:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> None:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: int | float | "Distance") -> None:
+        value = other if isinstance(other, (int, float)) else other.km
+        return Distance(self.km + value)
+
+    def __iadd__(self, other: int | float | "Distance") -> None:
+        value = other if isinstance(other, (int, float)) else other.km
+        self.km += value
+        return self
+
+    def __mul__(self, other: int | float | "Distance") -> None:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int | float | "Distance") -> None:
+        return Distance(round((self.km / other), 2))
+
+    def __lt__(self, other: int | float | "Distance") -> None:
+        return self.km < other
+
+    def __gt__(self, other: int | float | "Distance") -> None:
+        return self.km > other
+
+    def __eq__(self, other: int | float | "Distance") -> None:
+        return self.km == other
+
+    def __le__(self, other: int | float | "Distance") -> None:
+        return self.km <= other
+
+    def __ge__(self, other: int | float | "Distance") -> None:
+        return self.km >= other
