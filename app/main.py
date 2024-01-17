e97f63fb6ec7597e288e83b283a959261fef2241
sudakolha <sudakolha@outlook.com>
2024-01-17 15:00:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -12,32 +12,32 @@ class Distance:
     def __repr__(self) -> None:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float | "Distance") -> None:
+    def __add__(self, other: int | float | Distance) -> None:
         value = other if isinstance(other, (int, float)) else other.km
         return Distance(self.km + value)
 
-    def __iadd__(self, other: int | float | "Distance") -> None:
+    def __iadd__(self, other: int | float | Distance) -> None:
         value = other if isinstance(other, (int, float)) else other.km
         self.km += value
         return self
 
-    def __mul__(self, other: int | float | "Distance") -> None:
+    def __mul__(self, other: int | float | Distance) -> None:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int | float | "Distance") -> None:
+    def __truediv__(self, other: int | float | Distance) -> None:
         return Distance(round((self.km / other), 2))
 
-    def __lt__(self, other: int | float | "Distance") -> None:
+    def __lt__(self, other: int | float | Distance) -> None:
         return self.km < other
 
-    def __gt__(self, other: int | float | "Distance") -> None:
+    def __gt__(self, other: int | float | Distance) -> None:
         return self.km > other
 
-    def __eq__(self, other: int | float | "Distance") -> None:
+    def __eq__(self, other: int | float | Distance) -> None:
         return self.km == other
 
-    def __le__(self, other: int | float | "Distance") -> None:
+    def __le__(self, other: int | float | Distance) -> None:
         return self.km <= other
 
-    def __ge__(self, other: int | float | "Distance") -> None:
+    def __ge__(self, other: int | float | Distance) -> None:
         return self.km >= other
