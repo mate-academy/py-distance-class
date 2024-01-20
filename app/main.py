5d60a742758c47f797fbc23aa8f5488d25afada4
volry <v.v.ryzhuk@gmail.com>
2024-01-20 13:07:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,96 +1,84 @@
-#%%
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
-    
+
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
-    
-    def __add__(self, other):
+
+    def __add__(self, other: Distance) -> None:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
         else:
-            raise f"Gosh,- relax, drink coffee"
-    
-    def __iadd__(self, other):
+            raise NotImplementedError("Distance to Distance or int")
+
+    def __iadd__(self, other: Distance) -> None:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
             self.km += other
         else:
-            raise f"Gosh,- relax, drink coffee"
+            raise NotImplementedError
         return self
-    
-    def __mul__(self, other):
+
+    def __mul__(self, other: Distance) -> None:
         if isinstance(other, Distance):
             raise TypeError("__mul__ method not use instance")
-        elif isinstance(other, (int,float)):
+        elif isinstance(other, (int, float)):
             self.km *= other
         else:
-            raise f"Gosh,- relax, drink coffee"
+            raise NotImplementedError
         return self
-    
-    def __truediv__(self, other):
+
+    def __truediv__(self, other: Distance) -> None:
         if isinstance(other, (int, float)):
-            return Distance(round(self.km / other,2))
+            return Distance(round(self.km / other, 2))
         else:
-            raise f"Division error - math is your pain"
-    
-    def __lt__(self, other):
+            raise f"Division error - math is your pain{other}"
+
+    def __lt__(self, other: Distance) -> None:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
-        else: 
+        else:
             return NotImplemented
-    
-    def __gt__(self, other):
+
+    def __gt__(self, other: Distance) -> None:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
-        else: 
+        else:
             return NotImplemented
 
-    def __eq__(self, other):
+    def __eq__(self, other: Distance) -> None:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
-        else: 
+        else:
             return NotImplemented
-        
-    def __le__(self, other):
+
+    def __le__(self, other: Distance) -> None:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
-        else: 
+        else:
             return NotImplemented
-    
-    def __ge__(self, other):
+
+    def __ge__(self, other: Distance) -> None:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
             return self.km >= other
-        else: 
+        else:
             return NotImplemented
-    
-    
-    
-
-
-
-# %%
-distance = Distance(50)
-
-# %%
-distance < Distance(60)
-# %%
-distance == Distance(100)
-# %%
