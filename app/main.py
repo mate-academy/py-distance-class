54649f17c3e15868db527eb33f25f29cab3646e1
voronvasia <voronvasia@gmail.com>
2024-01-21 20:34:41 +0200

    fixed code

--- app/main.py
+++ app/main.py
@@ -5,61 +5,6 @@ class Distance:
     def __init__(self, km: int | float) -> None:
         self.km = km
 
-    def func_for_math(self, other: Distance | int, decimal: str) -> Distance:
-        result = 0
-        if isinstance(other, Distance):
-            if decimal == "+":
-                result = self.km + other.km
-            if decimal == "+=":
-                self.km += other.km
-                return self
-            return Distance(
-                km=result
-            )
-        else:
-            if decimal == "+":
-                result = self.km + other
-            if decimal == "+=":
-                self.km += other
-                return self
-        return Distance(
-            km=result
-        )
-
-    def funct_for_mult_div(self, other: int, decimal: str) -> Distance:
-        result = 0
-        if decimal == "*":
-            result = self.km * other
-        if decimal == "/":
-            result = round(self.km / other, 2)
-        return Distance(
-            km=result
-        )
-
-    def func_for_leveling(self, other: Distance | int, decimal: str) -> bool:
-        if isinstance(other, Distance):
-            if decimal == "<":
-                return self.km < other.km
-            if decimal == "<=":
-                return self.km <= other.km
-            if decimal == ">":
-                return self.km > other.km
-            if decimal == ">=":
-                return self.km >= other.km
-            if decimal == "==":
-                return self.km == other.km
-        else:
-            if decimal == "<":
-                return self.km < other
-            if decimal == "<=":
-                return self.km <= other
-            if decimal == ">":
-                return self.km > other
-            if decimal == ">=":
-                return self.km >= other
-            if decimal == "==":
-                return self.km == other
-
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
@@ -67,28 +12,45 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: int | Distance) -> Distance:
-        return self.func_for_math(other, "+")
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
 
     def __iadd__(self, other: int | Distance) -> Distance:
-        return self.func_for_math(other, "+=")
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        else:
+            self.km += other
+            return self
 
     def __mul__(self, other: int) -> Distance:
-        return self.funct_for_mult_div(other, "*")
+        return Distance(self.km * other)
 
     def __truediv__(self, other: int) -> Distance:
-        return self.funct_for_mult_div(other, "/")
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int) -> bool:
-        return self.func_for_leveling(other, "<")
+        if isinstance(other, Distance):
+            return self.km < other.km
+        return self.km < other
 
     def __le__(self, other: Distance | int) -> bool:
-        return self.func_for_leveling(other, "<=")
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        return self.km <= other
 
     def __gt__(self, other: Distance | int) -> bool:
-        return self.func_for_leveling(other, ">")
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
 
     def __ge__(self, other: Distance | int) -> bool:
-        return self.func_for_leveling(other, ">=")
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return self.km >= other
 
     def __eq__(self, other: Distance | int) -> bool:
-        return self.func_for_leveling(other, "==")
+        if isinstance(other, Distance):
+            return self.km == other.km
+        return self.km == other
