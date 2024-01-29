0d29a8b0fdf9b30d3756a7b25b34f9667ff64365
Vitalii Burkalo <vitaliyburkalo81@gmail.com>
2024-01-29 11:19:12 +0200

    UPD: Fixed annotation as you suggested and solved failing tests

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class Distance:
     def __init__(self, km: int | str | float) -> None:
         self.km = km
@@ -8,7 +11,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance" | int | float) -> "Distance":
+    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -16,7 +19,7 @@ class Distance:
         else:
             raise TypeError("Unsupported type for addition with Distance")
 
-    def __iadd__(self, other: "Distance" | int | float) -> "Distance":
+    def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -25,7 +28,7 @@ class Distance:
             raise TypeError("Unsupported type for addition with Distance")
         return self
 
-    def __mul__(self, other: "Distance" | int | float) -> "Distance":
+    def __mul__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             raise (TypeError
                    ("'__mul__' method should not accept Distance instance")
@@ -35,13 +38,13 @@ class Distance:
         else:
             raise TypeError("Unsupported type for addition with Distance")
 
-    def __truediv__(self, other: "Distance" | int | float) -> "Distance":
+    def __truediv__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
         else:
             raise TypeError("Unsupported type for division with Distance")
 
-    def __lt__(self, other: "Distance" | int | float) -> bool:
+    def __lt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
@@ -49,7 +52,7 @@ class Distance:
         else:
             return NotImplemented
 
-    def __gt__(self, other: "Distance" | int | float) -> bool:
+    def __gt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
@@ -57,7 +60,7 @@ class Distance:
         else:
             return NotImplemented
 
-    def __eq__(self, other: "Distance" | int | float) -> bool:
+    def __eq__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
@@ -65,7 +68,7 @@ class Distance:
         else:
             return NotImplemented
 
-    def __le__(self, other: "Distance" | int | float) -> bool:
+    def __le__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
@@ -73,7 +76,7 @@ class Distance:
         else:
             return NotImplemented
 
-    def __ge__(self, other: "Distance" | int | float) -> bool:
+    def __ge__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
