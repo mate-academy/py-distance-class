9dbd64222ab289ee0ddf90ee8555a25a2209e5e5
Vitalii Burkalo <vitaliyburkalo81@gmail.com>
2024-01-29 14:37:31 +0200

    Implemented bug fixes

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from typing import Union
 
 
 class Distance:
-    def __init__(self, km: int | str | float) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
+        if not isinstance(km, (int, float)):
+            raise TypeError("km must be type int or float")
         self.km = km
 
     def __str__(self) -> str:
@@ -28,21 +30,19 @@ class Distance:
             raise TypeError("Unsupported type for addition with Distance")
         return self
 
-    def __mul__(self, other: Union["Distance", int, float]) -> "Distance":
-        if isinstance(other, Distance):
-            raise (TypeError
-                   ("'__mul__' method should not accept Distance instance")
-                   )
-        elif isinstance(other, (int, float)):
+    def __mul__(self, other: Union[int, float]) -> "Distance":
+        if isinstance(other, (int, float)):
             return Distance(self.km * other)
         else:
-            raise TypeError("Unsupported type for addition with Distance")
+            raise TypeError("Unsupported type to interact with Distance")
 
-    def __truediv__(self, other: Union["Distance", int, float]) -> "Distance":
+    def __truediv__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
+            if other == 0:
+                raise ValueError("Division by zero cant be used")
             return Distance(round(self.km / other, 2))
         else:
-            raise TypeError("Unsupported type for division with Distance")
+            raise TypeError("Unsupported type to interact with Distance")
 
     def __lt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
@@ -50,7 +50,7 @@ class Distance:
         elif isinstance(other, (int, float)):
             return self.km < other
         else:
-            return NotImplemented
+            raise TypeError("Unsupported type to interact with Distance")
 
     def __gt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
@@ -58,7 +58,7 @@ class Distance:
         elif isinstance(other, (int, float)):
             return self.km > other
         else:
-            return NotImplemented
+            raise TypeError("Unsupported type to interact with Distance")
 
     def __eq__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
@@ -66,7 +66,7 @@ class Distance:
         elif isinstance(other, (int, float)):
             return self.km == other
         else:
-            return NotImplemented
+            raise TypeError("Unsupported type to interact with Distance")
 
     def __le__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
@@ -74,7 +74,7 @@ class Distance:
         elif isinstance(other, (int, float)):
             return self.km <= other
         else:
-            return NotImplemented
+            raise TypeError("Unsupported type to interact with Distance")
 
     def __ge__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
@@ -82,4 +82,4 @@ class Distance:
         elif isinstance(other, (int, float)):
             return self.km >= other
         else:
-            return NotImplemented
+            raise TypeError("Unsupported type to interact with Distance")
