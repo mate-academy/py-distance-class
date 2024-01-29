a5e001145b4e556bf581a2a7e8c2f71710e85974
Maryna Barannik <sober2night@gmail.com>
2024-01-29 11:21:56 +0100

    'Fix'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
+from __future__ import annotations
 from typing import Callable
 
 
 class Distance:
-    def __init__(self, km: float) -> None:
-        self.km: float = km
+    def __init__(self, km: float | int) -> None:
+        self.km = km
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
@@ -11,16 +12,18 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def _operate(self, other: "Distance", operation: Callable) -> "Distance":
+    def _operate(
+        self, other: int | float | Distance, operation: Callable
+    ) -> Distance:
         if isinstance(other, Distance):
             return Distance(operation(self.km, other.km))
         elif isinstance(other, (int, float)):
             return Distance(operation(self.km, other))
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: int | float | Distance) -> Distance:
         return self._operate(other, lambda x, y: x + y)
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -28,29 +31,31 @@ class Distance:
 
         return self
 
-    def __mul__(self, factor: float) -> "Distance":
+    def __mul__(self, factor: int | float) -> Distance:
         return self._operate(Distance(factor), lambda x, y: x * y)
 
-    def __truediv__(self, divisor: float) -> "Distance":
+    def __truediv__(self, divisor: int | float) -> Distance:
         return self._operate(Distance(divisor), lambda x, y: round(x / y, 2))
 
-    def _compare(self, other: "Distance", operation: Callable) -> bool:
+    def _compare(
+        self, other: int | float | Distance, operation: Callable
+    ) -> bool:
         if isinstance(other, Distance):
             return operation(self.km, other.km)
         elif isinstance(other, (int, float)):
             return operation(self.km, other)
 
-    def __lt__(self, other: "Distance") -> bool:
+    def __lt__(self, other: int | float | Distance) -> bool:
         return self._compare(other, lambda x, y: x < y)
 
-    def __gt__(self, other: "Distance") -> bool:
+    def __gt__(self, other: int | float | Distance) -> bool:
         return self._compare(other, lambda x, y: x > y)
 
-    def __eq__(self, other: "Distance") -> bool:
+    def __eq__(self, other: int | float | Distance) -> bool:
         return self._compare(other, lambda x, y: x == y)
 
-    def __le__(self, other: "Distance") -> bool:
+    def __le__(self, other: int | float | Distance) -> bool:
         return self._compare(other, lambda x, y: x <= y)
 
-    def __ge__(self, other: "Distance") -> bool:
+    def __ge__(self, other: int | float | Distance) -> bool:
         return self._compare(other, lambda x, y: x >= y)
