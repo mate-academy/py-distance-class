680fcecd26e5221bfc81a1ed3a8f5849ac8088ec
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2024-01-31 14:19:20 +0200

    Update app/main.py
    Co-authored-by: Eduard Alekseiev <129166823+EdAlekseiev@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -40,8 +40,7 @@ class Distance:
     def __eq__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, (int, float)):
-            return self.km == other
+        return self.km == other
 
     def __le__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
