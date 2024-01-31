19014d561033c8b6ba1e6f1f14797fba44d6c56f
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2024-01-31 14:19:09 +0200

    Update app/main.py
    Co-authored-by: Eduard Alekseiev <129166823+EdAlekseiev@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -30,8 +30,7 @@ class Distance:
     def __lt__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif isinstance(other, (int, float)):
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
