00159adca084c9b3b59fa91f2e357b7722045727
TarasFirst <firsttaras@mail.com>
2024-02-01 17:21:22 +0200

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
+
 class Distance:
 
-    def __init__(self, km: float) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -9,39 +12,39 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: ("Distance", float, int)) -> "Distance":
+    def __add__(self, other: Distance | float | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (float, int)):
+        elif isinstance(other, float | int):
             return Distance(self.km + other)
 
-    def __iadd__(self, other: ("Distance", float, int)) -> "Distance":
+    def __iadd__(self, other: Distance | float | int) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
-        elif isinstance(other, (float, int)):
+        elif isinstance(other, float | int):
             self.km += other
         return self
 
-    def __mul__(self, other: float) -> "Distance":
+    def __mul__(self, other: float | int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float) -> "Distance":
+    def __truediv__(self, other: float | int) -> Distance:
         if other == 0:
             raise ValueError("Cannot divide by zero")
         result = round((self.km / other), 2)
         return Distance(result)
 
-    def __lt__(self, other: float) -> bool:
+    def __lt__(self, other: float | int) -> bool:
         return self.km < other
 
-    def __gt__(self, other: float) -> bool:
+    def __gt__(self, other: float | int) -> bool:
         return self.km > other
 
-    def __eq__(self, other: float) -> bool:
+    def __eq__(self, other: float | int) -> bool:
         return self.km == other
 
-    def __le__(self, other: float) -> bool:
+    def __le__(self, other: float | int) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: float) -> bool:
+    def __ge__(self, other: float | int) -> bool:
         return self.km >= other
