9260b0443fa88aaf926923cbefe1e4524acf4265
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-02-07 10:47:47 +0100

    Fixed float annotation

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -11,41 +11,41 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, value: Distance | int) -> Distance:
+    def __add__(self, value: Distance | int | float) -> Distance:
         result = Distance.check_value(value)
         return Distance(self.km + result)
 
-    def __iadd__(self, value: Distance | int) -> Distance:
+    def __iadd__(self, value: Distance | int | float) -> Distance:
         result = Distance.check_value(value)
         self.km = self.km + result
         return self
 
-    def __mul__(self, value: int) -> Distance:
+    def __mul__(self, value: int | float) -> Distance:
         return Distance(self.km * value)
 
-    def __truediv__(self, value: float | int) -> Distance:
+    def __truediv__(self, value: int | float) -> Distance:
         return Distance(round(self.km / value, 2))
 
-    def __lt__(self, value: Distance | int) -> bool:
+    def __lt__(self, value: Distance | int | float) -> bool:
         result = Distance.check_value(value)
         return self.km < result
 
-    def __gt__(self, value: Distance | int) -> bool:
+    def __gt__(self, value: Distance | int | float) -> bool:
         result = Distance.check_value(value)
         return self.km > result
 
-    def __eq__(self, value: Distance | int) -> bool:
+    def __eq__(self, value: Distance | int | float) -> bool:
         result = Distance.check_value(value)
         return self.km == result
 
-    def __le__(self, value: Distance | int) -> bool:
+    def __le__(self, value: Distance | int | float) -> bool:
         result = Distance.check_value(value)
         return self.km <= result
 
-    def __ge__(self, value: Distance | int) -> bool:
+    def __ge__(self, value: Distance | int | float) -> bool:
         result = Distance.check_value(value)
         return self.km >= result
 
     @staticmethod
-    def check_value(value: Distance | int) -> int:
+    def check_value(value: Distance | int | float) -> int:
         return value.km if isinstance(value, Distance) else value
