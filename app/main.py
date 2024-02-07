43e21962d60977f7a65fb8504b6e7bc3572617e2
Andrei Butko <andreybutkomerefa@gmail.com>
2024-02-07 12:41:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,55 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:  # print() or str()
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance) -> Distance:
+
+        if isinstance(other, Distance):
+            self.km += other.km
+
+        else:
+            self.km += other
+
+        return Distance(self.km)
+
+    def __iadd__(self, other: Distance) -> Distance:
+
+        if isinstance(other, Distance):
+            self.km += other.km
+
+        else:
+            self.km += other
+
+        return self
+
+    def __mul__(self, other: Distance) -> Distance:
+        self.km *= other
+        return Distance(self.km)
+
+    def __truediv__(self, other: Distance) -> Distance:
+        self.km /= other
+        return Distance(round(self.km, 2))
+
+    def __eq__(self, other: float) -> bool:
+        return self.km == other
+
+    def __lt__(self, other: float) -> bool:
+        return self.km < other
+
+    def __gt__(self, other: float) -> bool:
+        return self.km > other
+
+    def __le__(self, other: float) -> bool:
+        return self.km <= other
+
+    def __ge__(self, other: float) -> bool:
+        return self.km >= other
