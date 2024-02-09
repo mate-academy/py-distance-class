21582eb8563c3f52ed38f989b167b977cbbcb8e8
Maksym Hekalo <maximgekalo@gmail.com>
2024-02-09 21:01:36 +0200

    add float, int anotations for param in method

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 class Distance:
 
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     @staticmethod
@@ -15,19 +15,19 @@ class Distance:
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self) -> str:
+    def __repr__(self) -> str | float:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         return Distance(
             km=self.km + self.return_correct_data(other)
         )
 
-    def __iadd__(self, other: Distance | int) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += self.return_correct_data(other)
         return self
 
-    def __mul__(self, other: int) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(
             km=self.km * other
         )
@@ -37,17 +37,17 @@ class Distance:
             km=round(self.km / other, 2)
         )
 
-    def __lt__(self, other: Distance) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < self.return_correct_data(other)
 
-    def __gt__(self, other: Distance) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > self.return_correct_data(other)
 
-    def __eq__(self, other: Distance) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == self.return_correct_data(other)
 
-    def __le__(self, other: Distance) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= self.return_correct_data(other)
 
-    def __ge__(self, other: Distance) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= self.return_correct_data(other)
