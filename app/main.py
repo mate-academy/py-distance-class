d703d1a746ab39e526a9261768a22c6df5f05ea6
Maksym Shramko <shramkomax@gmail.com>
2024-02-14 13:31:11 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -8,7 +11,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -16,7 +19,7 @@ class Distance:
         else:
             raise TypeError("Unsupported operand type for +")
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -25,13 +28,13 @@ class Distance:
             raise TypeError("Unsupported operand type for +=")
         return self
 
-    def __mul__(self, other: "Distance") -> "Distance":
+    def __mul__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         else:
             raise TypeError("Unsupported operand type for *")
 
-    def __truediv__(self, other: "Distance") -> "Distance":
+    def __truediv__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
         else:
