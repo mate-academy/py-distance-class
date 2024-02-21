ec1e2c8527cad21504cf340bda5dc159e783ea83
VerFox <misha.159357@ukr.net>
2024-02-21 23:11:29 +0200

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+from ctypes import Union
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -8,51 +12,51 @@ class Distance:
     def __repr__(self) -> None:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: float) -> None:
+    def __add__(self, other: Union[float, Distance]) -> None:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         else:
             return Distance(self.km + other)
 
-    def __iadd__(self, other: float) -> None:
+    def __iadd__(self, other: Union[float, Distance]) -> None:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: float) -> None:
+    def __mul__(self, other: Union[float, Distance]) -> None:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float) -> None:
+    def __truediv__(self, other: Union[float, Distance]) -> None:
         result = self.km / other
         return Distance(round(result, 2))
 
-    def __lt__(self, other: float) -> None:
+    def __lt__(self, other: Union[float, Distance]) -> None:
         if isinstance(other, Distance):
             return self.km < other.km
         else:
             return self.km < other
 
-    def __gt__(self, other: float) -> None:
+    def __gt__(self, other: Union[float, Distance]) -> None:
         if isinstance(other, Distance):
             return self.km > other.km
         else:
             return self.km > other
 
-    def __eq__(self, other: float) -> None:
+    def __eq__(self, other: Union[float, Distance]) -> None:
         if isinstance(other, Distance):
             return self.km == other.km
         else:
             return self.km == other
 
-    def __le__(self, other: float) -> None:
+    def __le__(self, other: Union[float, Distance]) -> None:
         if isinstance(other, Distance):
             return self.km <= other.km
         else:
             return self.km <= other
 
-    def __ge__(self, other: float) -> None:
+    def __ge__(self, other: Union[float, Distance]) -> None:
         if isinstance(other, Distance):
             return self.km >= other.km
         else:
