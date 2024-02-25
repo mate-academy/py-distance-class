040e2335e743bd9ffc47a0df7c78ca4c37566ac5
skeletronys <mykytcey@gmail.com>
2024-02-25 13:27:46 +0100

    add float also possible for other

--- app/main.py
+++ app/main.py
@@ -5,18 +5,18 @@ class Distance:
     def __init__(self, km: int | float) -> None:
         self.km = km
 
-    def __str__(self) -> str:
+    def __str__(self: Distance) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self) -> str:
+    def __repr__(self: Distance) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: Distance | int) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
