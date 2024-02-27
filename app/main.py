7b9c4f894bfae2cd6c19c98ecee3a8f33442f8be
IRYNA LYAKHOVA <ipppk@outlook.com>
2024-02-27 19:14:41 +0200

    Fix error

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
+from __future__ import annotations
+
+
 class Distance:
+    # ініціалізація об'єкта класу
     def __init__(self, km: int) -> None:
         self.km = km
 
+    # представлення у зручному для людини форматі
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
+    # формат для відтворення у новому об'єкті з тими ж властивостями
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float) -> callable:
+    # дозволяє додавати до об'єкта відстань і зберігати результат
+    def __add__(self, other: int | float | Distance) -> callable:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -17,6 +24,7 @@ class Distance:
             raise TypeError("Unsupported operand type(s) "
                             "for +: 'Distance' and '{}'".format(type(other)))
 
+    # Цей метод реалізує операцію += для об'єктів Distance
     def __iadd__(self, other: int | float) -> callable:
         if isinstance(other, Distance):
             self.km += other.km
@@ -27,12 +35,15 @@ class Distance:
                             "for +=: 'Distance' and '{}'".format(type(other)))
         return self
 
+    # множення на скаляр
     def __mul__(self, scalar: int | float) -> callable:
         return Distance(self.km * scalar)
 
+    # ділення з округленням
     def __truediv__(self, scalar: int | float) -> callable:
         return Distance(round(self.km / scalar, 2))
 
+    # Порівнює поточну відстань з other "<"
     def __lt__(self, other: int | float) -> callable:
         if isinstance(other, Distance):
             return self.km < other.km
@@ -42,6 +53,7 @@ class Distance:
             raise TypeError("Unsupported operand type(s) "
                             "for <: 'Distance' and '{}'".format(type(other)))
 
+    # Порівнює поточну відстань з other ">"
     def __gt__(self, other: int | float) -> callable:
         if isinstance(other, Distance):
             return self.km > other.km
@@ -51,6 +63,7 @@ class Distance:
             raise TypeError("Unsupported operand type(s) "
                             "for >: 'Distance' and '{}'".format(type(other)))
 
+    # Порівнює поточну відстань з other "="
     def __eq__(self, other: int | float) -> int | str:
         if isinstance(other, Distance):
             return self.km == other.km
@@ -60,6 +73,7 @@ class Distance:
             raise TypeError("Unsupported operand type(s) "
                             "for ==: 'Distance' and '{}'".format(type(other)))
 
+    # Порівнює поточну відстань з other "<="
     def __le__(self, other: int | float) -> int | float | str:
         if isinstance(other, Distance):
             return self.km <= other.km
@@ -69,6 +83,7 @@ class Distance:
             raise TypeError("Unsupported operand type(s) "
                             "for <=: 'Distance' and '{}'".format(type(other)))
 
+    # Порівнює поточну відстань з other ">="
     def __ge__(self, other: int | float) -> callable:
         if isinstance(other, Distance):
             return self.km >= other.km
