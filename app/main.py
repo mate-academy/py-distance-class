c8f9c378c708915ff7687b8195c0ed229998a333
bujhm <Bujhm99@gmail.com>
2024-03-12 23:16:22 +0200

    not object annotation, not second, no short names, previous methods

--- app/main.py
+++ app/main.py
@@ -11,10 +11,10 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, addentum: object | int) -> Distance:
+    def __add__(self, addentum: Distance | int) -> Distance:
         return Distance(self.km + getattr(addentum, "km", addentum))
 
-    def __iadd__(self, addentum: object | int) -> Distance:
+    def __iadd__(self, addentum: Distance | int) -> Distance:
         self.km += getattr(addentum, "km", addentum)
         return self
 
@@ -22,23 +22,23 @@ class Distance:
         self.km *= multiplier
         return self
 
-    def __truediv__(self, divider: int) -> Distance:
+    def __truediv__(self, divider: int) -> Distance | float:
         if divider == 0:
             return float("inf")
         self.km = round(self.km / divider, 2)
         return self
 
-    def __lt__(self, comparer: object | int) -> bool:
+    def __lt__(self, comparer: Distance | int) -> bool:
         return self.km < getattr(comparer, "km", comparer)
 
-    def __gt__(self, comparer: object | int) -> bool:
+    def __gt__(self, comparer: Distance | int) -> bool:
         return self.km > getattr(comparer, "km", comparer)
 
-    def __eq__(self, comparer: object | int) -> bool:
+    def __eq__(self, comparer: Distance | int) -> bool:
         return self.km == getattr(comparer, "km", comparer)
 
-    def __le__(self, comparer: object | int) -> bool:
+    def __le__(self, comparer: Distance | int) -> bool:
         return not self.km > getattr(comparer, "km", comparer)
 
-    def __ge__(self, comparer: object | int) -> bool:
+    def __ge__(self, comparer: Distance | int) -> bool:
         return not self.km < getattr(comparer, "km", comparer)
