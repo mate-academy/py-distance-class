86df510406230827e532de1e32d5e650ffa589cc
tiway <stilon670@gmail.com>
2024-03-15 14:10:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from functools import total_ordering
+from typing import Union
 
 
 @total_ordering
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,20 +13,20 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         if isinstance(other, (int, float)):
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
 
