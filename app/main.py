73feacb38d39d6472e5fc16bc47ccc4784348d6d
tiway <stilon670@gmail.com>
2024-03-15 15:17:10 +0200

    add __future__ import annotations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from functools import total_ordering
 from typing import Union
 
@@ -13,34 +14,34 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[int, float, "Distance"]) -> "Distance":
+    def __add__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
 
-    def __iadd__(self, other: Union[int, float, "Distance"]) -> "Distance":
+    def __iadd__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         if isinstance(other, (int, float)):
             self.km += other
         return self
 
-    def __mul__(self, other: Union[int, float, "Distance"]) -> "Distance":
+    def __mul__(self, other: Union[int, float, Distance]) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
 
-    def __truediv__(self, other: int | float) -> "Distance":
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (float, int)):
             return Distance(round(self.km / other, 2))
 
-    def __eq__(self, other: "Distance") -> bool:
+    def __eq__(self, other: Distance) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         if isinstance(other, (int, float)):
             return self.km == other
 
-    def __gt__(self, other: "Distance") -> bool:
+    def __gt__(self, other: Distance) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         if isinstance(other, (int, float)):
