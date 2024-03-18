ad399715462674e5317c6e7a151019bc46dd0dab
NioviIki <charmniovi@tutanota.com>
2024-03-18 13:45:46 +0200

    added Distance

--- app/main.py
+++ app/main.py
@@ -1,3 +1,46 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    __slots__ = "km"
+
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance | float | int) -> Distance:
+        if isinstance(other, Distance):
+            other = other.km
+        return Distance(self.km + other)
+
+    def __iadd__(self, other: Distance | float | int) -> Distance:
+        if isinstance(other, Distance):
+            other = other.km
+        self.km += other
+        return self
+
+    def __mul__(self, other: float | int) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: float | int) -> Distance:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: float | int) -> bool:
+        return self.km < other
+
+    def __gt__(self, other: float | int) -> bool:
+        return self.km > other
+
+    def __eq__(self, other: float | int) -> bool:
+        return self.km == other
+
+    def __le__(self, other: float | int) -> bool:
+        return self.km <= other
+
+    def __ge__(self, other: float | int) -> bool:
+        return self.km >= other
