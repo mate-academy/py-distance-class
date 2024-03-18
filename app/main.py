f172201540d966a6bd72382ec809e153af1bc55e
Dede <specialized8393@gmail.com>
2024-03-18 13:41:01 +0000

    added float

--- app/main.py
+++ app/main.py
@@ -2,43 +2,43 @@ from __future__ import annotations
 
 
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
         self.real = self.km
 
     def __str__(self) -> str:
-        return f"Distance: {self.km} kilometers."
+        return f"Distance: {self.real} kilometers."
 
     def __repr__(self) -> str:
-        return f"Distance(km={self.km})"
+        return f"Distance(km={self.real})"
 
-    def __add__(self, other: Distance | int) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         return Distance(km=self.km + other.real)
 
-    def __iadd__(self, other: Distance | int) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += other.real
         return self
 
-    def __mul__(self, num: int) -> Distance:
+    def __mul__(self, num: int | float) -> Distance:
         return Distance(km=self.km * num)
 
-    def __truediv__(self, num: int) -> Distance:
+    def __truediv__(self, num: int | float) -> Distance:
         trim_res = self.km / num
         return Distance(km=round(trim_res, 2))
 
-    def __lt__(self, other: Distance | int) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.real < other.real
 
-    def __gt__(self, other: Distance | int) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.real > other.real
 
-    def __eq__(self, other: Distance | int) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.real == other.real
 
-    def __le__(self, other: Distance | int) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.real <= other.real
 
-    def __ge__(self, other: Distance | int) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.real >= other.real
 
     def __len__(self) -> int:
