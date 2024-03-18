1e39bef22b8afa5ffaa78af9692bfa34db009ba3
NioviIki <charmniovi@tutanota.com>
2024-03-18 17:33:51 +0200

    added Union and annotations

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
+from typing import Union
 from math import isclose
 
 
 class Distance:
     __slots__ = "km"
 
-    def __init__(self, km: int | float) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -14,30 +15,30 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | float | int) -> Distance:
+    def __add__(self, other: Union[Distance, int, float]) -> Distance:
         return Distance(self.km + getattr(other, "km", other))
 
-    def __iadd__(self, other: Distance | float | int) -> Distance:
+    def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
         self.km += getattr(other, "km", other)
         return self
 
-    def __mul__(self, other: float | int) -> Distance:
+    def __mul__(self, other: Union[float, int]) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float | int) -> Distance:
+    def __truediv__(self, other: Union[float, int]) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: float | int) -> bool:
+    def __lt__(self, other: Union[Distance, int, float]) -> bool:
         return self.km < getattr(other, "km", other)
 
-    def __gt__(self, other: float | int) -> bool:
+    def __gt__(self, other: Union[Distance, int, float]) -> bool:
         return self.km > getattr(other, "km", other)
 
-    def __eq__(self, other: float | int) -> bool:
+    def __eq__(self, other: Union[Distance, int, float]) -> bool:
         return isclose(self.km, getattr(other, "km", other))
 
-    def __le__(self, other: float | int) -> bool:
+    def __le__(self, other: Union[Distance, int, float]) -> bool:
         return self.km <= getattr(other, "km", other)
 
-    def __ge__(self, other: float | int) -> bool:
+    def __ge__(self, other: Union[Distance, int, float]) -> bool:
         return self.km >= getattr(other, "km", other)
