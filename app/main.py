0e4f413d89ce27d6662c77aaa1f71cfe9e381a94
NioviIki <charmniovi@tutanota.com>
2024-03-19 18:47:25 +0200

    added property and isinstance validation

--- app/main.py
+++ app/main.py
@@ -27,39 +27,39 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[Distance, int, float]) -> Distance:
+    def __add__(self, other: Union[Distance, int, float]) -> Distance | None:
         if isinstance(other, (Distance, int, float)):
             return Distance(self.km + getattr(other, "km", other))
 
-    def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
+    def __iadd__(self, other: Union[Distance, int, float]) -> Distance | None:
         if isinstance(other, (Distance, int, float)):
             self.km += getattr(other, "km", other)
             return self
 
-    def __mul__(self, other: Union[float, int]) -> Distance:
+    def __mul__(self, other: Union[float, int]) -> Distance | None:
         if isinstance(other, (Distance, int, float)):
             return Distance(self.km * other)
 
-    def __truediv__(self, other: Union[float, int]) -> Distance:
+    def __truediv__(self, other: Union[float, int]) -> Distance | None:
         if isinstance(other, (Distance, int, float)):
             return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: Union[Distance, int, float]) -> bool:
+    def __lt__(self, other: Union[Distance, int, float]) -> bool | None:
         if isinstance(other, (Distance, int, float)):
             return self.km < getattr(other, "km", other)
 
-    def __gt__(self, other: Union[Distance, int, float]) -> bool:
+    def __gt__(self, other: Union[Distance, int, float]) -> bool | None:
         if isinstance(other, (Distance, int, float)):
             return self.km > getattr(other, "km", other)
 
-    def __eq__(self, other: Union[Distance, int, float]) -> bool:
+    def __eq__(self, other: Union[Distance, int, float]) -> bool | None:
         if isinstance(other, (Distance, int, float)):
             return isclose(self.km, getattr(other, "km", other))
 
-    def __le__(self, other: Union[Distance, int, float]) -> bool:
+    def __le__(self, other: Union[Distance, int, float]) -> bool | None:
         if isinstance(other, (Distance, int, float)):
             return self.km <= getattr(other, "km", other)
 
-    def __ge__(self, other: Union[Distance, int, float]) -> bool:
+    def __ge__(self, other: Union[Distance, int, float]) -> bool | None:
         if isinstance(other, (Distance, int, float)):
             return self.km >= getattr(other, "km", other)
