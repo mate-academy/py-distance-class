67885a28e55f34a0403e7668d1c66737ea292f39
NioviIki <charmniovi@tutanota.com>
2024-03-19 18:43:52 +0200

    added property and isinstance validation

--- app/main.py
+++ app/main.py
@@ -4,10 +4,22 @@ from math import isclose
 
 
 class Distance:
-    __slots__ = "km"
 
     def __init__(self, km: Union[int, float]) -> None:
         self.km = km
+
+    @property
+    def km(self) -> int | float:
+        return self._km
+
+    @km.setter
+    def km(self, value: int | float) -> None:
+        if not isinstance(value, (int | float)):
+            raise ValueError(
+                f"""Unsupported type. Expected float or int.
+                Got {type(value)} instead."""
+            )
+        self._km = value
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
@@ -16,29 +28,38 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Union[Distance, int, float]) -> Distance:
-        return Distance(self.km + getattr(other, "km", other))
+        if isinstance(other, (Distance, int, float)):
+            return Distance(self.km + getattr(other, "km", other))
 
     def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
-        self.km += getattr(other, "km", other)
-        return self
+        if isinstance(other, (Distance, int, float)):
+            self.km += getattr(other, "km", other)
+            return self
 
     def __mul__(self, other: Union[float, int]) -> Distance:
-        return Distance(self.km * other)
+        if isinstance(other, (Distance, int, float)):
+            return Distance(self.km * other)
 
     def __truediv__(self, other: Union[float, int]) -> Distance:
-        return Distance(round(self.km / other, 2))
+        if isinstance(other, (Distance, int, float)):
+            return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Union[Distance, int, float]) -> bool:
-        return self.km < getattr(other, "km", other)
+        if isinstance(other, (Distance, int, float)):
+            return self.km < getattr(other, "km", other)
 
     def __gt__(self, other: Union[Distance, int, float]) -> bool:
-        return self.km > getattr(other, "km", other)
+        if isinstance(other, (Distance, int, float)):
+            return self.km > getattr(other, "km", other)
 
     def __eq__(self, other: Union[Distance, int, float]) -> bool:
-        return isclose(self.km, getattr(other, "km", other))
+        if isinstance(other, (Distance, int, float)):
+            return isclose(self.km, getattr(other, "km", other))
 
     def __le__(self, other: Union[Distance, int, float]) -> bool:
-        return self.km <= getattr(other, "km", other)
+        if isinstance(other, (Distance, int, float)):
+            return self.km <= getattr(other, "km", other)
 
     def __ge__(self, other: Union[Distance, int, float]) -> bool:
-        return self.km >= getattr(other, "km", other)
+        if isinstance(other, (Distance, int, float)):
+            return self.km >= getattr(other, "km", other)
