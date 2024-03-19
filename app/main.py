f91ac96aef6024d237993a0f157f9e2131302779
Mikhail Yemets <mikhailyemets.pl@gmail.com>
2024-03-19 11:07:50 +0100

    fixing error issue and reusing of func

--- app/main.py
+++ app/main.py
@@ -13,67 +13,43 @@ class Distance:
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
-        else:
-            raise TypeError("Unsupported operand for:+ operation")
 
     def __iadd__(self, other: "Distance") -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
             self.km += other
-        else:
-            raise TypeError("Unsupported operand for for:+= operation")
         return self
 
     def __mul__(self, other: int | float) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        else:
-            raise TypeError("Unsupported operand for:* operation")
 
     def __truediv__(self, other: (int, float)) -> "Distance":
         if isinstance(other, (int, float)):
             result = self.km / other
             return Distance(round(result, 2))
-        else:
-            raise TypeError("Unsupported operand for:/ operation")
 
     def __lt__(self, other: ("Distance", float, int)) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (float, int)):
             return self.km < other
-        else:
-            raise TypeError("Unsupported operand for bool operation")
 
     def __gt__(self, other: ("Distance", float, int)) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (float, int)):
             return self.km > other
-        else:
-            raise TypeError("Unsupported operand for bool operation")
 
     def __eq__(self, other: ("Distance", float, int)) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (float, int)):
             return self.km == other
-        else:
-            raise TypeError("Unsupported operand for bool operation")
 
     def __le__(self, other: ("Distance", float, int)) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        elif isinstance(other, (float, int)):
-            return self.km <= other
-        else:
-            raise TypeError("Unsupported operand for bool operation")
+        return self == other or self < other
 
     def __ge__(self, other: ("Distance", float, int)) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        elif isinstance(other, (float, int)):
-            return self.km >= other
-        else:
-            raise TypeError("Unsupported operand for bool operation")
+        return self == other or self > other
