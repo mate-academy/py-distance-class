9947856633aa617d9790096a797a6bddd99350b2
kekW <panicua12@gmail.com>
2024-03-19 12:49:21 +0200

    fixes

--- app/main.py
+++ app/main.py
@@ -5,94 +5,69 @@ class Distance:
     def __init__(self, km: int | float) -> None:
         self.km = km
 
+    @property
+    def km(self) -> int | float:
+        return self._km
+
+    @km.setter
+    def km(self, value: int | float) -> None:
+        if not isinstance(value, (int | float)):
+            raise ValueError(
+                f"Unsupported type. <class 'float'> "
+                f"or <class 'int'> were expected. "
+                f"Got {type(value)} instead."
+            )
+        self._km = value
+
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float | Distance) -> Distance | TypeError:
-        if isinstance(other, Distance):
+    def __add__(self, other: int | float | Distance) -> Distance:
+        if isinstance(other, (Distance, int, float)):
+            if isinstance(other, (int, float)):
+                other = Distance(other)
             return Distance(self.km + other.km)
-        if isinstance(other, (int, float)):
-            other = Distance(other)
-            return Distance(self.km + other.km)
-        raise self.add_iadd_error_handler(other)
 
-    def __iadd__(self, other: int | float | Distance) -> Distance | TypeError:
-        if isinstance(other, Distance):
+    def __iadd__(self, other: int | float | Distance) -> Distance:
+        if isinstance(other, (Distance, int, float)):
+            if isinstance(other, (int, float)):
+                other = Distance(other)
             self.km += other.km
             return self
-        if isinstance(other, (int, float)):
-            other = Distance(other)
-            self.km += other.km
-            return self
-        raise self.add_iadd_error_handler(other)
 
-    def __mul__(self, other: int | float) -> Distance | TypeError:
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             other = Distance(other)
             return Distance(self.km * other.km)
-        raise self.mul_div_error_handler(other)
 
-    def __truediv__(self, other: int | float) -> Distance | TypeError:
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             other = Distance(other)
             return Distance(round(self.km / other.km, 2))
-        raise self.mul_div_error_handler(other)
 
-    def __lt__(self, other: int | float | Distance) -> bool | TypeError:
-        if isinstance(other, Distance):
+    def __lt__(self, other: int | float | Distance) -> bool:
+        if isinstance(other, (Distance, int, float)):
+            if isinstance(other, (int, float)):
+                other = Distance(other)
             return self.km < other.km
-        if isinstance(other, (int, float)):
-            other = Distance(other)
-            return self.km < other.km
-        raise self.comparison_error_handler(other)
 
-    def __gt__(self, other: int | float | Distance) -> bool | TypeError:
-        if isinstance(other, Distance):
+    def __gt__(self, other: int | float | Distance) -> bool:
+        if isinstance(other, (Distance, int, float)):
+            if isinstance(other, (int, float)):
+                other = Distance(other)
             return self.km > other.km
-        if isinstance(other, (int, float)):
-            other = Distance(other)
-            return self.km > other.km
-        raise self.comparison_error_handler(other)
 
-    def __eq__(self, other: int | float | Distance) -> bool | TypeError:
-        if isinstance(other, Distance):
+    def __eq__(self, other: int | float | Distance) -> bool:
+        if isinstance(other, (Distance, int, float)):
+            if isinstance(other, (int, float)):
+                other = Distance(other)
             return self.km == other.km
-        if isinstance(other, (int, float)):
-            other = Distance(other)
-            return self.km == other.km
-        raise self.comparison_error_handler(other)
 
-    def __le__(
-        self, other: int | float | Distance
-    ) -> bool | callable(TypeError):
+    def __le__(self, other: int | float | Distance) -> bool:
         return not self > other
 
-    def __ge__(
-        self, other: int | float | Distance
-    ) -> bool | callable(TypeError):
+    def __ge__(self, other: int | float | Distance) -> bool:
         return not self < other
-
-    @staticmethod
-    def comparison_error_handler(other: int | float | Distance) -> TypeError:
-        raise TypeError(
-            f"Unsupported type for this method. 'Distance', <class 'float'> "
-            f"or <class 'int'> were expected. But received {type(other)}"
-        )
-
-    @staticmethod
-    def mul_div_error_handler(other: int | float) -> TypeError:
-        raise TypeError(
-            f"Unsupported type for this method. <class 'float'> "
-            f"or <class 'int'> were expected. But received {type(other)}"
-        )
-
-    @staticmethod
-    def add_iadd_error_handler(other: int | float | Distance) -> TypeError:
-        raise TypeError(
-            f"Unsupported type for this method. 'Distance', "
-            f"<class 'float'> or <class 'int'> were expected. "
-            f"But received {type(other)}"
-        )
