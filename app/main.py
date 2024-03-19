e59fe88d0858b74782a2e0b3a33b876f1a113733
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-03-19 12:04:06 +0200

    fix problems

--- app/main.py
+++ app/main.py
@@ -13,56 +13,45 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float | Distance) -> Distance | TypeError:
+    def __add__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
 
-        raise TypeError("Unsupported operand for addition")
-
-    def __iadd__(self, other: int | float | Distance) -> Distance | TypeError:
+    def __iadd__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             self.km += other
-        else:
-            raise TypeError(
-                f"Unsupported operand type(s) for +=: /n"
-                f" 'Distance' and '{type(other)}'"
-            )
         return self
 
-    def __mul__(self, other: int | float) -> Distance | TypeError:
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        elif isinstance(other, Distance):
-            raise TypeError("Unsupported operation.")
 
-    def __truediv__(self, other: int | float) -> Distance | TypeError:
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
-        elif isinstance(other, Distance):
-            raise TypeError("Unsupported operation.")
 
-    def __lt__(self, other: int | float) -> bool | TypeError:
+    def __lt__(self, other: Distance | int | float) -> bool | TypeError:
         return self._compare_with_operand(other, operator.lt)
 
-    def __gt__(self, other: int | float) -> bool | TypeError:
+    def __gt__(self, other: Distance | int | float) -> bool | TypeError:
         return self._compare_with_operand(other, operator.gt)
 
-    def __eq__(self, other: int | float) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self._compare_with_operand(other, operator.eq)
 
-    def __ge__(self, other: int | float) -> bool | TypeError:
+    def __ge__(self, other: Distance | int | float) -> bool | TypeError:
         return self._compare_with_operand(other, operator.ge)
 
-    def __le__(self, other: int | float) -> bool | TypeError:
+    def __le__(self, other: Distance | int | float) -> bool | TypeError:
         return self._compare_with_operand(other, operator.le)
 
     def _compare_with_operand(
             self,
-            other: Distance | float,
+            other: Distance | float | int,
             comparison_operator: callable
     ) -> bool | TypeError:
         if isinstance(other, Distance):
