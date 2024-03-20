26d5be0b065d081d02fe3fd8be6541c5973338cb
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-03-20 13:20:46 +0200

    Changed __le__ and __ge__ methods

--- app/main.py
+++ app/main.py
@@ -47,11 +47,7 @@ class Distance:
         return self.km == other
 
     def __le__(self, other: int | float | Distance) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        return self.km <= other
+        return not self > other
 
     def __ge__(self, other: int | float | Distance) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        return self.km >= other
+        return not self < other
