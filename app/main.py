54375f410752a2529cd9d58f80785faa3728c645
Vitalii <vitalikbashkiser@gmail.com>
2024-03-21 14:14:30 +0200

    Solution#3

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import operator
 from typing import Union, Callable
 
@@ -14,25 +16,28 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union["Distance", float]) -> "Distance":
-        return (Distance(self.km + other.km) if isinstance(other, Distance)
-                else Distance(self.km + other))
+    def __add__(self, other: Union[Distance, int, float]) -> Distance:
+        return Distance(
+            self.km + other.km
+            if isinstance(other, Distance)
+            else self.km + other
+        )
 
-    def __iadd__(self, other: Union["Distance", float]) -> "Distance":
+    def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
         self.km += other.km if isinstance(other, Distance) else other
         return self
 
-    def __mul__(self, other: float) -> "Distance":
+    def __mul__(self, other: float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: Union[float, "Distance"]) -> "Distance":
+    def __truediv__(self, other: Union[float, Distance]) -> Distance:
         if isinstance(other, Distance):
             raise TypeError("Unsupported operation.")
         return Distance(round(self.km / other, 2))
 
     def _compare_with_operand(
             self,
-            other: Union["Distance", float],
+            other: Union[Distance, float],
             comparison_operator: Callable
     ) -> bool:
         return comparison_operator(
@@ -40,17 +45,17 @@ class Distance:
             other.km if isinstance(other, Distance) else other
         )
 
-    def __lt__(self, other: Union["Distance", float]) -> bool:
+    def __lt__(self, other: Union[Distance, float]) -> bool:
         return self._compare_with_operand(other, operator.lt)
 
-    def __gt__(self, other: Union["Distance", float]) -> bool:
+    def __gt__(self, other: Union[Distance, float]) -> bool:
         return self._compare_with_operand(other, operator.gt)
 
-    def __eq__(self, other: Union["Distance", float]) -> bool:
+    def __eq__(self, other: Union[Distance, float]) -> bool:
         return self._compare_with_operand(other, operator.eq)
 
-    def __le__(self, other: Union["Distance", float]) -> bool:
+    def __le__(self, other: Union[Distance, float]) -> bool:
         return self._compare_with_operand(other, operator.le)
 
-    def __ge__(self, other: Union["Distance", float]) -> bool:
+    def __ge__(self, other: Union[Distance, float]) -> bool:
         return self._compare_with_operand(other, operator.ge)
