2277cdbb1a65c4cdd4439277930c1624d38089a2
Shershen Alexandra <hornet240204@gmail.com>
2024-03-21 12:11:22 +0200

    Fix code in magic methods __le__ and __ge__

--- app/main.py
+++ app/main.py
@@ -56,7 +56,7 @@ class Distance:
         return self.km == other
 
     def __le__(self, other: ("Distance", int, float)) -> bool:
-        return self.__lt__(other) or self.__eq__(other)
+        return not self > other
 
     def __ge__(self, other: ("Distance", int, float)) -> bool:
-        return not self.__lt__(other)
+        return not self < other
