223a3ae4b383fa074ffc631e1f6c6618c0c9a4ed
Andrii Chubenko <stalkerock@gmail.com>
2024-03-21 23:21:34 +0200

    Rewrote method _le_ and _ge_ by _gt_ and _eq_

--- app/main.py
+++ app/main.py
@@ -78,13 +78,7 @@ class Distance:
             return self.km == other
 
     def __le__(self, other: Union["Distance", int, float]) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        elif isinstance(other, (int, float)):
-            return self.km <= other
+        return not self > other
 
     def __ge__(self, other: Union["Distance", int, float]) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        elif isinstance(other, (int, float)):
-            return self.km >= other
+        return not self < other
