1ff17eaaac37a8e51f0aa01bb73baa2f61378436
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-03-22 13:28:28 +0000

    solution for __le__ and __ge__ in one line each

--- app/main.py
+++ app/main.py
@@ -46,11 +46,7 @@ class Distance:
         return self.km == other.km
 
     def __le__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, (int, float)):
-            return self.km <= other
-        return self.km <= other.km
+        return not self.km > other
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, (int, float)):
-            return self.km >= other
-        return self.km >= other.km
+        return not self.km < other
