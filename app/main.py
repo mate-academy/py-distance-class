4126fa2016fcf30427d349a76edc221cd45bf7ba
Anna Shevtsova <aniashevtsova@gmail.com>
2024-04-11 12:20:49 +0200

    comments adressed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any
+from typing import Union
 
 
 class Distance:
@@ -12,58 +12,49 @@ class Distance:
     def __repr__(self) -> str:
         return f"{type(self).__name__}(km={self.km})"
 
-    def __add__(self, other: Any) -> Distance:
+    def __add__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
-    def __iadd__(self, other: Any) -> Distance:
+    def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
-        elif isinstance(other, (int, float)):
-            self.km += other
-            return self
+        self.km += other
+        return self
 
-    def __mul__(self, other: Any) -> Distance:
+    def __mul__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        elif isinstance(other, Distance):
-            raise TypeError("Cannot multiply Distance by Distance")
+        raise TypeError("Cannot multiply Distance by Distance")
 
-    def __truediv__(self, other: Any) -> Distance:
+    def __truediv__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
-        elif isinstance(other, Distance):
-            raise TypeError("Cannot divide Distance by Distance")
+        raise TypeError("Cannot divide Distance by Distance")
 
-    def __lt__(self, other: Any) -> bool:
+    def __lt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif isinstance(other, (int, float)):
-            return self.km < other
+        return self.km < other
 
-    def __gt__(self, other: Any) -> bool:
+    def __gt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif isinstance(other, (int, float)):
-            return self.km > other
+        return self.km > other
 
-    def __eq__(self, other: Any) -> bool:
+    def __eq__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, (int, float)):
-            return self.km == other
+        return self.km == other
 
-    def __le__(self, other: Any) -> bool:
+    def __le__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif isinstance(other, (int, float)):
-            return self.km <= other
+        return self.km <= other
 
-    def __ge__(self, other: Any) -> bool:
+    def __ge__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif isinstance(other, (int, float)):
-            return self.km >= other
+        return self.km >= other
