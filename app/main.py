705644055ec3af0695bf331a84be8eb455db16a1
Davyd Kravchuk <kvDevid@gmail.com>
2024-04-16 22:35:21 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class Distance:
     def __init__(self, km: int | float) -> None:
         self.km = km
@@ -8,34 +11,34 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float) -> "Distance":
+    def __add__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         else:
             return Distance(self.km + other)
 
-    def __iadd__(self, other: int | float) -> "Distance":
+    def __iadd__(self, other: Union[int, float, "Distance"]) -> "Distance":
         other_km = other.km if isinstance(other, Distance) else other
         self.km += other_km
         return self
 
-    def __mul__(self, other: int | float) -> "Distance":
+    def __mul__(self, other: Union[int, float, "Distance"]) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int | float) -> "Distance":
+    def __truediv__(self, other: Union[int, float, "Distance"]) -> "Distance":
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: int | float) -> bool:
+    def __lt__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km < other
 
-    def __gt__(self, other: int | float) -> bool:
+    def __gt__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km > other
 
-    def __eq__(self, other: int | float) -> bool:
+    def __eq__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km == other
 
-    def __le__(self, other: int | float) -> bool:
+    def __le__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: int | float) -> bool:
+    def __ge__(self, other: Union[int, float, "Distance"]) -> bool:
         return self.km >= other
