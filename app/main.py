bbadd965bd74c8b304c7e5b938513fe23284e43d
hhorhe <horhehorda@gmail.com>
2024-04-23 17:18:49 +0200

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -11,71 +11,63 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
-        else:
-            return NotImplemented
+        raise TypeError("some informative message")
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
             self.km += other
         else:
-            return NotImplemented
+            raise TypeError("some informative message")
         return self
 
     def __mul__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        else:
-            return NotImplemented
+        raise TypeError("some informative message")
 
     def __truediv__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
-        else:
-            return NotImplemented
+        raise TypeError("some informative message")
 
-    def __lt__(self, other: "Distance") -> bool:
+    def __lt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
-        else:
-            return NotImplemented
+        raise TypeError("some informative message")
 
-    def __gt__(self, other: "Distance") -> bool:
+    def __gt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
-        else:
-            return NotImplemented
+        raise TypeError("some informative message")
 
-    def __eq__(self, other: "Distance") -> bool:
+    def __eq__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
-        else:
-            return NotImplemented
+        raise TypeError("some informative message")
 
-    def __le__(self, other: "Distance") -> bool:
+    def __le__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
-        else:
-            return NotImplemented
+        raise TypeError("some informative message")
 
-    def __ge__(self, other: "Distance") -> bool:
+    def __ge__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
             return self.km >= other
-        else:
-            return NotImplemented
+        raise TypeError("some informative message")
