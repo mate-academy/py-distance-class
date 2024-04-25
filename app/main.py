07e28cb6ad347d4c4cafb8d9b97eb3485cb95df0
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-04-25 23:38:52 +0200

    import annotations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Union
 
 
@@ -11,49 +12,57 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[float, "Distance"]) -> "Distance":
+    def __add__(self, other: Union[float, int, Distance]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        return Distance(self.km + other)
+        else:
+            return Distance(self.km + other)
 
-    def __iadd__(self, other: Union[float, "Distance"]) -> "Distance":
+    def __iadd__(self, other: Union[float, int, Distance]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: Union[float, int]) -> "Distance":
+    def __mul__(self, other: Union[float, int]) -> Distance:
         if isinstance(other, (float, int)):
             return Distance(self.km * other)
-        raise TypeError
+        raise TypeError("Unsupported operand type(s) for *:"
+                        " 'Distance' and '{}'".format(type(other).__name__))
 
-    def __truediv__(self, other: Union[float, int]) -> "Distance":
+    def __truediv__(self, other: Union[float, int]) -> Distance:
         if isinstance(other, (float, int)):
             return Distance(round(self.km / other, 2))
-        raise TypeError
+        raise TypeError("Unsupported operand type(s) for /:"
+                        " 'Distance' and '{}'".format(type(other).__name__))
 
-    def __lt__(self, other: Union[float, "Distance"]) -> bool:
+    def __lt__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        return self.km < other
+        else:
+            return self.km < other
 
-    def __gt__(self, other: Union[float, "Distance"]) -> bool:
+    def __gt__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        return self.km > other
+        else:
+            return self.km > other
 
-    def __eq__(self, other: Union[float, "Distance"]) -> bool:
+    def __eq__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        return self.km == other
+        else:
+            return self.km == other
 
-    def __le__(self, other: Union[float, "Distance"]) -> bool:
+    def __le__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        return self.km <= other
+        else:
+            return self.km <= other
 
-    def __ge__(self, other: Union[float, "Distance"]) -> bool:
+    def __ge__(self, other: Union[float, int, Distance]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        return self.km >= other
+        else:
+            return self.km >= other
