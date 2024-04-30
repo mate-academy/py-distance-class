e64d5200d4211e7c6eecb6d16562be7ab2218f3f
Viktor Panasiuk <Viktor66462@gmail.com>
2024-04-30 23:17:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ class Distance:
     def __mul__(self, other: int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int | float | Distance) -> Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int | float | Distance) -> bool:
