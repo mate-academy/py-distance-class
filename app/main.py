f3f746066db8c0c1ab86478c309516dbd82e1124
Vova <v.kampanchenko@gmail.com>
2024-05-05 15:07:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -11,41 +11,39 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float) -> object:
-        if isinstance(other, int | float):
-            distance2 = self.km + other
+    def __add__(self, distance: int | float) -> object:
+        if isinstance(distance, int | float):
+            distance2 = self.km + distance
             return Distance(distance2)
-        else:
-            distance3 = self.km + other.km
-            return Distance(distance3)
+        distance3 = self.km + distance.km
+        return Distance(distance3)
 
-    def __iadd__(self, other: int | float) -> object:
-        if isinstance(other, int | float):
-            self.km += other
-            return self
+    def __iadd__(self, distance: int | float) -> object:
+        if isinstance(distance, int | float):
+            self.km += distance
         else:
-            self.km += other.km
-            return self
+            self.km += distance.km
+        return self
 
-    def __mul__(self, other: int | float) -> object:
-        distance2 = self.km * other
+    def __mul__(self, distance: int | float) -> object:
+        distance2 = self.km * distance
         return Distance(distance2)
 
-    def __truediv__(self, other: int | float) -> object:
-        distance2 = self.km / other
+    def __truediv__(self, distance: int | float) -> object:
+        distance2 = self.km / distance
         return Distance(round(distance2, 2))
 
-    def __lt__(self, other: Any) -> bool:
-        return self.km < other
+    def __lt__(self, distance: Any) -> bool:
+        return self.km < distance
 
-    def __gt__(self, other: Any) -> bool:
-        return self.km > other
+    def __gt__(self, distance: Any) -> bool:
+        return self.km > distance
 
-    def __eq__(self, other: object) -> bool:
-        return self.km == other
+    def __eq__(self, distance: object) -> bool:
+        return self.km == distance
 
-    def __le__(self, other: Any) -> bool:
-        return self.km <= other
+    def __le__(self, distance: Any) -> bool:
+        return self.km <= distance
 
-    def __ge__(self, other: Any) -> bool:
-        return self.km >= other
+    def __ge__(self, distance: Any) -> bool:
+        return self.km >= distance
