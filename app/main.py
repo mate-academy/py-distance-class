a221adb90730db6f33e73f3e5bcbd1142f9e189d
Vitalii Tsukanov <vitalii.tsunakov@icloud.com>
2024-05-08 21:29:10 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from typing import Union
 
 
 class Distance:
+    pass
+
+
+class Distance:
     def __init__(self, km: float) -> None:
         self.km: float = km
 
@@ -11,7 +15,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union["Distance", float, int]) -> "Distance":
+    def __add__(self, other: Union[Distance, float, int]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -20,7 +24,7 @@ class Distance:
             raise TypeError("Unsupported operand type for +:"
                             " Distance and {}".format(type(other)))
 
-    def __iadd__(self, other: Union["Distance", float, int]) -> "Distance":
+    def __iadd__(self, other: Union[Distance, float, int]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -30,14 +34,14 @@ class Distance:
                             " Distance and {}".format(type(other)))
         return self
 
-    def __mul__(self, scalar: Union[float, int]) -> "Distance":
+    def __mul__(self, scalar: Union[float, int]) -> Distance:
         if isinstance(scalar, (int, float)):
             return Distance(self.km * scalar)
         else:
             raise TypeError("Unsupported operand type for *:"
                             " Distance and {}".format(type(scalar)))
 
-    def __truediv__(self, divisor: Union[float, int]) -> "Distance":
+    def __truediv__(self, divisor: Union[float, int]) -> Distance:
         if isinstance(divisor, (int, float)):
             result = self.km / divisor
             return Distance(round(result, 2))
@@ -45,7 +49,7 @@ class Distance:
             raise TypeError("Unsupported operand type for /:"
                             " Distance and {}".format(type(divisor)))
 
-    def __lt__(self, other: Union["Distance", float, int]) -> bool:
+    def __lt__(self, other: Union[Distance, float, int]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
@@ -54,7 +58,7 @@ class Distance:
             raise TypeError("Unsupported operand type for <:"
                             " Distance and {}".format(type(other)))
 
-    def __gt__(self, other: Union["Distance", float, int]) -> bool:
+    def __gt__(self, other: Union[Distance, float, int]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
@@ -63,7 +67,7 @@ class Distance:
             raise TypeError("Unsupported operand type for >:"
                             " Distance and {}".format(type(other)))
 
-    def __eq__(self, other: Union["Distance", float, int]) -> bool:
+    def __eq__(self, other: Union[Distance, float, int]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
@@ -72,7 +76,7 @@ class Distance:
             raise TypeError("Unsupported operand type for ==:"
                             " Distance and {}".format(type(other)))
 
-    def __le__(self, other: Union["Distance", float, int]) -> bool:
+    def __le__(self, other: Union[Distance, float, int]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
@@ -81,7 +85,7 @@ class Distance:
             raise TypeError("Unsupported operand type for <=:"
                             " Distance and {}".format(type(other)))
 
-    def __ge__(self, other: Union["Distance", float, int]) -> bool:
+    def __ge__(self, other: Union[Distance, float, int]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
