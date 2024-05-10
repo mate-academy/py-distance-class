33b790bf267cfb476e08fbd25893afab59bc9390
Andriy <grushevoyandrey@gmail.com>
2024-05-10 19:39:48 +0300

    add annotation 'Distance'

--- app/main.py
+++ app/main.py
@@ -26,34 +26,34 @@ class Distance:
         elif isinstance(other, (int, float)):
             return Distance(km=self.km * other)
 
-    def __truediv__(self, other: (int, float)) -> bool:
+    def __truediv__(self, other: (int, float)) -> "Distance":
         if isinstance(other, Distance):
             raise TypeError
         return Distance(
             km=round(self.km / other, 2)
         )
 
-    def __lt__(self, other: (int, float)) -> bool:
+    def __lt__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         return self.km < other
 
-    def __gt__(self, other: (int, float)) -> bool:
+    def __gt__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         return self.km > other
 
-    def __eq__(self, other: (int, float)) -> bool:
+    def __eq__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         return self.km == other
 
-    def __le__(self, other: (int, float)) -> bool:
+    def __le__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         return self.km <= other
 
-    def __ge__(self, other: (int, float)) -> bool:
+    def __ge__(self, other: "Distance") -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         return self.km >= other
