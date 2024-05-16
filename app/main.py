45725e3ca71cc0a7339570aa07492b5703453752
Anastasiia Yavorska <anastasiayavorskaya1234@gmail.com>
2024-05-16 19:28:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
-        if not isinstance(other,Distance):
+    def __add__(self, other: "Distance") -> "Distance":
+        if not isinstance(other, Distance):
             return Distance(
                 km=self.km + other
             )
@@ -17,48 +17,48 @@ class Distance:
             km=self.km + other.km
         )
 
-    def __iadd__(self, other):
-        if not isinstance(other,Distance):
+    def __iadd__(self, other: "Distance") -> "Distance":
+        if not isinstance(other, Distance):
             self.km += other
             return self
         self.km += other.km
         return self
 
-    def __mul__(self, other):
-        if not isinstance(other,Distance):
+    def __mul__(self, other: "Distance") -> "Distance":
+        if not isinstance(other, Distance):
             return Distance(
                 km=self.km * other
             )
         raise TypeError("unsupported operand type")
 
-    def __truediv__(self, other):
-        if not isinstance(other,Distance):
+    def __truediv__(self, other: "Distance") -> "Distance":
+        if not isinstance(other, Distance):
             return Distance(
                 km=round(self.km / other, 2)
             )
         raise TypeError("unsupported operand type")
 
-    def __lt__(self, other):
+    def __lt__(self, other: "Distance") -> "Distance":
         if not isinstance(other, Distance):
             return self.km < other
         return self.km < other.km
 
-    def __gt__(self, other):
+    def __gt__(self, other: "Distance") -> bool:
         if not isinstance(other, Distance):
             return self.km > other
         return self.km > other.km
 
-    def __eq__(self,other):
+    def __eq__(self, other: "Distance") -> bool:
         if not isinstance(other, Distance):
             return self.km == other
         return self.km == other.km
 
-    def __le__(self, other):
+    def __le__(self, other: "Distance") -> bool:
         if not isinstance(other, Distance):
             return self.km <= other
         return self.km <= other.km
 
-    def __ge__(self, other):
+    def __ge__(self, other: "Distance") -> bool:
         if not isinstance(other, Distance):
             return self.km >= other
         return self.km >= other.km
