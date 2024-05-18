9e31bad55d89f64012bf08d076f470e9651d9adb
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-05-18 18:16:11 +0300

    "Solution"

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
@@ -8,35 +11,35 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: [int, float, "Distance"]) -> "Distance":
+    def __add__(self, other: [int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + float(other))
 
-    def __iadd__(self, other: [int, float, "Distance"]) -> "Distance":
+    def __iadd__(self, other: [int, float, Distance]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += float(other)
         return self
 
-    def __mul__(self, other: int) -> "Distance":
+    def __mul__(self, other: int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: [float, "Distance"]) -> "Distance":
+    def __truediv__(self, other: [float, Distance]) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: [float, "Distance"]) -> bool:
+    def __lt__(self, other: [float, Distance]) -> bool:
         return self.km < other
 
-    def __gt__(self, other: [float, "Distance"]) -> bool:
+    def __gt__(self, other: [float, Distance]) -> bool:
         return self.km > other
 
-    def __eq__(self, other: [float, "Distance"]) -> bool:
+    def __eq__(self, other: [float, Distance]) -> bool:
         return self.km == other
 
-    def __le__(self, other: [float, "Distance"]) -> bool:
+    def __le__(self, other: [float, Distance]) -> bool:
         return self.km <= other
 
-    def __ge__(self, other: [float, "Distance"]) -> bool:
+    def __ge__(self, other: [float, Distance]) -> bool:
         return self.km >= other
