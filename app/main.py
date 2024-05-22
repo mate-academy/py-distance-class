d43d3c7307b29a68142d2f2930a57262040b1edd
Sergii Musiienko <vpnmus@ukr.net>
2024-05-22 12:20:29 +0300

    fix first Solution

--- app/main.py
+++ app/main.py
@@ -17,23 +17,26 @@ class Distance:
         return Distance(km=self.km + other)
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, Distance):
-            self.km += other.km
-            return self
-        self.km += other
+        self.km += other.km if isinstance(other, Distance) else other
         return self
 
     def __mul__(self, other: int | float) -> Distance:
-        return Distance(km=self.km * other)
+        if isinstance(other, Distance):
+            print("error")
+        return Distance(self.km * other)
 
     def __truediv__(self, other: int | float) -> Distance:
+        if isinstance(other, Distance):
+            print("error")
         return Distance(km=round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
-        return self.km < other
+        tmp = other.km if isinstance(other, Distance) else other
+        return self.km < tmp
 
     def __gt__(self, other: Distance | int | float) -> bool:
-        return self.km > other
+        tmp = other.km if isinstance(other, Distance) else other
+        return self.km > tmp
 
     def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == other
