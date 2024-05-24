fccdcaeb28ab5d190441aae29ec168dfabf784ae
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-05-24 10:27:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -12,20 +12,13 @@ class Distance:
         return f"Distance(km={self.km})"
 
     def __add__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
-            return Distance(self.km + other)
-        else:
-            raise TypeError
+        return Distance(self.km + other.km) \
+            if isinstance(other, Distance) \
+            else Distance(self.km + other)
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, Distance):
-            self.km += other.km
-            return self
-        else:
-            self.km += other
-            return self
+        self.km += other.km if isinstance(other, Distance) else other
+        return self
 
     def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
