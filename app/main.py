0842915da82b6da24928e5add64db5feb1ac14cc
Illia <illiatsiuper@gmail.com>
2024-06-16 14:45:20 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,52 +1,73 @@
+from __future__ import annotations
+
+
 class Distance:
-    def __init__(self, km: float = 0.0):
+    def __init__(self, km: float | int):
         self.km = km
 
-    def __str__(self):
-        return f"Distance is - {self.km} kilometers"
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
-        if isinstance(other, Distance) is False:
-            raise TypeError("Wrong instance, try again")
-        return Distance(self.km + other.km)
+    def __add__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        raise TypeError("Wrong Instance")
 
-    def __iadd__(self, other):
-        self.km = self.km + other.km
-        return self.km
+    def __iadd__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        elif isinstance(other, (int, float)):
+            self.km += other
+        else:
+            raise TypeError("Wrong Instance")
+        return self
 
-    def __mul__(self, other):
-        return Distance(self.km * other.km)
+    def __mul__(self, other: Distance) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(self.km * other)
+        raise TypeError("Wrong Instance")
 
-    def __truediv__(self, other):
-        return Distance(round(self.km / other.km, 2))
+    def __truediv__(self, other: Distance) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(round(self.km / other, 2))
+        raise TypeError("Wrong Instance")
 
-    def __lt__(self, other):
-        return Distance(self.km + other.km > other.km + self.km)
+    def __lt__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        elif isinstance(other, (int, float)):
+            return self.km < other
+        raise TypeError("Wrong Instance")
 
-    def __gt__(self, other):
-        return Distance(self.km > other.km)
+    def __gt__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        elif isinstance(other, (int, float)):
+            return self.km > other
+        raise TypeError("Wrong Instance")
 
-    def __eq__(self, other):
-        return Distance(self.km == other.km)
+    def __eq__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        elif isinstance(other, (int, float)):
+            return self.km == other
+        raise TypeError("Wrong Instance")
 
-    def __le__(self, other):
-        return Distance(self.km <= other.km)
+    def __le__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        elif isinstance(other, (int, float)):
+            return self.km <= other
+        raise TypeError("Wrong Instance")
 
-    def __ge__(self, other):
-        return Distance(self.km >= other.km)
-
-
-distance1 = Distance(km=20)
-distance2 = Distance(km=30)
-
-
-
-
-
-
-
-
-
+    def __ge__(self, other: Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        elif isinstance(other, (int, float)):
+            return self.km >= other
+        raise TypeError("Wrong Instance")
