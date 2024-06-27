d01d14a14d3d7de2d040ea9476bb5f000aaf7ce4
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-06-28 01:47:31 +0300

    Implement Magic Methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,57 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int or float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km!r})"
+
+    def __add__(self, other: int or "Distance") -> "Distance":
+        if isinstance(other, (int, float)):
+            return Distance(
+                self.km + other
+            )
+        return Distance(
+            self.km + other.km
+        )
+
+    def __iadd__(self, other: int or "Distance") -> "Distance":
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        self.km += other
+        return self
+
+    def __mul__(self, other: int) -> "Distance":
+        self.km *= other
+        return self
+
+    def __truediv__(self, other: int) -> "Distance":
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: int or "Distance") -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        return self.km < other
+
+    def __gt__(self, other: int or "Distance") -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
+
+    def __eq__(self, other: int or "Distance") -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        return self.km == other
+
+    def __le__(self, other: int or "Distance") -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        return self.km <= other
+
+    def __ge__(self, other: int or "Distance") -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return self.km >= other
