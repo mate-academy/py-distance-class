d2a97316d3205e4fc1e75acd4b3bfddcb4c76e4a
Stepan <oleksiukstepann@gmail.com>
2024-07-12 17:13:40 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
 
-    def __str__(self) -> None:
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
     def __repr__(self) -> str:
