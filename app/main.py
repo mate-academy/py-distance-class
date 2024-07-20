d0d8c4f1533dccbcca8d38ba6e25197c75f222e0
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-20 23:16:09 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,41 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, dist: float) -> "Distance":
+        if not isinstance(dist, Distance):
+            dist = Distance(dist)
+        return Distance(self.km + dist.km)
+
+    def __iadd__(self, dist: float) -> "Distance":
+        if not isinstance(dist, Distance):
+            dist = Distance(dist)
+        self.km += dist.km
+        return self
+
+    def __mul__(self, mult: int) -> "Distance":
+        return Distance(self.km * mult)
+
+    def __truediv__(self, div: int) -> "Distance":
+        return Distance(round(self.km / div, 2))
+
+    def __lt__(self, dist: int) -> bool:
+        return True if self.km < dist else False
+
+    def __gt__(self, dist: int) -> bool:
+        return True if self.km > dist else False
+
+    def __eq__(self, dist: int) -> bool:
+        return True if self.km == dist else False
+
+    def __le__(self, dist: int) -> bool:
+        return True if self.km <= dist else False
+
+    def __ge__(self, dist: int) -> bool:
+        return True if self.km >= dist else False
