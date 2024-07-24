836a540994d1d7c6e92ea3e1cb54e9a4b53c537f
serhii <ockar2018@gmail.com>
2024-07-24 17:24:28 +0300

    fixed flake8 mistakes

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: int) -> int:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -16,7 +16,7 @@ class Distance:
         else:
             return None
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: int) -> int:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -25,20 +25,20 @@ class Distance:
             return None
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: int) -> int:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         else:
             return None
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: int) -> int:
         if isinstance(other, (int, float)):
             result = self.km / other
             return Distance(round(result, 2))
         else:
             return None
 
-    def comparison(self, other):
+    def comparison(self, other: int) -> bool:
         if isinstance(other, Distance):
             return self.km - other.km
         elif isinstance(other, (int, float)):
@@ -46,17 +46,17 @@ class Distance:
         else:
             return None
 
-    def __lt__(self, other):
+    def __lt__(self, other: int) -> bool:
         return self.comparison(other) < 0
 
-    def __gt__(self, other):
+    def __gt__(self, other: int) -> bool:
         return self.comparison(other) > 0
 
-    def __eq__(self, other):
+    def __eq__(self, other: int) -> bool:
         return self.comparison(other) == 0
 
-    def __le__(self, other):
+    def __le__(self, other: int) -> bool:
         return self.comparison(other) <= 0
 
-    def __ge__(self, other):
+    def __ge__(self, other: int) -> bool:
         return self.comparison(other) >= 0
