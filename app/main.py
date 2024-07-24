a3e6440ec1bf8f071648f4c5bbce9f29f4adba8f
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-07-24 08:39:36 +0300

    a little correction

--- app/main.py
+++ app/main.py
@@ -11,13 +11,13 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union["Distance", int]) -> "Distance":
+    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(km=self.km + other.km)
 
         return Distance(km=self.km + other)
 
-    def __iadd__(self, other: Union["Distance", int]) -> "Distance":
+    def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
             return self
@@ -25,12 +25,11 @@ class Distance:
         return self
 
     def __mul__(self, other: Union[int, float]) -> "Distance":
-        if isinstance(other, int | float):
-            return Distance(km=self.km * other)
+
+        return Distance(km=self.km * other)
 
     def __truediv__(self, other: Union[int, float]) -> "Distance":
-        if isinstance(other, int | float):
-            return Distance(km=round(self.km / other, 2))
+        return Distance(km=round(self.km / other, 2))
 
     def __lt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
