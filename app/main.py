02c9e6d05fd06aac74b57b9915d755c0dc4499fb
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-07-25 13:02:45 +0200

    implement py-distance-class with no duplicate

--- app/main.py
+++ app/main.py
@@ -19,10 +19,7 @@ class Distance:
             return Distance(self.km + other)
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, Distance):
-            self.km += other.km
-        elif isinstance(other, (int, float)):
-            self.km += other
+        self.km = self.__add__(other)
         return self
 
     def __mul__(self, other: int | float) -> Distance | None:
@@ -49,14 +46,10 @@ class Distance:
         return self.km > other
 
     def __le__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        return self.km <= other
+        return self.__lt__(other) or self.__eq__(other)
 
     def __ge__(self, other: Distance | int | float) -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        return self.km >= other
+        return self.__gt__(other) or self.__eq__(other)
 
     def __eq__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
