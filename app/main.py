e868c1d0e10d46400cf3b2f1a4830a81d9af33db
Misha Havryliuk <misharob31415@gmail.com>
2024-07-25 12:17:51 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
-from functools import total_ordering
-from typing import Union
+from __future__ import annotations
 
 
-@total_ordering
 class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
@@ -13,14 +11,14 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union["Distance", int]) -> "Distance":
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
         return NotImplemented
 
-    def __iadd__(self, other: Union["Distance", int]) -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -29,12 +27,12 @@ class Distance:
             return NotImplemented
         return self
 
-    def __mul__(self, other: Union[int, float]) -> "Distance":
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         return NotImplemented
 
-    def __truediv__(self, other: Union[int, float]) -> "Distance":
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
         return NotImplemented
@@ -46,9 +44,18 @@ class Distance:
             return self.km == other
         return NotImplemented
 
-    def __lt__(self, other: Union["Distance", int]) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
         return NotImplemented
+
+    def __le__(self, other: Distance | int | float) -> bool:
+        return self < other or self == other
+
+    def __gt__(self, other: Distance | int | float) -> bool:
+        return not (self <= other)
+
+    def __ge__(self, other: Distance | int | float) -> bool:
+        return not (self < other)
