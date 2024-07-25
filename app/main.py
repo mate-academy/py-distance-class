dfe28748a108388954de0a2d189ecbae12f64295
Maxim Turenko <turenkomaksim099@gmail.com>
2024-07-25 13:02:02 +0300

    implement py-distance-class(fixed duplicate code, added division by zero check in __truediv__)

--- app/main.py
+++ app/main.py
@@ -27,34 +27,28 @@ class Distance:
 
     def __truediv__(self, other: "int | float") -> "Distance":
         if isinstance(other, (int, float)):
+            if other == 0:
+                raise ValueError("Cannot divide by zero")
             return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: "Distance | int | float") -> bool:
+    @staticmethod
+    def _get_km(other: "Distance | int | float") -> float:
         if isinstance(other, Distance):
-            return self.km < other.km
+            return other.km
         elif isinstance(other, (int, float)):
-            return self.km < other
+            return other
+
+    def __lt__(self, other: "Distance | int | float") -> bool:
+        return self.km < self._get_km(other)
 
     def __gt__(self, other: "Distance | int | float") -> bool:
-        if isinstance(other, Distance):
-            return self.km > other.km
-        elif isinstance(other, (int, float)):
-            return self.km > other
+        return self.km > self._get_km(other)
 
     def __eq__(self, other: "Distance | int | float") -> bool:
-        if isinstance(other, Distance):
-            return self.km == other.km
-        elif isinstance(other, (int, float)):
-            return self.km == other
+        return self.km == self._get_km(other)
 
     def __le__(self, other: "Distance | int | float") -> bool:
-        if isinstance(other, Distance):
-            return self.km <= other.km
-        elif isinstance(other, (int, float)):
-            return self.km <= other
+        return self.km <= self._get_km(other)
 
     def __ge__(self, other: "Distance | int | float") -> bool:
-        if isinstance(other, Distance):
-            return self.km >= other.km
-        elif isinstance(other, (int, float)):
-            return self.km >= other
+        return self.km >= self._get_km(other)
