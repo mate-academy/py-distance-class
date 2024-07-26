2ab0ec7c193e74bb18ff079ad33f114161575a9c
Yakiv Lobach <lobachyakiv@gmail.com>
2024-07-26 11:29:50 +0300

    Distance class fourth_try

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from typing import Union
 
 
 class Distance:
@@ -14,42 +13,40 @@ class Distance:
         return f"Distance(km={self.km})"
 
     @staticmethod
-    def _convert_to_km(other: Union[int, float, Distance]) -> float:
+    def _convert_to_km(other: int | float | Distance) -> float:
         if isinstance(other, Distance):
             return other.km
-        elif isinstance(other, Union[int, float, Distance]):
+        elif isinstance(other, int | float):
             return float(other)
         raise TypeError("Unsupported type for arithmetic operation")
 
-    def __add__(self, other: Union[int, float, Distance]) -> "Distance":
+    def __add__(self, other: int | float | Distance) -> Distance:
         return Distance(self.km + self._convert_to_km(other))
 
-    def __iadd__(self, other: Union[int, float, Distance]) -> "Distance":
+    def __iadd__(self, other: int | float | Distance) -> Distance:
         self.km += self._convert_to_km(other)
         return self
 
-    def __mul__(self, other: Union[int, float, Distance]) -> "Distance":
+    def __mul__(self, other: int | float | Distance) -> Distance:
         return Distance(self.km * self._convert_to_km(float(other)))
 
-    def __truediv__(self, other: (int, float)) -> "Distance":
+    def __truediv__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             raise TypeError("Unsupported type for arithmetic operation")
         other_km = self._convert_to_km(other)
-        if other_km == 0:
-            raise ZeroDivisionError("Cannot divide by zero")
         return Distance(round(self.km / other_km, 2))
 
-    def __lt__(self, other: Union[int, float, Distance]) -> bool:
+    def __lt__(self, other: int | float | Distance) -> bool:
         return self.km < self._convert_to_km(other)
 
-    def __gt__(self, other: Union[int, float, Distance]) -> bool:
+    def __gt__(self, other: int | float | Distance) -> bool:
         return self.km > self._convert_to_km(other)
 
-    def __eq__(self, other: Union[int, float, Distance]) -> bool:
+    def __eq__(self, other: int | float | Distance) -> bool:
         return self.km == self._convert_to_km(other)
 
-    def __le__(self, other: Union[int, float, Distance]) -> bool:
+    def __le__(self, other: int | float | Distance) -> bool:
         return not self.__gt__(other)
 
-    def __ge__(self, other: Union[int, float, Distance]) -> bool:
+    def __ge__(self, other: int | float | Distance) -> bool:
         return not self.__lt__(other)
