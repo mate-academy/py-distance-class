0db026dd91a2b13458bfedcb012e0ab36bd4deba
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-07-27 23:26:01 +0300

    class (magic) method __gt__()

--- app/main.py
+++ app/main.py
@@ -34,3 +34,8 @@ class Distance:
         if isinstance(other, Distance):
             return self.km < other.km
         return self.km < other
+
+    def __gt__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
