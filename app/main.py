aadcde5312efcfac1db0afb0ff9864f7f8390a4e
serhii <ockar2018@gmail.com>
2024-07-29 21:46:42 +0300

    fixed some mistakes

--- app/main.py
+++ app/main.py
@@ -14,52 +14,42 @@ class Distance:
     def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
-            return Distance(self.km + other)
-        else:
-            return None
+        return Distance(self.km + other)
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
-        elif isinstance(other, (int, float)):
-            self.km += other
-        else:
-            return None
+            return self
+        self.km += other
         return self
 
-    def __mul__(self, other: Distance | int | float) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        else:
-            return None
+        raise TypeError
 
-    def __truediv__(self, other: Distance | int | float) -> Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             result = self.km / other
             return Distance(round(result, 2))
-        else:
-            return None
+        raise TypeError
 
     def comparison(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return self.km - other.km
-        elif isinstance(other, (int, float)):
-            return self.km - other
-        else:
-            return None
+        return self.km - other
 
-    def __lt__(self, other: int) -> bool:
+    def __lt__(self, other: int | float) -> bool:
         return self.comparison(other) < 0
 
-    def __gt__(self, other: int) -> bool:
+    def __gt__(self, other: int | float) -> bool:
         return self.comparison(other) > 0
 
-    def __eq__(self, other: int) -> bool:
+    def __eq__(self, other: int | float) -> bool:
         return self.comparison(other) == 0
 
-    def __le__(self, other: int) -> bool:
-        return self.comparison(other) <= 0
+    def __le__(self, other: int | float) -> bool:
+        return not self > other
 
-    def __ge__(self, other: int) -> bool:
-        return self.comparison(other) >= 0
+    def __ge__(self, other: int | float) -> bool:
+        return not self < other
