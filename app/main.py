5c66c9aed2c59967c2b3bf6cca8ca2d3323f0213
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-07-29 13:12:17 +0300

    add magic methods

--- app/main.py
+++ app/main.py
@@ -25,13 +25,11 @@ class Distance:
             self.km += other.km
         return self
 
-    def __mul__(self, other: Distance | int) -> Distance:
+    def __mul__(self, other: Distance | int | float) -> Distance:
         if not isinstance(other, Distance):
-            return Distance(
-                km=self.km * other
-            )
+            return Distance(km=self.km * other)
 
-    def __truediv__(self, other: Distance) -> Distance:
+    def __truediv__(self, other: Distance | int | float) -> Distance:
         if not isinstance(other, Distance):
             return Distance(
                 km=round(self.km / other, 2)
@@ -53,11 +51,7 @@ class Distance:
         return self.km == other.km
 
     def __le__(self, other: Distance | int) -> bool:
-        if not isinstance(other, Distance):
-            return self.km <= other
-        return self.km <= other.km
+        return not self.__gt__(other)
 
     def __ge__(self, other: Distance | int) -> bool:
-        if not isinstance(other, Distance):
-            return self.km >= other
-        return self.km >= other.km
+        return not self.__lt__(other)
