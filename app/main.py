380318207f716bddad356b725be3cf85332b31e6
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-07-31 15:11:12 +0300

    add init and str methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
