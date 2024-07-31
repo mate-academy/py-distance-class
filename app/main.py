849bc439a5e9587cff964868332d3d8561c7d1c3
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-07-31 16:43:42 +0300

    add repr and add methods

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+from __future__ import annotations
+
 class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        if isinstance(other, (int, float)):
+            return Distance(self.km + other)
