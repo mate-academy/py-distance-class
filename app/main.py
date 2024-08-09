5b59f09fbf58a795a3f192e5bd06b33ff0d44d77
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-08-10 00:42:03 +0300

    fix if-else condition in methods

--- app/main.py
+++ app/main.py
@@ -17,94 +17,85 @@ class Distance:
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'Distance', 'int' or 'float'"
-            )
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'Distance', 'int' or 'float'"
+        )
 
     def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, Distance):
             self.km = self.km + other.km
+            return self
         elif isinstance(other, (int, float)):
             self.km = self.km + other
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'Distance', 'int' or 'float'"
-            )
-
-        return self
+            return self
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'Distance', 'int' or 'float'"
+        )
 
     def __mul__(self, other: Union[int, float]) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'int' or 'float'"
-            )
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'int' or 'float'"
+        )
 
     def __truediv__(self, other: Union[int, float]) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'int' or 'float'"
-            )
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'int' or 'float'"
+        )
 
     def __lt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return True if self.km < other.km else False
         elif isinstance(other, (int, float)):
             return True if self.km < other else False
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'Distance', 'int' or 'float'"
-            )
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'Distance', 'int' or 'float'"
+        )
 
     def __gt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return True if self.km > other.km else False
         elif isinstance(other, (int, float)):
             return True if self.km > other else False
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'Distance', 'int' or 'float'"
-            )
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'Distance', 'int' or 'float'"
+        )
 
     def __eq__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return True if self.km == other.km else False
         elif isinstance(other, (int, float)):
             return True if self.km == other else False
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'Distance', 'int' or 'float'"
-            )
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'Distance', 'int' or 'float'"
+        )
 
     def __le__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return True if self.km <= other.km else False
         elif isinstance(other, (int, float)):
             return True if self.km <= other else False
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'Distance', 'int' or 'float'"
-            )
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'Distance', 'int' or 'float'"
+        )
 
     def __ge__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return True if self.km >= other.km else False
         elif isinstance(other, (int, float)):
             return True if self.km >= other else False
-        else:
-            raise TypeError(
-                f"Wrong type of '{other}'."
-                f"Should be 'Distance', 'int' or 'float'"
-            )
+        raise TypeError(
+            f"Wrong type of '{other}'."
+            f"Should be 'Distance', 'int' or 'float'"
+        )
