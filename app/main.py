b36b0f99df5cbfcaa424fe689b2448b043e5e960
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-08-10 19:07:26 +0300

    Fix annotations

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from __future__ import annotations
 
 
 class Distance:
@@ -12,41 +12,41 @@ class Distance:
         return f"Distance(km={self.km})"
 
     @staticmethod
-    def to_distance(value: Union["Distance", int, float]) -> float:
+    def to_distance(value: Distance | int | float) -> float:
         if isinstance(value, Distance):
             return value.km
         elif isinstance(value, (int, float)):
             return float(value)
         raise TypeError("Unsupported type for arithmetic with Distance")
 
-    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
+    def __add__(self, other: Distance | int | float) -> Distance:
         return Distance(self.km + self.to_distance(other))
 
-    def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += self.to_distance(other)
         return self
 
-    def __mul__(self, other: Union[int, float]) -> "Distance":
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         raise TypeError("Multiplication only supports int or float types")
 
-    def __truediv__(self, other: Union[int, float]) -> "Distance":
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
         raise TypeError("Division only supports int or float types")
 
-    def __lt__(self, other: Union["Distance", int, float]) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < self.to_distance(other)
 
-    def __gt__(self, other: Union["Distance", int, float]) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > self.to_distance(other)
 
-    def __eq__(self, other: Union["Distance", int, float]) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == self.to_distance(other)
 
-    def __le__(self, other: Union["Distance", int, float]) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= self.to_distance(other)
 
-    def __ge__(self, other: Union["Distance", int, float]) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= self.to_distance(other)
