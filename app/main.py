f52dac76943d370d5fa3edbc573ad6cc1a469d76
taras <vadym@ohmanalytics.com>
2024-08-17 19:53:17 +0300

    solution 2

--- app/main.py
+++ app/main.py
@@ -12,33 +12,33 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         km = other.km if isinstance(other, Distance) else other
         return Distance(self.km + km)
 
-    def __iadd__(self, other: Distance | int) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += self.check_instance(other)
         return self
 
-    def __mul__(self, number: int) -> Distance:
+    def __mul__(self, number: int | float) -> Distance:
         return Distance(self.km * number)
 
-    def __truediv__(self, number: int) -> Distance:
+    def __truediv__(self, number: int | float) -> Distance:
         return Distance(round(self.km / number, 2))
 
-    def __lt__(self, other: Distance) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < self.check_instance(other)
 
-    def __gt__(self, other: Distance) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > self.check_instance(other)
 
-    def __eq__(self, other: Distance) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == self.check_instance(other)
 
-    def __le__(self, other: Distance) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= self.check_instance(other)
 
-    def __ge__(self, other: Distance) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= self.check_instance(other)
 
     @staticmethod
