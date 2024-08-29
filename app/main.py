5e0c81b552d850b4c42bde6b5ad80e969ca41cab
Artem Mahda <artm.magda@gmail.com>
2024-08-29 16:03:31 +0300

    added int | float to other: Distance

--- app/main.py
+++ app/main.py
@@ -12,10 +12,10 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         return Distance(km=self.km + other.real)
 
-    def __iadd__(self, other: Distance) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += other.real
         return self
 
@@ -25,17 +25,17 @@ class Distance:
     def __truediv__(self, value: float) -> Distance:
         return Distance(km=round((self.km / value), 2))
 
-    def __lt__(self, other: Distance) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < other.real
 
-    def __gt__(self, other: Distance) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > other.real
 
-    def __eq__(self, other: Distance) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == other.real
 
-    def __le__(self, other: Distance) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= other.real
 
-    def __ge__(self, other: Distance) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= other.real
