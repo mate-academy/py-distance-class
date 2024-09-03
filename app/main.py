5d11cc7ffb5fab0d2670e8f5dae4abd1d1e07fcb
Lev <lev2099@gmail.com>
2024-09-03 18:29:54 +0300

    Add Distance annotation

--- app/main.py
+++ app/main.py
@@ -41,25 +41,25 @@ class Distance:
     def __eq__(self, other: int | float) -> bool:
         return self.km == other
 
-    def __gt__(self, other: int | float) -> bool:
+    def __gt__(self, other: Distance| int | float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
 
-    def __ge__(self, other: int | float) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
             return self.km >= other
 
-    def __lt__(self, other: int | float) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
 
-    def __le__(self, other: int | float) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
