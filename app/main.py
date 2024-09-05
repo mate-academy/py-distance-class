32e628923bf9e80c8cebdf58266df2f0eb37484e
olgas <olgashydlovska@gmail.com>
2024-09-05 09:37:20 +0300

    Changing to future annotations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -8,23 +11,23 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: any) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
 
-    def __iadd__(self, other: any) -> "Distance":
+    def __iadd__(self, other: any) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
             self.km += other
         return self
 
-    def __mul__(self, multiplier: float) -> "Distance":
+    def __mul__(self, multiplier: float) -> Distance:
         return Distance(self.km * multiplier)
 
-    def __truediv__(self, divisor: float) -> "Distance":
+    def __truediv__(self, divisor: float) -> Distance:
         return Distance(round((self.km / divisor), 2))
 
     def __lt__(self, other: any) -> bool:
