6a11a21180be76531712b3197e518cb2848679cd
Viacheslav417865 <sfinks417865@gmail.com>
2024-09-09 18:45:54 +0300

    new Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+from typing import Union
+
+
 class Distance:
 
     def __init__(self, km: float) -> None:
@@ -9,7 +13,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: object) -> "Distance":
+    def __add__(self, other: Union[Distance, int, float]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
