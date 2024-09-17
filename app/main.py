fc5b1e3ab9c35621eb7260a750d9eec1c75f66c3
Fedir <fedor.agureev@gmail.com>
2024-09-17 23:24:48 +0300

    fixes flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
+
 class Distance:
-    def __init__(self, km: float) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -8,35 +11,35 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: int) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: Distance | int) -> bool:
         return self.km < (other.km if isinstance(other, Distance) else other)
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: Distance | int) -> bool:
         return self.km > (other.km if isinstance(other, Distance) else other)
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: Distance | int) -> bool:
         return self.km == (other.km if isinstance(other, Distance) else other)
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: Distance | int) -> bool:
         return self.km <= (other.km if isinstance(other, Distance) else other)
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: Distance | int) -> bool:
         return self.km >= (other.km if isinstance(other, Distance) else other)
