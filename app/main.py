85552aa04c47eec5cbbca189525c6bc121518752
Maksym Kaptsov <makskaptsov13@gmail.com>
2024-09-22 19:01:47 +0300

    Solved problems with type annotations

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Union
 
 
 class Distance:
@@ -11,72 +11,72 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Any) -> object:
+    def __add__(self, other: Union["Distance", int, float]) -> object:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
-        else:
-            assert TypeError("Unsupported element type")
 
-    def __iadd__(self, other: Any) -> object:
+        raise TypeError("Unsupported element type")
+
+    def __iadd__(self, other: Union["Distance", int, float]) -> object:
         if isinstance(other, Distance):
             self.km += other.km
             return self
         elif isinstance(other, (int, float)):
             self.km += other
             return self
-        else:
-            assert TypeError("Unsupported element type")
 
-    def __mul__(self, other: Any) -> object:
+        raise TypeError("Unsupported element type")
+
+    def __mul__(self, other: Union["Distance", int, float]) -> object:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        else:
-            assert TypeError("Unsupported element type")
 
-    def __truediv__(self, other: Any) -> object:
+        raise TypeError("Unsupported element type")
+
+    def __truediv__(self, other: Union[int, float]) -> object:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
-        else:
-            assert TypeError("Unsupported element type")
 
-    def __lt__(self, other: Any) -> bool:
+        raise TypeError("Unsupported element type")
+
+    def __lt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
-        else:
-            assert TypeError("Unsupported element type")
 
-    def __gt__(self, other: Any) -> bool:
+        raise TypeError("Unsupported element type")
+
+    def __gt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
-        else:
-            assert TypeError("Unsupported element type")
 
-    def __eq__(self, other: Any) -> bool:
+        raise TypeError("Unsupported element type")
+
+    def __eq__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
-        else:
-            assert TypeError("Unsupported element type")
 
-    def __le__(self, other: Any) -> bool:
+        raise TypeError("Unsupported element type")
+
+    def __le__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
-        else:
-            assert TypeError("Unsupported element type")
 
-    def __ge__(self, other: Any) -> bool:
+        raise TypeError("Unsupported element type")
+
+    def __ge__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
             return self.km >= other
-        else:
-            assert TypeError("Unsupported element type")
+
+        raise TypeError("Unsupported element type")
