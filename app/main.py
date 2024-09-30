d87bff10b25f7b0d1a9a80f52cf113ca6e24f95f
Evgeniya <didovichevgeniya@gmail.com>
2024-09-30 18:14:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -12,13 +12,13 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: object) -> Distance:
+    def __add__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
 
-    def __iadd__(self, other: object) -> Distance:
+    def __iadd__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -45,7 +45,7 @@ class Distance:
         elif isinstance(other, (int, float)):
             return self.km > other
 
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
