3e5cb018ad6de129c194221b2d4872a9e6cdcbc3
Den_k0 <denys.kotovskiy@gmail.com>
2024-10-03 18:08:51 +0300

    Solution fix

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 from functools import total_ordering
-from typing import Union
 
 
 @total_ordering
 class Distance:
-    def __init__(self, km: Union[int, float]) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     @staticmethod
-    def validate(other: Union[int, float, Distance]) -> float:
+    def validate(other: int | float | Distance) -> float:
         if isinstance(other, Distance):
             return other.km
         elif isinstance(other, (int, float)):
@@ -23,25 +22,25 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[int, float, Distance]) -> Distance:
+    def __add__(self, other: int | float | Distance) -> Distance:
         return Distance(self.km + self.validate(other))
 
-    def __iadd__(self, other: Union[int, float, Distance]) -> Distance:
+    def __iadd__(self, other: int | float | Distance) -> Distance:
         self.km += self.validate(other)
         return self
 
-    def __mul__(self, other: Union[int, float, Distance]) -> Distance:
+    def __mul__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             raise TypeError("Distance instance is not supported")
         return Distance(self.km * self.validate(other))
 
-    def __truediv__(self, other: Union[int, float, Distance]) -> Distance:
+    def __truediv__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             raise TypeError("Distance instance is not supported")
         return Distance(round(self.km / self.validate(other), 2))
 
-    def __lt__(self, other: Union[int, float, Distance]) -> bool:
+    def __lt__(self, other: int | float | Distance) -> bool:
         return self.km < self.validate(other)
 
-    def __eq__(self, other: Union[int, float, Distance]) -> bool:
+    def __eq__(self, other: int | float | Distance) -> bool:
         return self.km == self.validate(other)
