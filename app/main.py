a2007b0a82f9e47264e83395aa919bd99c856a00
Vova Savenkov <sava.v80@gmail.com>
2024-10-09 21:11:15 +0300

    fix mistakes

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 class Distance:
-    def __init__(self, km: any) -> None:
+    def __init__(self, km: float | int) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -11,36 +11,35 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: any) -> Distance:
+    def __add__(self, other: float | int | Distance) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        else:
-            return Distance(self.km + other)
+        return Distance(self.km + other)
 
-    def __iadd__(self, other: any) -> Distance:
+    def __iadd__(self, other: float | int | Distance) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: any) -> Distance:
+    def __mul__(self, other: float | int | Distance) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: any) -> Distance:
+    def __truediv__(self, other: float | int | Distance) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __gt__(self, other: any) -> bool:
+    def __gt__(self, other: float | int | Distance) -> bool:
         return self.km > other
 
-    def __lt__(self, other: any) -> bool:
+    def __lt__(self, other: float | int | Distance) -> bool:
         return self.km < other
 
-    def __ge__(self, other: any) -> bool:
+    def __ge__(self, other: float | int | Distance) -> bool:
         return self.km >= other
 
-    def __le__(self, other: any) -> bool:
+    def __le__(self, other: float | int | Distance) -> bool:
         return self.km <= other
 
-    def __eq__(self, other: any) -> bool:
+    def __eq__(self, other: float | int | Distance) -> bool:
         return self.km == other
