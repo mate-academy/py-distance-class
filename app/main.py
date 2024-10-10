ab226efb71fb12071fe566de9bacbb8c683ed504
Volodymyr Povroznyk <vpovroznyk1998@gmail.com>
2024-10-11 00:09:07 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -14,48 +14,44 @@ class Distance:
     def __add__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(km=self.km + other.km)
-        else:
-            return Distance(km=self.km + other)
+        return Distance(km=self.km + other)
 
     def __iadd__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
-            self.km = self.km + other.km
+            self.km += other.km
         else:
-            self.km = self.km + other
+            self.km += other
         return self
 
     def __mul__(self, other: int) -> Distance:
         return Distance(km=self.km * other)
 
     def __truediv__(self, other: int) -> Distance:
+        if other == 0:
+            raise ZeroDivisionError("Division by zero is not allowed.")
         return Distance(km=round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        else:
-            return self.km < other
+        return self.km < other
 
     def __gt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        else:
-            return self.km > other
+        return self.km > other
 
     def __eq__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        else:
-            return self.km == other
+        return self.km == other
 
     def __le__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        else:
-            return self.km <= other
+        return self.km <= other
 
     def __ge__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        else:
-            return self.km >= other
+        return self.km >= other
