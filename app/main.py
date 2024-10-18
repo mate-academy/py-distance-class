c245f782edc64f7a87139b3a4b3d79033ebf0ca1
Ihor <andreiunderwhat@mail.com>
2024-10-18 20:47:14 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: callable) -> callable:
+    def __add__(self, other: ("Distance", int, float)) -> callable:
         if not isinstance(other, (int, float)):
             return Distance(self.km + other.km)
         else:
             return Distance(self.km + other)
 
-    def __iadd__(self, other: callable) -> callable:
+    def __iadd__(self, other: (int, float)) -> callable:
         if not isinstance(other, (int, float)):
             self.km += other.km
             return self
@@ -22,23 +22,23 @@ class Distance:
             self.km += other
             return self
 
-    def __mul__(self, other: int) -> callable:
+    def __mul__(self, other: int) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other: callable) -> callable:
+    def __truediv__(self, other: ("Distance", int, float)) -> callable:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: int) -> callable:
+    def __lt__(self, other: ("Distance", int, float)) -> callable:
         return self.km < other
 
-    def __gt__(self, other: int) -> callable:
+    def __gt__(self, other: ("Distance", int, float)) -> callable:
         return self.km > other
 
-    def __eq__(self, other: int) -> callable:
+    def __eq__(self, other: ("Distance", int, float)) -> callable:
         return self.km == other
 
-    def __le__(self, other: int) -> callable:
+    def __le__(self, other: ("Distance", int, float)) -> callable:
         return self.km <= other
 
-    def __ge__(self, other: int) -> callable:
+    def __ge__(self, other: ("Distance", int, float)) -> callable:
         return self.km >= other
