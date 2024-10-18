e5af39f47680c3ee476e06ea04d0fedf378066d2
Ihor <andreiunderwhat@mail.com>
2024-10-18 20:32:07 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,44 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: callable) -> callable:
+        if not isinstance(other, (int, float)):
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __iadd__(self, other: callable) -> callable:
+        if not isinstance(other, (int, float)):
+            self.km += other.km
+            return self
+        else:
+            self.km += other
+            return self
+
+    def __mul__(self, other: int) -> callable:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: callable) -> callable:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: int) -> callable:
+        return self.km < other
+
+    def __gt__(self, other: int) -> callable:
+        return self.km > other
+
+    def __eq__(self, other: int) -> callable:
+        return self.km == other
+
+    def __le__(self, other: int) -> callable:
+        return self.km <= other
+
+    def __ge__(self, other: int) -> callable:
+        return self.km >= other
