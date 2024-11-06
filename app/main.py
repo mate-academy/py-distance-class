ae53b54d72f9dd7878802b89a5216a285211b88b
Ilia Terentev <wenhun1@gmail.com>
2024-11-06 13:42:32 +0200

    Solution with changes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Union
+
 
 class Distance:
     def __init__(self, km: int or float) -> None:
@@ -11,54 +13,63 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int or float) -> Distance:
+    def __add__(self, other: Union[Distance, int, float]) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(self.km + other)
+
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, (int, float)):
             self.km += other
             return self
-        else:
-            self.km += other.km
-            return self
 
-    def __mul__(self, other: int or float) -> Distance:
+        self.km += other.km
+        return self
+
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             self.km *= other
             return self
-        else:
-            raise TypeError(f"{other} is not int")
 
-    def __truediv__(self, other: int or float) -> Distance:
+        raise TypeError(f"{other} must be number of type: int or float")
+
+    def __truediv__(self, other: int | float) -> Distance:
+        if other == 0:
+            raise TypeError(f"{other} cannot be division on zero!")
+
         if isinstance(other, (int, float)):
             self.km /= other
             self.km = round(self.km, 2)
             return self
-        else:
-            raise TypeError(f"{other} is not int")
 
-    def __lt__(self, other: Distance or int or float) -> bool:
+        raise TypeError(f"{other} must be number of type: int or float")
+
+    def __lt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km < other
         else:
             return self.km < other.km
 
-    def __gt__(self, other: Distance or int or float) -> bool:
+    def __gt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km > other
         else:
             return self.km > other.km
 
-    def __eq__(self, other: Distance or int or float) -> bool:
+    def __eq__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km == other
         else:
             return self.km == other.km
 
-    def __le__(self, other: Distance or int or float) -> bool:
+    def __le__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km <= other
         else:
             return self.km <= other.km
 
-    def __ge__(self, other: Distance or int or float) -> bool:
+    def __ge__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km >= other
         else:
