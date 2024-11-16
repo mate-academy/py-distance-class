cfc5147f6a842f8774f7f93c6018de389e11e831
Liza Shulika <liza.shulika2009@gmail.com>
2024-11-16 20:50:19 +0200

    Distance

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return Distance(km=self.km + other.km)
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+
+    def __mul__(self, factor):
+        if isinstance(factor, (int, float)):
+            return Distance(km=self.km * factor)
+
+    def __truediv__(self, divisor):
+        if isinstance(divisor, (int, float)) and divisor != 0:
+            return Distance(km=self.km / divisor)
+
