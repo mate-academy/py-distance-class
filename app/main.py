a1fb55c414532de90e6b7da0d6134adef1bcd717
Yehor Yurchenko <yegoryurtchenko05@gmail.com>
2024-11-18 23:43:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
+from __future__ import annotations
+
 class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
+
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
+
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
-    def __add__(self, other: float | int) -> int | float:
+
+    def __add__(self, other: float | int) -> Distance:
         if isinstance(other, Distance):
             # Додавання значень двох лічильників
             return Distance(self.km + other.km)
@@ -14,25 +19,33 @@ class Distance:
             return Distance(self.km + other)
         else:
             return NotImplemented
-    def __iadd__(self, other: float | int) -> any:
+
+    def __iadd__(self, other: float | int) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
-    def __mul__(self, other: float | int) -> any:
+
+    def __mul__(self, other: float | int) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-    def __truediv__(self, other:  float | int) -> any:
+
+    def __truediv__(self, other:  float | int) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
-    def __lt__(self, other: float | int) -> any:
+
+    def __lt__(self, other: float | int) -> bool:
         return self.km < other
-    def __gt__(self, other: float | int) -> any:
+
+    def __gt__(self, other: float | int) -> bool:
         return self.km > other
-    def __eq__(self, other: float | int) -> any:
+
+    def __eq__(self, other: float | int) -> bool:
         return self.km == other
-    def __le__(self, other: float | int) -> any:
+
+    def __le__(self, other: float | int) -> bool:
         return self.km <= other
-    def __ge__(self, other: float | int) -> any:
+
+    def __ge__(self, other: float | int) -> bool:
         return self.km >= other
