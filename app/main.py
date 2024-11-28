698151b67bc7f335e31683d911244fd476b47255
Volodymyr Havrylenko <vladimirvgavrilenko@gmail.com>
2024-11-29 01:16:08 +0200

    Solusion_32_failed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,46 @@
+from __future__ import annotations
+
 class Distance:
-    # Write your code here
-    pass
+    
+    def __init__ (self, real: int) ->None:
+        self.real = real
+        self.km = real
+       
+
+    def __str__(self) -> str:
+        return f"Distance: {self.real} kilometers."
+
+    def __repr__(self) -> str:
+        self.messege = f"Distance(km={self.real})"
+        return self.messege
+
+    def __add__(self, other):
+        self.real = self.real + other.real
+        return self.real
+
+    def __iadd__(self, other) -> int:
+        self.real += other.real
+        return self
+    
+    def __mul__(self, other) -> int:
+        self.real = self.real * other.real
+        return self
+    def __truediv__(self, other) -> int:
+        self.real = round(self.real / other.real, 2)
+        return self.real
+
+    def __lt__(self, other) -> bool:
+        return self.real < other.real
+
+    def __gt__(self, other) -> bool:
+        return self.real > other.real
+    
+    def __eq__(self, other) -> bool:
+        return self.real == other.real
+
+    def __le__(self, other) -> bool: 
+        return self.real <= other.real
+       
+    def __ge__(self, other) -> bool:
+        return self.real >= other.real
+    
