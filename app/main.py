b19bfcbd6a0bb93d7849a8d6109baf965c381159
Trukhanov Danylo <trukhanov.danya@gmail.com>
2024-12-02 13:10:41 +0200

    create function to check if object is int or float, added more descriptive message for TypeError

--- app/main.py
+++ app/main.py
@@ -14,26 +14,26 @@ class Distance:
     def __add__(self, other: Any) -> "Distance":
         if isinstance(other, Distance):
             return Distance(km=self.km + other.km)
-        if isinstance(other, int | float):
+        if self._is_number(other):
             return Distance(km=self.km + other)
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for + operator")
 
     def __iadd__(self, other: Any) -> "Distance":
-        if isinstance(other, int | float):
+        if self._is_number(other):
             self.km += other
             return self
         if isinstance(other, Distance):
             self.km += other.km
             return self
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for += operator")
 
     def __mul__(self, other: Any) -> "Distance":
-        if isinstance(other, int | float):
+        if self._is_number(other):
             return Distance(km=self.km * other)
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for * operator")
 
     def __truediv__(self, other: Any) -> "Distance":
         if isinstance(other, int | float):
@@ -42,44 +42,48 @@ class Distance:
             else:
                 return Distance(km=round(self.km / other, 2))
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for / operator")
 
     def __lt__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        if isinstance(other, int | float):
+        if self._is_number(other):
             return self.km < other
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for < operator")
 
     def __gt__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        if isinstance(other, int | float):
+        if self._is_number(other):
             return self.km > other
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for > operator")
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        if isinstance(other, int | float):
+        if self._is_number(other):
             return self.km == other
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for == operator")
 
     def __le__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        if isinstance(other, int | float):
+        if self._is_number(other):
             return self.km <= other
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for <= operator")
 
     def __ge__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        if isinstance(other, int | float):
+        if self._is_number(other):
             return self.km >= other
         else:
-            raise TypeError("Can't calculate")
+            raise TypeError("Unsupported operand type for >= operator")
+
+    @staticmethod
+    def _is_number(obj: Any) -> bool:
+        return isinstance(obj, (int, float))
