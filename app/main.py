d9c417210e8a9c0974c51cbe2a4603510b2a0674
Vova <vova.gld05@gmail.com>
2024-12-02 12:28:49 +0200

    сÑcorrection of the solution

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 
 class Distance:
 
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     @staticmethod
-    def check_value(value: [Distance, int]) -> int:
+    def check_value(value: Distance | int | float) -> int | float:
         return (
-            value.real if "real" in dir(value)
+            value if isinstance(value, (int, float))
             else value.km
         )
 
@@ -19,18 +19,18 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: [Distance, int]) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         return Distance(
             km=self.km + self.check_value(other)
         )
 
-    def __iadd__(self, other: [Distance, int]) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += self.check_value(other)
         return self
 
-    def __mul__(self, other: [Distance, int]) -> Distance:
+    def __mul__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
-            raise TypeError("Cannot divide one Distance instance by another")
+            raise TypeError("Cannot multiply one Distance instance by another")
 
         elif isinstance(other, (int, float)):
             return Distance(
@@ -38,42 +38,43 @@ class Distance:
             )
         else:
             raise TypeError(
-                f"Unsupported type for division: {type(other).__name__}"
+                f"Unsupported type for multiply: {type(other).__name__}"
             )
 
-    def __truediv__(self, other: [Distance, int]) -> Distance:
+    def __truediv__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             raise TypeError("Cannot divide one Distance instance by another")
+
         elif isinstance(other, (int, float)):
             return Distance(
-                round((self.km / other), 2)
+                km=round((self.km / other), 2)
             )
         else:
             raise TypeError(
                 f"Unsupported type for division: {type(other).__name__}"
             )
 
-    def __lt__(self, other: [Distance, int]) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return (
             self.km < self.check_value(other)
         )
 
-    def __gt__(self, other: [Distance, int]) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return (
             self.km > self.check_value(other)
         )
 
-    def __eq__(self, other: [Distance, int]) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return (
             self.km == self.check_value(other)
         )
 
-    def __le__(self, other: [Distance, int]) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return (
             self.km <= self.check_value(other)
         )
 
-    def __ge__(self, other: [Distance, int]) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return (
             self.km >= self.check_value(other)
         )
