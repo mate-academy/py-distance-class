afbd4bc72636d3298d1ee30ee7acdaa020886934
Dmytro.Poberezhnyi <foxymood@gmail.com>
2024-12-04 10:15:16 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 
 class Distance:
     def __init__(self, km: int) -> None:
@@ -9,24 +11,27 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int) -> int:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         else:
             return Distance(self.km + other)
 
-    def __iadd__(self, other: int) -> int:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, (Distance, int, float)):
             self.km += other.km if isinstance(other, Distance) else other
         return self
 
-    def __mul__(self, other: int) -> int:
+    def __mul__(self, other: Distance | int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int) -> int:
-        return Distance(round(self.km / other, 2))
+    def __truediv__(self, other: Distance | int | float) -> Distance:
+        if other != 0:
+            return Distance(round(self.km / other, 2))
+        else:
+            raise ValueError("Unable to division by zero")
 
-    def __eq__(self, other: int) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         if isinstance(other, (Distance, int, float)):
             return self.km == (
                 other.km if isinstance(other, Distance) else other
