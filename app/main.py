b08493796d137a0025389ec3f8b03afcf9f2f3ea
bc2024-4 <bzabrodskij20@gmail.com>
2024-12-26 17:54:52 +0200

    fix bug

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
@@ -8,68 +11,71 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int) -> "Distance":
+    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
-            self.km = self.km + other.km
-            return Distance(
-                self.km
-            )
+            return Distance(self.km + other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km + other)
         else:
-            self.km = self.km + other.real
-            return Distance(
-                self.km
-            )
+            raise TypeError("Unsupported operand type(s) for +")
 
-    def __iadd__(self, other: int) -> "Distance":
+    def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
-        else:
+        elif isinstance(other, (int, float)):
             self.km += other
+        else:
+            raise TypeError("Unsupported operand type(s) for +=")
         return self
 
-    def __mul__(self, other: int) -> "Distance":
+    def __mul__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         else:
-            raise TypeError("Unsupported operand type(s)")
+            raise TypeError("Unsupported operand type(s) for *")
 
-    def __truediv__(self, other: int) -> "Distance":
+    def __truediv__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
         else:
-            raise TypeError("Unsupported operand type(s)")
+            raise TypeError("Unsupported operand type(s) for /")
 
-    def __lt__(self, other: int) -> bool:
+    def __lt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-
+        elif isinstance(other, (int, float)):
+            return self.km < other
         else:
-            return self.km < other.real
+            raise TypeError("Unsupported operand type(s) for <")
 
-    def __gt__(self, other: int) -> bool:
+    def __gt__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-
+        elif isinstance(other, (int, float)):
+            return self.km > other
         else:
-            return self.km > other.real
+            raise TypeError("Unsupported operand type(s) for >")
 
-    def __le__(self, other: int) -> bool:
+    def __le__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-
+        elif isinstance(other, (int, float)):
+            return self.km <= other
         else:
-            return self.km <= other.real
+            raise TypeError("Unsupported operand type(s) for <=")
 
-    def __ge__(self, other: int) -> bool:
+    def __ge__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-
+        elif isinstance(other, (int, float)):
+            return self.km >= other
         else:
-            return self.km >= other.real
+            raise TypeError("Unsupported operand type(s) for >=")
 
-    def __eq__(self, other: int) -> bool:
+    def __eq__(self, other: Union["Distance", int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-
+        elif isinstance(other, (int, float)):
+            return self.km == other
         else:
-            return self.km == other.real
+            raise TypeError("Unsupported operand type(s) for ==")
