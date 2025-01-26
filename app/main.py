b9a2c0ca47d349ecb9cb42b653fd0e2cca0eebec
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-01-26 14:11:30 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,63 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: float | Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(km=self.km + other.km)
+        else:
+            return Distance(km=self.km + other)
+
+    def __iadd__(self, other: float | Distance) -> Distance:
+        if isinstance(other, Distance):
+            self.km = self.km + other.km
+            return self
+        else:
+            self.km = self.km + other
+            return self
+
+    def __mul__(self, other: float | Distance) -> Distance:
+        return Distance(km=self.km * other)
+
+    def __lt__(self, other: float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        else:
+            return self.km < other
+
+    def __le__(self, other: float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        else:
+            return self.km <= other
+
+    def __gt__(self, other: float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        else:
+            return self.km > other
+
+    def __ge__(self, other: float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        else:
+            return self.km >= other
+
+    def __eq__(self, other: float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        else:
+            return self.km == other
+
+    def __truediv__(self, other: float | Distance) -> Distance:
+        return Distance(km=round(self.km / other, 2))
