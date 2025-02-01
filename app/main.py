1a1304b9a79efdbb08a195ace8abebd129cbfdf0
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-02-01 23:30:51 +0200

    Refactored code.

--- app/main.py
+++ app/main.py
@@ -1,43 +1,33 @@
-from typing import Type
+from __future__ import annotations
 
 
 class Distance:
     def __init__(self, km: any) -> None:
         self.km = km
 
-    def __add__(self, other: Type["Distance"] | int | float) -> "Distance":
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(km={self.km})"
+
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(km=self.km + other.km)
-        elif isinstance(other, (int, float)):
-            return Distance(km=self.km + other)
-        else:
-            raise TypeError(f"Unsupported operand type(s) for +: "
-                            f" 'Distance' and '{type(other).__name__}")
+        return Distance(km=self.km + other)
 
-    def __iadd__(self, other: Type["Distance"] | int | float) -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
-        elif isinstance(other, (int, float)):
-            self.km += other
         else:
-            raise TypeError(f"Unsupported operand type(s) for +: "
-                            f" 'Distance' and '{type(other).__name__}")
-
+            self.km += other
         return self
 
-    def __mul__(self, other: int | float) -> "Distance":
-        if isinstance(other, (int, float)):
-            return Distance(km=self.km * other)
-        else:
-            raise TypeError(f"Unsupported operand type(s) for *: "
-                            f" 'Distance' and '{type(other).__name__}")
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(km=self.km * other)
 
-    def __truediv__(self, other: int | float) -> "Distance":
-        if isinstance(other, (int, float)):
-            return Distance(km=round(self.km / other, 2))
-        else:
-            raise TypeError(f"Unsupported operand type(s) for /: "
-                            f" 'Distance' and '{type(other).__name__}")
+    def __truediv__(self, other: int | float) -> Distance:
+        return Distance(km=round(self.km / other, 2))
 
     def __lt__(self, other: int | float) -> bool:
         return self.km < other
@@ -56,13 +46,3 @@ class Distance:
 
     def __ne__(self, other: int | float) -> bool:
         return self.km != other
-
-    @classmethod
-    def get_info_class(cls) -> Type["Distance"]:
-        return cls
-
-    def __str__(self) -> str:
-        return f"Distance: {self.km} kilometers."
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(km={self.km})"
