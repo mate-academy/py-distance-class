fb350562bebcdd134829e3feee56c15922a91a3b
Yevhenii <110053436+Yevhenii0536@users.noreply.github.com>
2025-02-08 22:16:41 +0200

    fix: added annotations

--- app/main.py
+++ app/main.py
@@ -1,47 +1,50 @@
+from __future__ import annotations
+
+
 class Distance:
     @staticmethod
-    def check_is_instance(other):
+    def check_is_instance(other: Distance | int) -> bool:
         return isinstance(other, Distance)
 
-    def __init__(self, km: int):
+    def __init__(self, km: int) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: Distance | int) -> Distance:
         if self.check_is_instance(other):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Distance | int) -> Distance:
         if self.check_is_instance(other):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: Distance | int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: Distance | int) -> Distance:
         self.km = round(self.km / other, 2)
         return self
 
-    def __lt__(self, other):
+    def __lt__(self, other: Distance | int) -> bool:
         return self.km < other
 
-    def __gt__(self, other):
+    def __gt__(self, other: Distance | int) -> bool:
         return self.km > other
 
-    def __eq__(self, other):
+    def __eq__(self, other: Distance | int) -> bool:
         return self.km == other
 
-    def __le__(self, other):
+    def __le__(self, other: Distance | int) -> bool:
         return self.km <= other
 
-    def __ge__(self, other):
+    def __ge__(self, other: Distance | int) -> bool:
         return self.km >= other
