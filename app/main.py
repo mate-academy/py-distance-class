8e417f3466f3520d5373145978e6522fc71b4aaa
Marek Drządzewski <dzadzumaster@gmail.com>
2025-02-15 13:22:14 +0100

    added init, str, repr add

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
 class Distance:
     # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return  Distance(self.km + other.km)
+        elif isinstance(other, int | float):
+            return Distance(self.km + other)
+        else:
+            raise (
+                TypeError("Unsupported")
+            )
+
+
+
