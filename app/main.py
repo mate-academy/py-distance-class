f11ae186868bc0d458303aa98daf1d56a4e0bd67
Dmytro Lisovyi <kenedy11@ukr.net>
2025-02-15 19:03:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,65 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: int) -> int:
+        ot = other
+        if isinstance(other, Distance):
+            ot = other.km
+        return Distance(self.km + ot)
+
+    def __iadd__(self, other: int) -> int:
+        ot = other
+        if isinstance(other, Distance):
+            ot = other.km
+        self.km += ot
+        return self
+
+    def __mul__(self, other: int) -> int:
+        if not isinstance(other, (float, int)):
+            raise TypeError("Error")
+        if isinstance(other, Distance):
+            raise TypeError("Error")
+
+        ot = other
+        if isinstance(other, Distance):
+            ot = other.km
+        return Distance(self.km * ot)
+
+    def __truediv__(self, other: float) -> float:
+        if not isinstance(other, (float, int)):
+            raise TypeError
+        if isinstance(other, Distance):
+            raise TypeError
+
+        ot = other
+        if isinstance(other, Distance):
+            ot = other.km
+        return Distance(round(self.km / ot, 2))
+
+    def __lt__(self, other: int) -> int:
+        ot = other if isinstance(other, (float, int)) else other.km
+        return self.km < ot
+
+    def __gt__(self, other: int) -> int:
+        ot = other if isinstance(other, (float, int)) else other.km
+        return self.km > ot
+
+    def __eq__(self, other: int) -> int:
+        ot = other if isinstance(other, (float, int)) else other.km
+        return self.km == ot
+
+    def __le__(self, other: int) -> int:
+        ot = other if isinstance(other, (float, int)) else other.km
+        return self.km <= ot
+
+    def __ge__(self, other: float) -> float:
+        ot = other if isinstance(other, (float, int)) else other.km
+        return self.km >= ot
