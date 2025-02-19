9f8c8e81631a5633ea28a2788fb32c3470bf3fec
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-02-19 20:57:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,52 @@
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Any) -> "Distance":
+        if isinstance(other, (int, float)):
+            return Distance(self.km + other)
+
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+
+    def __iadd__(self, other: Any) -> "Distance":
+        if isinstance(other, (int, float)):
+            self.km += other
+
+        elif isinstance(other, Distance):
+            self.km += other.km
+
+        return self
+
+    def __mul__(self, other: Any) -> "Distance":
+        return Distance(self.km * other)
+
+    def __truediv__(self, other : Any) -> "Distance":
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other : Any) -> bool:
+        if isinstance(other, (int, float)):
+            return self.km < other
+        elif isinstance(other, Distance):
+            return self.km < other.km
+
+    def __gt__(self, other : Any) -> bool:
+        return self.km > other
+
+    def __eq__(self, other : Any) -> bool:
+        return self.km == other
+
+    def __le__(self, other : Any) -> bool:
+        return self < other or self == other
+
+    def __ge__(self, other : Any) -> bool:
+        return self > other or self == other
