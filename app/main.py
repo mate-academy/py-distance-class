02b787533d34c2c75571d2d73fa152c54c03350c
Claymore-AI <dimalishchuk2003@gmail.com>
2025-02-19 21:05:43 +0200

    solution_1

--- app/main.py
+++ app/main.py
@@ -13,7 +13,6 @@ class Distance:
             return Distance(self.km + other.km)
         if isinstance(other, (float, int)):
             return Distance(self.km + other)
-        return NotImplemented
 
     def __iadd__(self, other: object) -> "Distance":
         if isinstance(other, Distance):
@@ -22,51 +21,43 @@ class Distance:
         if isinstance(other, (float, int)):
             self.km += other
             return self
-        return NotImplemented
 
     def __mul__(self, other: object) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        return NotImplemented
 
     def __truediv__(self, other: object) -> "Distance":
         if isinstance(other, (int, float)):
             if other == 0:
                 raise ValueError("You can't divide by zero!")
             return Distance(round((self.km / other), 2))
-        return NotImplemented
 
     def __lt__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         if isinstance(other, (float, int)):
             return self.km < other
-        return NotImplemented
 
     def __gt__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         if isinstance(other, (float, int)):
             return self.km > other
-        return NotImplemented
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         if isinstance(other, (float, int)):
             return self.km == other
-        return NotImplemented
 
     def __le__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         if isinstance(other, (float, int)):
             return self.km <= other
-        return NotImplemented
 
     def __ge__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         if isinstance(other, (float, int)):
             return self.km >= other
-        return NotImplemented
