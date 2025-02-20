c7f6b88242e80524e77f385ae58a18ed78d24a6f
Roksolana Kalinska <woops359@gmail.com>
2025-02-20 22:15:29 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
+
 class Distance:
 
-    def __init__(self, km: int | float) -> None:
+    def __init__(self, km: "Distance" | int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -9,10 +12,10 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float) -> "Distance":
+    def __add__(self, other: "Distance" | int | float) -> "Distance":
         return Distance(self.km + other)
 
-    def __radd__(self, other: int | float) -> "Distance":
+    def __radd__(self, other: "Distance" | int | float) -> "Distance":
         return Distance(self.km + other)
 
     def __iadd__(self, other: int | float) -> "Distance":
