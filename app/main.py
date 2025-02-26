c222ed5832df5d75bb4b5d6610bbb89664e1c76f
Vladyslav Pihol <freepgvlad@gmail.com>
2025-02-26 11:13:55 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Distance:
-    def __init__(self, km: float) -> float:
+    def __init__(self, km: float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -8,7 +8,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int) -> float:
+    def __add__(self, other: int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -16,7 +16,7 @@ class Distance:
         raise TypeError(f"Unsupported operand type(s)"
                         f" for +: \"Distance\" and \"{type(other).__name__}\"")
 
-    def __iadd__(self, other: int) -> float:
+    def __iadd__(self, other: int) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -27,14 +27,14 @@ class Distance:
                             f"and \"{type(other).__name__}\"")
         return self
 
-    def __mul__(self, other: int) -> float:
+    def __mul__(self, other: int) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         raise TypeError(f"Unsupported operand type(s)"
                         f" for *: \"Distance\" "
                         f"and \"{type(other).__name__}\"")
 
-    def __truediv__(self, other: int) -> float:
+    def __truediv__(self, other: int) -> Distance:
         if isinstance(other, (int, float)):
             if other == 0:
                 raise ValueError("Division by zero is not allowed!")
