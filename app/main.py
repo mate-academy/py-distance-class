03adfc91b7a345ec8407e82e7f63078f90e16a15
Vadym <strauuch@gmail.com>
2025-03-03 16:03:08 +0100

    Sol2

--- app/main.py
+++ app/main.py
@@ -11,50 +11,71 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
         else:
-            raise ValueError(f"We can`t add {other}")
+            raise ValueError(f"We can't add {other}")
 
-    def __iadd__(self, other: Distance) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
             self.km += other
         else:
-            raise ValueError(f"We can`t add {other}")
+            raise ValueError(f"We can't add {other}")
         return self
 
-    def __mul__(self, other: Distance) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(self.km * other.km)
-        elif isinstance(other, (int, float)):
+    def __mul__(self, other: int | float) -> Distance:
+        if isinstance(other, (int, float)):
             return Distance(self.km * other)
         else:
-            raise ValueError(f"We can`t mul {other}")
+            raise TypeError("Multiplication is only supported with numbers (int or float)")
 
-    def __truediv__(self, other: Distance) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(self.km / other.km)
-        elif isinstance(other, (int, float)):
-            return Distance(self.km / other)
+    def __truediv__(self, other: int | float) -> Distance:
+        if isinstance(other, (int, float)):
+            return Distance(round(self.km / other, 2))
         else:
-            raise ValueError(f"We can`t mul {other}")
+            raise TypeError("Division is only supported with numbers (int or float)")
 
-    def __lt__(self, something: Distance) -> bool:
-        return self.km < something.km
+    def __lt__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        elif isinstance(other, (int, float)):
+            return self.km < other
+        else:
+            raise ValueError(f"We can't compare with {other}")
 
-    def __gt__(self, other: Distance) -> bool:
-        return self.km > other.km
+    def __gt__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        elif isinstance(other, (int, float)):
+            return self.km > other
+        else:
+            raise ValueError(f"We can't compare with {other}")
 
-    def __eq__(self, other: Distance) -> bool:
-        return self.km == other.km
+    def __eq__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        elif isinstance(other, (int, float)):
+            return self.km == other
+        else:
+            raise ValueError(f"We can't compare with {other}")
 
-    def __le__(self, other: Distance) -> bool:
-        return self.km <= other.km
+    def __le__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        elif isinstance(other, (int, float)):
+            return self.km <= other
+        else:
+            raise ValueError(f"We can't compare with {other}")
 
-    def __ge__(self, other: Distance) -> bool:
-        return self.km >= other.km
+    def __ge__(self, other: Distance | int | float) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        elif isinstance(other, (int, float)):
+            return self.km >= other
+        else:
+            raise ValueError(f"We can't compare with {other}")
