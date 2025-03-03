b37535b9b00cac3a7f085dc11ce4c3bf074ecc18
Vadym <strauuch@gmail.com>
2025-03-03 14:51:08 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,60 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        else:
+            raise ValueError(f"We can`t add {other}")
+
+    def __iadd__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        elif isinstance(other, (int, float)):
+            self.km += other
+        else:
+            raise ValueError(f"We can`t add {other}")
+        return self
+
+    def __mul__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km * other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km * other)
+        else:
+            raise ValueError(f"We can`t mul {other}")
+
+    def __truediv__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km / other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km / other)
+        else:
+            raise ValueError(f"We can`t mul {other}")
+
+    def __lt__(self, something: Distance) -> bool:
+        return self.km < something.km
+
+    def __gt__(self, other: Distance) -> bool:
+        return self.km > other.km
+
+    def __eq__(self, other: Distance) -> bool:
+        return self.km == other.km
+
+    def __le__(self, other: Distance) -> bool:
+        return self.km <= other.km
+
+    def __ge__(self, other: Distance) -> bool:
+        return self.km >= other.km
