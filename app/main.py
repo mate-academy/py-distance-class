37d95936d0579d74cdd98e2a194f4556499deb63
Vlad <7nomer7@gmail.com>
2025-03-16 18:40:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,14 +8,14 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
         return NotImplemented
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -32,29 +32,29 @@ class Distance:
             return Distance(round(self.km / other, 2))
         return NotImplemented
 
-    def __lt__(self, other: (int, float)) -> bool:
+    def __lt__(self, other: "Distance | int | float") -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
         return NotImplemented
 
-    def __gt__(self, other: (int, float)) -> bool:
+    def __gt__(self, other: "Distance | int | float") -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
         return NotImplemented
 
-    def __eq__(self, other: (int, float)) -> bool:
+    def __eq__(self, other: "Distance | int | float") -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
         return NotImplemented
 
-    def __le__(self, other: (int, float)) -> bool:
+    def __le__(self, other: "Distance | int | float") -> bool:
         return self < other or self == other
 
-    def __ge__(self, other: (int, float)) -> bool:
+    def __ge__(self, other: "Distance | int | float") -> bool:
         return self > other or self == other
