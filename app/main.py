6cfce1e0bb9dbdc22956a774f1faff37e6ebdee8
Emil <emmileg@gmail.com>
2025-03-20 22:49:58 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
+from __future__ import annotations
 from functools import total_ordering
 
 
 @total_ordering
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,30 +13,30 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: int) -> "Distance":
+    def __iadd__(self, other: int) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
         self.km += other
         return self
 
-    def __mul__(self, other: int) -> "Distance":
+    def __mul__(self, other: int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float | int) -> "Distance":
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
-    def __eq__(self, other: "Distance") -> bool:
+    def __eq__(self, other: Distance) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         return self.km == other
 
-    def __lt__(self, other: "Distance") -> bool:
+    def __lt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         return self.km < other
