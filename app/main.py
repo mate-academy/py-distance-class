c46249f5f3198b54c50a8308f5b18fb8366ff217
Elisiane Quadros <elisiane.quadros@hotmail.com>
2025-03-25 19:18:12 -0300

    Criar classe Distance com suporte a operações matemáticas e comparações

--- app/main.py
+++ app/main.py
@@ -1,3 +1,42 @@
+from __future__ import annotations
+from typing import Union
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Union[Distance, float]) -> Distance:
+        return Distance(
+            self.km + (other.km if isinstance(other, Distance) else other))
+
+    def __iadd__(self, other: Union[Distance, float]) -> Distance:
+        self.km += other.km if isinstance(other, Distance) else other
+        return self
+
+    def __mul__(self, factor: float) -> Distance:
+        return Distance(self.km * factor)
+
+    def __truediv__(self, divisor: float) -> Distance:
+        return Distance(round(self.km / divisor, 2))
+
+    def __lt__(self, other: Union[Distance, float]) -> bool:
+        return self.km < (other.km if isinstance(other, Distance) else other)
+
+    def __gt__(self, other: Union[Distance, float]) -> bool:
+        return self.km > (other.km if isinstance(other, Distance) else other)
+
+    def __eq__(self, other: Union[Distance, float]) -> bool:
+        return self.km == (other.km if isinstance(other, Distance) else other)
+
+    def __le__(self, other: Union[Distance, float]) -> bool:
+        return self.km <= (other.km if isinstance(other, Distance) else other)
+
+    def __ge__(self, other: Union[Distance, float]) -> bool:
+        return self.km >= (other.km if isinstance(other, Distance) else other)
