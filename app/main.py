646bec72f92fece1d5b10bf3ef262577e8017f22
rootDebian <askryvenkov@gmail.com>
2025-03-26 20:01:13 +0200

    Fix mentor's review comments#2

--- app/main.py
+++ app/main.py
@@ -11,14 +11,14 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int | float) -> Distance:
+    def __add__(self, other: int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
         return NotImplemented
 
-    def __iadd__(self, other: Distance | int | float) -> Distance:
+    def __iadd__(self, other: int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
