75f0239afa5c81163d9e00773f1e3cb77e91b338
Rafal_Piatek <raf2103@wp.pl>
2025-03-27 08:46:55 +0100

    Corected Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
+
 class Distance:
 
-    def __init__(self, km: (int, float)) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -9,7 +12,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: ("Distance", int, float)) -> "Distance":
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
@@ -20,7 +23,7 @@ class Distance:
                 f"'Distance' and '{type(other)}'"
             )
 
-    def __iadd__(self, other: ("Distance", int, float)) -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
@@ -33,7 +36,7 @@ class Distance:
                 f"'Distance' and '{type(other)}'"
             )
 
-    def __mul__(self, other: (int, float)) -> "Distance":
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         else:
@@ -42,7 +45,7 @@ class Distance:
                 f"'Distance' and '{type(other)}'"
             )
 
-    def __truediv__(self, other: (int, float)) -> "Distance":
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             if other != 0:
                 return Distance(round(self.km / other, 2))
@@ -54,7 +57,7 @@ class Distance:
                 f"'Distance' and '{type(other)}'"
             )
 
-    def __lt__(self, other: ("Distance", int, float)) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
@@ -65,7 +68,7 @@ class Distance:
                 f"'Distance' and '{type(other)}'"
             )
 
-    def __gt__(self, other: ("Distance", int, float)) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
@@ -76,18 +79,18 @@ class Distance:
                 f"'Distance' and '{type(other)}'"
             )
 
-    def __eq__(self, other: ("Distance", int, float)) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
         else:
             raise TypeError(
-                f"unsupported operand type(s) for =: "
+                f"unsupported operand type(s) for ==: "
                 f"'Distance' and '{type(other)}'"
             )
 
-    def __le__(self, other: ("Distance", int, float)) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
@@ -98,7 +101,7 @@ class Distance:
                 f"'Distance' and '{type(other)}'"
             )
 
-    def __ge__(self, other: ("Distance", int, float)) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
