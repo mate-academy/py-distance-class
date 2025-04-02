6af833b52be80ff6f2165b22b7de02bf7dd1507a
Willian Nomiyama <willian001@gmail.com>
2025-04-02 15:51:22 -0300

    Corrections

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+from typing import Union
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -8,16 +12,15 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return Distance(self.km + other)
-        else:
-            raise TypeError(f"Unsupported operand type(s) for +: "
-                            f"'Distance' and '"f"{type(other).__name__}'")
+        raise TypeError(f"Unsupported operand type(s) for +: "
+                        f"'Distance' and '{type(other).__name__}'")
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -27,61 +30,54 @@ class Distance:
                             f"'Distance' and '{type(other).__name__}'")
         return self
 
-    def __mul__(self, other: "Distance") -> "Distance":
-        if isinstance(other, (int, float)):
-            return Distance(self.km * other)
-        else:
+    def __mul__(self, other: Union[int, float]) -> Distance:
+        if not isinstance(other, (int, float)):
             raise TypeError(f"Unsupported operand type(s) for *: "
                             f"'Distance' and '{type(other).__name__}'")
+        return Distance(self.km * other)
 
-    def __truediv__(self, other: "Distance") -> "Distance":
-        if isinstance(other, (int, float)):
-            return Distance(round(self.km / other, 2))
-        else:
+    def __truediv__(self, other: Union[int, float]) -> Distance:
+        if not isinstance(other, (int, float)):
             raise TypeError(f"Unsupported operand type(s) for /: "
                             f"'Distance' and '{type(other).__name__}'")
+        return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: "Distance") -> bool:
+    def __lt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km < other
-        else:
-            raise TypeError(f"Unsupported operand type(s) for <: "
-                            f"'Distance' and '{type(other).__name__}'")
+        raise TypeError(f"Unsupported operand type(s) for <: "
+                        f"'Distance' and '{type(other).__name__}'")
 
-    def __gt__(self, other: "Distance") -> bool:
+    def __gt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km > other
-        else:
-            raise TypeError(f"Unsupported operand type(s) for >: "
-                            f"'Distance' and '{type(other).__name__}'")
+        raise TypeError(f"Unsupported operand type(s) for >: "
+                        f"'Distance' and '{type(other).__name__}'")
 
-    def __eq__(self, other: "Distance") -> bool:
+    def __eq__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km == other
-        else:
-            raise TypeError(f"Unsupported operand type(s) for ==: "
-                            f"'Distance' and '{type(other).__name__}'")
+        raise TypeError(f"Unsupported operand type(s) for ==: "
+                        f"'Distance' and '{type(other).__name__}'")
 
-    def __le__(self, other: "Distance") -> bool:
+    def __le__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km <= other
-        else:
-            raise TypeError(f"Unsupported operand type(s) for <=: "
-                            f"'Distance' and '{type(other).__name__}'")
+        raise TypeError(f"Unsupported operand type(s) for <=: "
+                        f"'Distance' and '{type(other).__name__}'")
 
-    def __ge__(self, other: "Distance") -> bool:
+    def __ge__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km >= other
-        else:
-            raise TypeError(f"Unsupported operand type(s) for >=: "
-                            f"'Distance' and '{type(other).__name__}'")
+        raise TypeError(f"Unsupported operand type(s) for >=: "
+                        f"'Distance' and '{type(other).__name__}'")
