11eae0f15b52dd76f8272fcef59f212dc2dfd619
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-04-05 19:35:18 +0200

    Solution fix annotation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from typing import Any
+from typing import Union, Optional, TypeVar
+
+Tp = TypeVar("Tp", bound="Distance")
 
 
 class Distance:
@@ -11,80 +13,71 @@ class Distance:
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __add__(self, other: Any) -> "Distance":
+    def __add__(self, other: Union[Tp, int, float]) -> Tp:
         if isinstance(other, Distance):
             new_ds = Distance(self.km + other.km)
             return new_ds
         elif isinstance(other, (int, float)):
             new_ds = Distance(self.km + other)
             return new_ds
-        else:
-            raise ValueError(f"Unsupported type {other} - {type(other)} ")
+        raise ValueError(f"Unsupported type {other} - {type(other)} ")
 
-    def __iadd__(self, other: Any) -> "Distance":
+    def __iadd__(self, other: Union[Tp, int, float]) -> Tp:
         if isinstance(other, Distance):
             self.km += other.km
             return self
         elif isinstance(other, (int, float)):
             self.km += other
             return self
-        else:
-            raise ValueError(f"Unsupported type {other} - {type(other)} ")
+        raise ValueError(f"Unsupported type {other} - {type(other)} ")
 
-    def __mul__(self, other: Any) -> Any:
+    def __mul__(self, other: Union[Tp, int, float]) -> Optional[Tp]:
         if isinstance(other, Distance):
             raise TypeError("Cannot divide two Distance objects")
         elif isinstance(other, (int, float)):
             new_ds = Distance(self.km * other)
             return new_ds
-        else:
-            raise ValueError(f"Unsupported type {other} - {type(other)} ")
+        raise ValueError(f"Unsupported type {other} - {type(other)} ")
 
-    def __truediv__(self, other: Any) -> Any:
+    def __truediv__(self, other: Union[Tp, int, float]) -> Optional[Tp]:
         if isinstance(other, Distance):
             raise TypeError("Cannot divide two Distance objects")
         elif isinstance(other, (int, float)):
             new_ds = Distance(round(self.km / other, 2))
             return new_ds
-        else:
-            raise ValueError(f"Unsupported type {other} - {type(other)} ")
+        raise ValueError(f"Unsupported type {other} - {type(other)} ")
 
-    def __lt__(self, other: Any) -> bool:
+    def __lt__(self, other: Union[Tp, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
-        else:
-            raise ValueError(f"Unsupported type {other} - {type(other)} ")
+        raise ValueError(f"Unsupported type {other} - {type(other)} ")
 
-    def __gt__(self, other: Any) -> bool:
+    def __gt__(self, other: Union[Tp, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
-        else:
-            raise ValueError(f"Unsupported type {other} - {type(other)} ")
+        raise ValueError(f"Unsupported type {other} - {type(other)} ")
 
-    def __eq__(self, other: Any) -> bool:
+    def __eq__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
-        else:
-            raise False
+        raise False
 
-    def __le__(self, other: Any) -> bool:
+    def __le__(self, other: Union[Tp, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
-        else:
-            raise ValueError(f"Unsupported type {other} - {type(other)} ")
+        raise ValueError(f"Unsupported type {other} - {type(other)} ")
 
-    def __ge__(self, other: Any) -> bool:
+    def __ge__(self, other: Union[Tp, int, float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
             return self.km >= other
-        else:
-            raise ValueError(f"Unsupported type {other} - {type(other)} ")
+        raise ValueError(f"Unsupported type {other} - {type(other)} ")
