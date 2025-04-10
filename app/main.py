6dc2f3f1e44119d2b2ffe4c53a1ee91be4bb7032
Vitalik <lg.mobile0808@gmail.com>
2025-04-10 18:37:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,57 @@
+import math
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Disnatce(km={self.km})"
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return NotImplemented
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km += other.km
+        return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return Distance(self.km * other)
+        return NotImplemented
+
+    def __truediv__(self, other):
+        if isinstance(other, int):
+            unrounded = self.km / other
+            rounded = math.floor(unrounded)
+            return Distance(rounded)
+        return NotImplemented
+
+    def __lt__(self,other):
+        if isinstance(other, int):
+            return self.km < other
+
+    def __gt__(self, other):
+        if isinstance(other, int):
+            return self.km > other
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.km == other
+
+    def __le__(self, other):
+        if isinstance(other, int):
+            return self.km <= other
+
+    def __ge__(self, other):
+        if isinstance(other, int):
+            return self.km >= other
+
+
+
