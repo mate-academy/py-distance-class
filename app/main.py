d93d9965e99c969ab901b5b52a96ff75d43b6e17
Bohdan Salamakha <allen.avanheim@gmail.com>
2025-04-13 17:43:16 +0300

    fix1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+from __future__ import annotations
 from typing import Union
 
 
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -11,27 +12,23 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union["Distance", int]) -> "Distance":
+    def __add__(self, other: Union[Distance, int]) -> "Distance":
+        if type(other) is Distance:
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __iadd__(self, other: Union[Distance, int]) -> "Distance":
         if type(other) is Distance:
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __iadd__(self, other: Union["Distance", int]) -> "Distance":
-        if type(other) is Distance:
-            self.km += other.km
-        else:
-            self.km += other
-        return self
+    def __mul__(self, other: int) -> Distance:
+        return Distance(self.km * other)
 
-    def __mul__(self, other: int) -> "Distance":
-        self.km *= other
-        return self
-
-    def __truediv__(self, other: int) -> "Distance":
-        self.km = round(self.km / other, 2)
-        return self
+    def __truediv__(self, other: int) -> Distance:
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int) -> bool:
         return self.km < other
