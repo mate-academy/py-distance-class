a5455d63f46638026f54ea5da861c690391f75c2
Dmytro <dragusandmitrij3@gmail.com>
2025-04-16 20:44:16 +0200

    Refactored main.py

--- app/main.py
+++ app/main.py
@@ -34,46 +34,39 @@ class Distance:
     def __mul__(self, other: Union[float, int]) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        raise TypeError("Unsupported type for multiplication")
 
     def __truediv__(self, other: Union[float, int]) -> "Distance":
         if isinstance(other, (int, float)):
             if other == 0:
                 raise ZeroDivisionError("Division by zero is not allowed")
             return Distance(round(self.km / other, 2))
-        raise TypeError("Unsupported type for division")
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         if isinstance(other, (int, float)):
             return self.km == other
-        return False
 
     def __gt__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         if isinstance(other, (int, float)):
             return self.km > other
-        raise TypeError("Unsupported type for comparison")
 
     def __ge__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         if isinstance(other, (int, float)):
             return self.km >= other
-        raise TypeError("Unsupported type for comparison")
 
     def __lt__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         if isinstance(other, (int, float)):
             return self.km < other
-        raise TypeError("Unsupported type for comparison")
 
     def __le__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         if isinstance(other, (int, float)):
             return self.km <= other
-        raise TypeError("Unsupported type for comparison")
