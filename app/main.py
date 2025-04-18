35d1ba72b5da899f782e1817b8f21a499e41eef5
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-04-18 17:59:41 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,40 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+    
+    def __add__(self, outro_km: int) -> int:
+        return self.km + self.outro_km
+
+    def __iadd__(self, outro_km: int) -> int:
+        self.km += self.outro_km
+        return self.km
+
+    def __mul__(self, outro_km: int) -> int:
+        return self.km * self.outro_km
+
+    def __truediv__(self, outro_km: int) -> int
+        return self.km / self.outro_km
+
+    def __lt__(self, outro_km: int) -> bool
+        return self.km < self.outro_km
+
+    def __eq__(self, outro_km: int) -> bool
+        if isinstance(outro_km, Distance):
+            if self.km == outro.km:
+                return True
+        return False
+
+    def __gt__(self, outro_km: int) -> bool
+        return self.km > self.outro_km
+
+    def __le__(self, outro_k: int) -> bool
+        return self.km <= self.outro_km
+
+    def __ge__(self, outro_km: int) -> bool
+        return self.km >= self.outro_km
