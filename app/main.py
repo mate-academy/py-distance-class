a8001162203a99d3e05156cfe8cfc333a9373520
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-04-21 12:17:53 -0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from numbers import Number
+from typing import Any
 
 
 error_msg = "Type mismatch: requires Distance or Number and received"
@@ -14,15 +15,15 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: any) -> any:
+    def __add__(self, other: Any) -> Any:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, Number):
             return Distance(self.km + other)
 
-        raise TypeError(error_msg + type(other))
+        raise TypeError(error_msg + str(type(other)))
 
-    def __iadd__(self, other: any) -> any:
+    def __iadd__(self, other: Any) -> Any:
         if isinstance(other, Distance):
             self.km += other.km
             return self
@@ -30,58 +31,58 @@ class Distance:
             self.km += other
             return self
 
-        raise TypeError(error_msg + type(other))
+        raise TypeError(error_msg + str(type(other)))
 
-    def __mul__(self, number: Number) -> any:
+    def __mul__(self, number: Number) -> Any:
         if not isinstance(number, Number):
 
-            raise TypeError(error_msg + type(number))
+            raise TypeError(error_msg + str(type(number)))
 
         return Distance(self.km * number)
 
-    def __truediv__(self, number: Number) -> any:
+    def __truediv__(self, number: Number) -> Any:
         if not isinstance(number, Number):
 
-            raise TypeError(error_msg + type(number))
+            raise TypeError(error_msg + str(type(number)))
 
         return Distance(round(self.km / number, 2))
 
-    def __lt__(self, other: any) -> bool:
+    def __lt__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, Number):
             return self.km < other
 
-        raise TypeError(error_msg + type(other))
+        raise TypeError(error_msg + str(type(other)))
 
-    def __gt__(self, other: any) -> bool:
+    def __gt__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, Number):
             return self.km > other
 
-        raise TypeError(error_msg + type(other))
+        raise TypeError(error_msg + str(type(other)))
 
-    def __eq__(self, other: any) -> bool:
+    def __eq__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, Number):
             return self.km == other
 
-        raise TypeError(error_msg + type(other))
+        raise TypeError(error_msg + str(type(other)))
 
-    def __le__(self, other: any) -> bool:
+    def __le__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, Number):
             return self.km <= other
 
-        raise TypeError(error_msg + type(other))
+        raise TypeError(error_msg + str(type(other)))
 
-    def __ge__(self, other: any) -> bool:
+    def __ge__(self, other: Any) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, Number):
             return self.km >= other
 
-        raise TypeError(error_msg + type(other))
+        raise TypeError(error_msg + str(type(other)))
