18afe60196ceb7d9de2fba0683df5c75b72634f0
Nadiia Bohdan <n.bogdan202@gmail.com>
2025-04-25 19:27:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,57 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other) -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __iadd__(self, other) -> "Distance":
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other) -> "Distance":
+        return Distance(self.km * other)
+
+    def __truediv__(self, other) -> "Distance":
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other) -> bool:
+        return self.km < (
+            other.km if isinstance(other, Distance)
+            else other
+        )
+
+    def __gt__(self, other) -> bool:
+        return self.km > (
+            other.km if isinstance(other, Distance)
+            else other
+        )
+
+    def __eq__(self, other) -> bool:
+        return self.km == (
+            other.km if isinstance(other, Distance)
+            else other
+        )
+
+    def __le__(self, other) -> bool:
+        return self.km <= (
+            other.km if isinstance(other, Distance)
+            else other
+        )
+
+    def __ge__(self, other) -> bool:
+        return self.km >= (
+            other.km if isinstance(other, Distance)
+            else other
+        )
