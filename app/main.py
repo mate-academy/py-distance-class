5c4ee94385546dc83afc6d073490c82d839331d4
Nadiia Bohdan <n.bogdan202@gmail.com>
2025-04-25 19:31:19 +0300

    Some type annotation fixes

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Distance:
     def __init__(self, km) -> None:
         self.km = km
@@ -8,49 +11,49 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other) -> "Distance":
+    def __add__(self, other: Any) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other) -> "Distance":
+    def __iadd__(self, other: Any) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other) -> "Distance":
+    def __mul__(self, other: Any) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other) -> "Distance":
+    def __truediv__(self, other: Any) -> "Distance":
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: Any) -> bool:
         return self.km < (
             other.km if isinstance(other, Distance)
             else other
         )
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: Any) -> bool:
         return self.km > (
             other.km if isinstance(other, Distance)
             else other
         )
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: Any) -> bool:
         return self.km == (
             other.km if isinstance(other, Distance)
             else other
         )
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: Any) -> bool:
         return self.km <= (
             other.km if isinstance(other, Distance)
             else other
         )
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: Any) -> bool:
         return self.km >= (
             other.km if isinstance(other, Distance)
             else other
