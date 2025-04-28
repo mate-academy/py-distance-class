2795f17ce1db9606d4341c9aeedc7810887be463
Antonov Andrew <prestigemilitary@gmail.com>
2025-04-28 18:48:35 +0300

    Need help2)

--- app/main.py
+++ app/main.py
@@ -1,36 +1,37 @@
 class Distance:
-    def __init__(self, km):
+    def __init__(self, km: int) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> None:
         print(f"Distance: {self.km} kilometers.")
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: Distance) -> int:
         return Distance(self.km + other.km)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Distance) -> None:
         self.km += other.km
 
-    def __mul__(self, other):
+    def __mul__(self, other: Distance) -> float:
         return Distance(self.km * other.km)
 
-    def __truediv__(self, other):
-        return Distance(self.km / other.km)
+    def __truediv__(self, other: Distance) -> float:
+        if isinstance(other, Distance):
+            return Distance(self.km / other.km)
 
-    def __lt__(self, other):
+    def __lt__(self, other: Distance) -> bool:
         return self.km < other.km
 
-    def __gt__(self, other):
+    def __gt__(self, other: Distance) -> bool:
         return self.km > other.km
 
-    def __eq__(self, other):
+    def __eq__(self, other: Distance) -> bool:
         return self.km == other.km
 
-    def __le__(self, other):
+    def __le__(self, other: Distance) -> bool:
         return self.km <= other.km
 
-    def __ge__(self, other):
+    def __ge__(self, other: Distance) -> bool:
         return self.km >= other.km
