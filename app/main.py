f12b6ff93ea55a7afe4a8fdc28f6b17fe6b0a8d7
Denis <godlike.kipish@gmail.com>
2025-05-11 20:13:08 +0300

    It would be YES))

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
+from typing import Union
 
 
 class Distance:
-    def __init__(self, km: [int, float]) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -11,7 +12,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: [Distance, int, float]) -> Distance:
+    def __add__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(
                 km=self.km + other
@@ -22,7 +23,7 @@ class Distance:
             km=self.km + other.km
         )
 
-    def __iadd__(self, other: [Distance, int, float]) -> Distance:
+    def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
         if isinstance(other, (int, float)):
             self.km = self.km + other
             return self
@@ -31,49 +32,49 @@ class Distance:
         self.km = self.km + other.km
         return self
 
-    def __mul__(self, other: [int, float]) -> Distance:
+    def __mul__(self, other: Union[int, float]) -> Distance:
         if not isinstance(other, (int, float)):
             raise TypeError
         return Distance(
             km=self.km * other
         )
 
-    def __truediv__(self, other: [int, float]) -> Distance:
+    def __truediv__(self, other: Union[int, float]) -> Distance:
         if not isinstance(other, (int, float)):
             raise TypeError
         return Distance(
             km=round(self.km / other, 2)
         )
 
-    def __lt__(self, other: [Distance, int, float]) -> bool:
+    def __lt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km < other
         elif not isinstance(other, (Distance, int, float)):
             raise TypeError
         return self.km < other.km
 
-    def __gt__(self, other: [Distance, int, float]) -> bool:
+    def __gt__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km > other
         elif not isinstance(other, (Distance, int, float)):
             raise TypeError
         return self.km > other.km
 
-    def __eq__(self, other: [Distance, int, float]) -> bool:
+    def __eq__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km == other
         elif not isinstance(other, (Distance, int, float)):
             raise TypeError
         return self.km == other.km
 
-    def __le__(self, other: [Distance, int, float]) -> bool:
+    def __le__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km <= other
         elif not isinstance(other, (Distance, int, float)):
             raise TypeError
         return self.km <= other.km
 
-    def __ge__(self, other: [Distance, int, float]) -> bool:
+    def __ge__(self, other: Union[Distance, int, float]) -> bool:
         if isinstance(other, (int, float)):
             return self.km >= other
         elif not isinstance(other, (Distance, int, float)):
