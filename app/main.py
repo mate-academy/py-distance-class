85383e55b627c93e3d5c0a558ce6a7f8150ca594
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-05-17 19:01:04 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,56 @@
+from decimal import Decimal
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: int)-> "Distance" or float:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __iadd__(self, other: int or "Distance") -> "Distance" or float:
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        self.km += other
+        return self
+
+    def __mul__(self, other: int ) -> "Distance":
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int or "Distance" or float) -> "Distance" or float:
+        num = round(Decimal(str(self.km / other)))
+        return Distance(float(num))
+
+    def __lt__(self, other: "Distance") -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        return self.km < other
+
+    def __gt__(self, other: "Distance" or int) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
+
+    def __eq__(self, other: "Distance") -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        return self.km == other
+
+    def __le__(self, other: "Distance" or int) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        return self.km <= other
+
+    def __ge__(self, other: "Distance" or int) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return self.km >= other
