5d2aff3578bf2745442f32e02c9c404bf4b6aa65
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-05-20 16:48:49 +0700

    Seemingly ready

--- app/main.py
+++ app/main.py
@@ -1,3 +1,65 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km):
+        self.km = km
+
+    def __add__(self, other):
+        if isinstance(other, Distance):
+            return  self.km + other.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __iadd__(self, other):
+        if isinstance(other, Distance):
+            self.km = self.km + other.km
+            return self.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __mul__(self, other):
+        if isinstance(other, Distance):S
+            return self.km * other.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __truediv__(self, other):
+        if isinstance(other, Distance):
+            return self.km / other.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __lt__(self, other):
+        if isinstance(other, Distance):
+            return self.km < other.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __gt__(self, other):
+        if isinstance(other, Distance):
+            return self.km > other.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __eq__(self, other):
+        if isinstance(other, Distance):
+            return self.km == other.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __le__(self, other):
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __ge__(self, other):
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        else:
+            raise TypeError(f"{other} is not an instance of {Distance}")
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(km={self.km})"
