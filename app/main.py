4c751a257af2f0a0d99896c67e77b232c52d4b2c
Lukian <lukian55d@gmail.com>
2025-05-24 01:25:22 +0100

    done task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,40 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance) -> Distance:
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other: Distance) -> Distance:
+        self.km += other.km
+        return self
+
+    def __mul__(self, other: Distance) -> Distance:
+        return Distance(self.km * other.km)
+
+    def __truediv__(self, other: Distance) -> float:
+        return self.km / other.km
+
+    def __lt__(self, other: Distance) -> bool:
+        return self.km < other.km
+
+    def __gt__(self, other: Distance) -> bool:
+        return self.km > other.km
+
+    def __eq__(self, other: Distance) -> bool:
+        return self.km == other.km
+
+    def __le__(self, other: Distance) -> bool:
+        return self.km <= other.km
+
+    def __ge__(self, other: Distance) -> bool:
+        return self.km >= other.km
