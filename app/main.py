6c22fb50e770c3f47863176de20d3d4207104eb6
Thalles Gabriel <thallesgabrieldasilvacosta@gmail.com>
2025-05-30 13:41:01 -0300

    Agora sim

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Distance:
-    def __init__(self, km: float):
-        self.km = km
+    def __init__(self, km: float) -> None:
+        self.km: float = km
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers"
@@ -8,22 +8,22 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other) -> 'Distance':
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + float(other))
 
-    def __iadd__(self, other):
+    def __iadd__(self, other) -> 'Distance':
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += float(other)
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> 'Distance':
         return Distance(self.km * float(other))
 
-    def __truediv__(self, other):
+    def __truediv__(self, other) -> 'Distance':
         return Distance(round(self.km / float(other), 2))
 
     def __lt__(self, other) -> bool:
