3f01e6d3d4f823a0371af4c8b78ab1c27948dce2
Serhii Markusyk <serega.markusik@gmail.com>
2025-06-09 11:34:43 +0200

    distance-class3

--- app/main.py
+++ app/main.py
@@ -11,22 +11,30 @@ class Distance:
     def __add__(self, other: "Distance") -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
-        return Distance(self.km + other.km)
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        raise TypeError("Unsupported type for addition with Distance")
 
     def __iadd__(self, other: "Distance") -> "Distance":
         if isinstance(other, (int, float)):
             self.km += other
         elif isinstance(other, Distance):
             self.km += other.km
+        else:
+            raise TypeError("Unsupported type for in-place addition with Distance")
         return self
 
     def __mul__(self, other: "Distance") -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
+        raise TypeError("Unsupported type for multiplication with Distance")
 
 
     def __truediv__(self, other) -> "Distance":
-        return Distance(round(self.km / other, 2))
+        if isinstance(other, (int, float)):
+            return Distance(round(self.km / other, 2))
+        raise TypeError("Unsupported type for division with Distance")
+
 
     def __lt__(self, other: "Distance"):
         return self.km < (other.km if isinstance(other, Distance) else other)
