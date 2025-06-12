87f2b3417d10e6b3590a39826c672bc27a1f0558
Olivier Mbayi <olimbayi@gmail.com>
2025-06-12 20:55:15 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,18 +3,18 @@ class Distance:
     def __init__(self, distance):
         self.distance = distance
 
-    def __str__(self, distance):
-        return f'("Distance: {distance} kilometers.")'
+    def __str__(self):
+        return f'("Distance: {self.distance} kilometers.")'
 
-    def __repr__(self, distance):
-        return f"Distance(km={distance})"
+    def __repr__(self):
+        return f"Distance(km={self.distance})"
     
     def __add__(self, other):
         return Distance(self.distance + other.distance)
 
     def __iadd__(self, other):
         self.distance = self.distance + other.distance
-        return Distance(self.distance)
+        return self.distance
     
     def __mul__(self, other):
         if isinstance(other, Distance):
@@ -23,29 +23,28 @@ class Distance:
     def __truediv__(self, other):
         if isinstance(other, Distance):
             if other.distance == 0:
-                raise
-                ZeroDivisionError("Division by zero")
+                raise ZeroDivisionError("Division by zero")
             return Distance(self.distance / other.distance) 
 
     def __lt__(self, other):
         if isinstance(other, Distance):
-            return Distance(self.distance < other.distance)
+            return self.distance < other.distance
 
     
     def __gt__(self, other):
         if isinstance(other, Distance):
-            return Distance(self.distance > other.distance)
+            return self.distance > other.distance
     
     def __eq__(self, other):
         if isinstance(other, Distance):
-            return Distance(self.distance == other.distance)
+            return self.distance == other.distance
         return False
     
     def __le__(self, other):
         if isinstance(other, Distance):
-            return Distance(self.distance <= other.distance)
+            return self.distance <= other.distance
     
     def __ge__(self, other):
         if isinstance(other, Distance):
-            return Distance(self.distance >= other.distance )
-        return "YES"
+            return self.distance >= other.distance
+        
