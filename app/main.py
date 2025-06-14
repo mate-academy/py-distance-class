ad00a1495f8f5e0602e898dd235b09e5b7e4e516
PavloReutskiy <87998640+PavloReutskiy@users.noreply.github.com>
2025-06-14 11:00:49 +0300

    fix solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
-from typing import Union
 
 
 class Distance:
-    def __init__(self, km: Union[int, float]) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,7 +11,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def get_value(self, other: Union[Distance, int, float]) -> float:
+    def get_value(self, other: Distance | int | float) -> float:
         if isinstance(other, Distance):
             return other.km
         if isinstance(other, (int, float)):
@@ -21,38 +20,40 @@ class Distance:
             f"unsupported operand type(s): 'Distance' and '{type(other)}'"
         )
 
-    def __add__(self, other: Union[Distance, int, float]) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         return Distance(km=self.km + self.get_value(other))
 
-    def __iadd__(self, other: Union[Distance, int, float]) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += self.get_value(other)
         return self
 
-    def __mul__(self, other: Union[int, float]) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(km=self.km * other)
         raise TypeError(
             f"unsupported operand type(s): 'Distance' and {type(other)}"
         )
 
-    def __truediv__(self, other: Union[int, float]) -> Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
+            if other == 0:
+                raise ZeroDivisionError("Cannot divide by zero")
             return Distance(km=round(self.km / other, 2))
         raise TypeError(
             f"unsupported operand type(s) for /: 'Distance' and {type(other)}"
         )
 
-    def __lt__(self, other: Union[Distance, int, float]) -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < self.get_value(other)
 
-    def __gt__(self, other: Union[Distance, int, float]) -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > self.get_value(other)
 
-    def __eq__(self, other: Union[Distance, int, float]) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == self.get_value(other)
 
-    def __le__(self, other: Union[Distance, int, float]) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= self.get_value(other)
 
-    def __ge__(self, other: Union[Distance, int, float]) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= self.get_value(other)
