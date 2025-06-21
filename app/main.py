dc0aa819539f0cc2d0cb9acfd6138b692e62910b
Hamster <dtaranenko137@gmail.com>
2025-06-21 18:07:59 +0300

    'Done'

--- app/main.py
+++ app/main.py
@@ -11,24 +11,29 @@ class Distance:
 
     def __add__(self, other: object) -> "Distance":
         if isinstance(other, Distance):
-            total_km = self.km + other.km
-        elif isinstance(other, (int, float)):
-            total_km = self.km + other
-        return Distance(total_km)
+            return Distance(self.km + other.km)
+        if isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        raise TypeError
 
     def __iadd__(self, other: object) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
             self.km += other
+        else:
+            raise TypeError
         return self
 
     def __mul__(self, other: object) -> "Distance":
+        if not isinstance(other, (int, float)):
+            raise TypeError
         return Distance(self.km * other)
 
     def __truediv__(self, other: object) -> "Distance":
-        result = round(self.km / other, 2)
-        return Distance(result)
+        if not isinstance(other, (int, float)):
+            raise TypeError
+        return Distance(round(self.km / other, 2))
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Distance):
