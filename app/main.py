cc969db4ac7f707526e1185a31db0bc27f12e397
Glego <glego200323@gmail.com>
2025-06-23 07:54:46 +0300

    Solution flake8 fix

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Distance:
-    def __init__(self, km: (int, float)):
+    def __init__(self, km: (int, float)) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -8,35 +8,35 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: "Distance") -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: "Distance") -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: "Distance") -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: "Distance") -> "Distance":
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other: "Distance") -> bool:
         return self.km < (other.km if isinstance(other, Distance) else other)
 
-    def __gt__(self, other):
+    def __gt__(self, other: "Distance") -> bool:
         return self.km > (other.km if isinstance(other, Distance) else other)
 
-    def __eq__(self, other):
+    def __eq__(self, other: "Distance") -> bool:
         return self.km == (other.km if isinstance(other, Distance) else other)
 
-    def __le__(self, other):
+    def __le__(self, other: "Distance") -> bool:
         return self.km <= (other.km if isinstance(other, Distance) else other)
 
-    def __ge__(self, other):
+    def __ge__(self, other: "Distance") -> bool:
         return self.km >= (other.km if isinstance(other, Distance) else other)
