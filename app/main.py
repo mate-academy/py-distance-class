68ad3d8a164fdba88c14383390e8226dc79f23e8
Nazar <nazardrobinsky@gmail.com>
2025-06-23 16:54:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -31,37 +31,37 @@ class Distance:
     def __truediv__(self, other: "Distance" | int | float) -> "Distance":
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: type) -> bool:
+    def __lt__(self, other: "Distance" | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
-        return None
+        return NotImplemented
 
-    def __gt__(self, other: type) -> bool:
+    def __gt__(self, other: "Distance" | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
-        return None
+        return NotImplemented
 
-    def __eq__(self, other: type) -> bool:
+    def __eq__(self, other: "Distance" | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
-        return None
+        return NotImplemented
 
-    def __le__(self, other: type) -> bool:
+    def __le__(self, other: "Distance" | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
-        return None
+        return NotImplemented
 
-    def __ge__(self, other: type) -> bool:
+    def __ge__(self, other: "Distance" | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
             return self.km >= other
-        return None
+        return NotImplemented
