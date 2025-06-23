34bd2b6f34d11ee020921c472eb064d0dfe45d6e
Nazar <nazardrobinsky@gmail.com>
2025-06-23 16:30:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -28,35 +28,35 @@ class Distance:
     def __truediv__(self, other: type) -> type:
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other: type) -> type:
+    def __lt__(self, other: type) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
         return None
 
-    def __gt__(self, other: type) -> type:
+    def __gt__(self, other: type) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
         return None
 
-    def __eq__(self, other: type) -> type:
+    def __eq__(self, other: type) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
         return None
 
-    def __le__(self, other: type) -> type:
+    def __le__(self, other: type) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
         return None
 
-    def __ge__(self, other: type) -> type:
+    def __ge__(self, other: type) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
