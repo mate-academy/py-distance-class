07a0aaa0ba49a2c0fb48b4f1186627a9d0ec51b8
Kostya Mochulskiy <mochulskiykostya@gmail.com>
2025-06-23 17:27:56 +0300

    Solution v1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,63 @@
+from typing import Union
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: float) -> None:
+        self.km = float(km)
+
+    def __str__(self) -> str:
+        if self.km == int(self.km):
+            return f"Distance: {int(self.km)} kilometers."
+        else:
+            return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        if self.km == int(self.km):
+            return f"Distance(km={int(self.km)})"
+        else:
+            return f"Distance(km={self.km})"
+        return f"Distance(km={int(self.km)})"
+
+    def __add__(self, other: Union[float, "Distance"]) -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(
+                km=self.km + other.km
+            )
+        else:
+            return Distance(
+                km=self.km + float(other)
+            )
+
+    def __iadd__(self, other: Union["Distance", float]) -> "Distance":
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += float(other)
+        return self
+
+    def __mul__(self, other: float) -> "Distance":
+        return Distance(round(self.km * other, 3))
+
+    def __truediv__(self, other: float) -> "Distance":
+        return Distance(round(self.km / float(other), 2))
+        # isinstance(distance2, Distance) is True
+    # distance2.km == 2.85
+    # Note: rounded to 2 decimals
+
+    def __lt__(self, other: Union["Distance", float]) -> bool:
+        return self.km < other
+
+    def __gt__(self, other: Union["Distance", float]) -> bool:
+        return self.km > other
+
+    def __eq__(self, other: Union["Distance", float]) -> bool:
+        return self.km == other
+
+    def __le__(self, other: Union["Distance", float]) -> bool:
+        return self.km <= other
+
+    def __ge__(self, other: Union["Distance", float]) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other
+        return self.km >= float(other)
