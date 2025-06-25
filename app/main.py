63f5d529078e6ee2a52ad020b4f947e9afd7643b
Dasha <dariia.leshchenko@gmail.com>
2025-06-25 21:22:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from typing import Union
+
+
 class Distance:
-    def __init__(self, km: (int, float)) -> None:
+    def __init__(self, km: Union[int, float]) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -8,25 +11,25 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, Distance):
             other = other.km
         return Distance(km=self.km + other)
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: Union[int, float, "Distance"]) -> "Distance":
         if isinstance(other, Distance):
             other = other.km
         self.km += other
         return self
 
-    def __mul__(self, other: (int, float)) -> "Distance":
+    def __mul__(self, other: Union[int, float]) -> "Distance":
         if not isinstance(other, (int, float)):
             raise TypeError(
                 "__mul__ only supports multiplication by int or float"
             )
         return Distance(km=self.km * other)
 
-    def __truediv__(self, other: (int, float)) -> "Distance":
+    def __truediv__(self, other: Union[int, float]) -> "Distance":
         if not isinstance(other, (int, float)):
             raise TypeError(
                 "__truediv__ only supports division by int or float"
@@ -35,27 +38,27 @@ class Distance:
             raise ZeroDivisionError("Cannot divide by zero.")
         return Distance(km=round(self.km / other, 2))
 
-    def __lt__(self, other: "Distance") -> bool:
+    def __lt__(self, other: Union[int, float, "Distance"]) -> bool:
         if isinstance(other, Distance):
             other = other.km
         return self.km < other
 
-    def __gt__(self, other: "Distance") -> bool:
+    def __gt__(self, other: Union[int, float, "Distance"]) -> bool:
         if isinstance(other, Distance):
             other = other.km
         return self.km > other
 
-    def __eq__(self, other: "Distance") -> bool:
+    def __eq__(self, other: Union[int, float, "Distance"]) -> bool:
         if isinstance(other, Distance):
             other = other.km
         return self.km == other
 
-    def __le__(self, other: "Distance") -> bool:
+    def __le__(self, other: Union[int, float, "Distance"]) -> bool:
         if isinstance(other, Distance):
             other = other.km
         return self.km <= other
 
-    def __ge__(self, other: "Distance") -> bool:
+    def __ge__(self, other: Union[int, float, "Distance"]) -> bool:
         if isinstance(other, Distance):
             other = other.km
         return self.km >= other
