8e5424a5a981029d906498f6ecb351644b801bff
Yehor Hyrzhev <yehor.hyrzhev@student.tuke.sk>
2025-06-27 00:52:27 +0200

    add code

--- app/main.py
+++ app/main.py
@@ -1,3 +1,61 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other) -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __iadd__(self, other) -> "Distance":
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+
+    def __mul__(self, other) -> "Distance":
+        self.km *= other
+        return Distance(self.km)
+
+    def __truediv__(self, other) -> "Distance":
+        self.km /= other
+        return Distance(round(self.km, 2))
+
+    def __lt__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return True if self.km < other.km else False
+        else:
+            return True if self.km < other else False
+
+    def __gt__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return True if self.km > other.km else False
+        else:
+            return True if self.km > other else False
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return True if self.km == other.km else False
+        else:
+            return True if self.km == other else False
+
+    def __le__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return True if self.km <= other.km else False
+        else:
+            return True if self.km <= other else False
+
+    def __ge__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return True if self.km >= other.km else False
+        else:
+            return True if self.km >= other else False
