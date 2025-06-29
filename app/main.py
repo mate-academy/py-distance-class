f33144de2beecb98a61fdf543d85ec44c6e81610
godfri <godfri@kali>
2025-06-29 21:09:39 +0300

    Виправив помилки в класі Distance

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union, Any
 
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km  # Основна одиниця зберігання - кілометри
@@ -10,14 +11,14 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union['Distance', float]) -> 'Distance':
+    def __add__(self, other: Union["Distance", float]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
         raise TypeError("Operand must be Distance or number")
 
-    def __iadd__(self, other: Union['Distance', float]) -> 'Distance':
+    def __iadd__(self, other: Union["Distance", float]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -26,12 +27,14 @@ class Distance:
             raise TypeError("Operand must be Distance or number")
         return self
 
-    def __mul__(self, other: float) -> 'Distance':
+    def __mul__(self, other: float) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         raise TypeError("Can only multiply by number")
 
-    def __truediv__(self, other: Union[float, 'Distance']) -> Union['Distance', float]:
+    def __truediv__(
+        self, other: Union[float, "Distance"]
+    ) -> Union["Distance", float]:
         if isinstance(other, (int, float)):
             if other == 0:
                 raise ZeroDivisionError("division by zero is not allowed")
@@ -47,28 +50,28 @@ class Distance:
             return self.km == other
         return False
 
-    def __lt__(self, other: Union['Distance', float]) -> bool:
+    def __lt__(self, other: Union["Distance", float]) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
         raise TypeError("Can only compare with Distance or number")
 
-    def __le__(self, other: Union['Distance', float]) -> bool:
+    def __le__(self, other: Union["Distance", float]) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
         raise TypeError("Can only compare with Distance or number")
 
-    def __gt__(self, other: Union['Distance', float]) -> bool:
+    def __gt__(self, other: Union["Distance", float]) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
         raise TypeError("Can only compare with Distance or number")
 
-    def __ge__(self, other: Union['Distance', float]) -> bool:
+    def __ge__(self, other: Union["Distance", float]) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
