5ee9b2779a9fb2b8c7d734d38f002a5e5c45a4b6
Konkov Mykola <kharkivnick@gmail.com>
2025-06-30 19:53:39 +0300

    Removed extra validation

--- app/main.py
+++ app/main.py
@@ -31,16 +31,12 @@ class Distance:
         return self
 
     def __mul__(self, other: Any) -> "Distance":
-        if isinstance(other, Distance):
-            return Distance(self.km * other.km)
-        elif isinstance(other, (int, float)):
-            return self.km * other
+        if isinstance(other, (int, float)):
+            return Distance(self.km * other)
         return NotImplemented
 
     def __truediv__(self, other: Any) -> "Distance":
-        if isinstance(other, Distance):
-            return Distance(round((self.km / other.km), 2))
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return Distance(round((self.km / other), 2))
         return NotImplemented
 
